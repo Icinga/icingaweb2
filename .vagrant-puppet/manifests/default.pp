ff3d21889063ab1cbc4bf381fcde4108a9e4c662
Eric Lippmann <eric.lippmann@netways.de>
2013-05-29 18:23:55 +0200

    Vagrant: Add automated provisioning of Icinga 1.9.1
    refs #4215
    refs #4216

---
+++ .vagrant-puppet/manifests/default.pp
@@ -0,0 +1,156 @@
+include apache
+include php
+include mysql
+include pgsql
+
+Exec { path => '/bin:/usr/bin' }
+
+exec { 'create-mysql-icinga-db':
+  unless  => 'mysql -uicinga -picinga icinga',
+  command => 'mysql -uroot -e "CREATE DATABASE icinga; \
+              GRANT ALL ON icinga.* TO icinga@localhost \
+              IDENTIFIED BY \'icinga\';"',
+  require => Service['mysqld'],
+}
+
+exec{ 'create-pgsql-icinga-db':
+  unless  => 'sudo -u postgres psql -l 2> /dev/null | grep -s icinga',
+  command => 'sudo -u postgres psql -c "CREATE ROLE icinga WITH LOGIN PASSWORD \'icinga\';" && sudo -u postgres createdb -O icinga -E UTF8 icinga && sudo -u postgres createlang plpgsql icinga',
+  require => Service['postgresql']
+}
+
+$icinga_packages = [ 'gcc', 'glibc', 'glibc-common', 'gd', 'gd-devel',
+  'libpng', 'libpng-devel', 'net-snmp', 'net-snmp-devel', 'net-snmp-utils',
+  'libdbi', 'libdbi-devel', 'libdbi-drivers',
+  'libdbi-dbd-mysql', 'libdbi-dbd-pgsql' ]
+package { $icinga_packages: ensure => installed }
+
+group { 'icinga-cmd':
+  ensure  => present,
+  members => 'www-data'
+}
+
+user { 'icinga':
+  ensure     => present,
+  groups     => 'icinga-cmd',
+  managehome => false
+}
+
+cmmi { 'icinga-mysql':
+  url     => 'http://sourceforge.net/projects/icinga/files/icinga/1.9.1/icinga-1.9.1.tar.gz/download',
+  output  => 'icinga-1.9.1.tar.gz',
+  flags   => '--prefix=/usr/local/icinga-mysql --with-command-group=icinga-cmd \
+              --enable-idoutils --with-init-dir=/tmp/icinga-mysql/etc/init.d',
+  creates => '/usr/local/icinga-mysql',
+  require => User['icinga']
+}
+
+file { '/etc/init.d/icinga-mysql':
+  source  => '/tmp/icinga-mysql/etc/init.d/icinga',
+  require => Cmmi['icinga-mysql']
+}
+
+file { '/etc/init.d/ido2db-mysql':
+  source  => '/tmp/icinga-mysql/etc/init.d/ido2db',
+  require => Cmmi['icinga-mysql']
+}
+
+cmmi { 'icinga-pgsql':
+  url     => 'http://sourceforge.net/projects/icinga/files/icinga/1.9.1/icinga-1.9.1.tar.gz/download',
+  output  => 'icinga-1.9.1.tar.gz',
+  flags   => '--prefix=/usr/local/icinga-pgsql \
+              --with-command-group=icinga-cmd --enable-idoutils \
+              --with-init-dir=/tmp/icinga-pgsql/etc/init.d',
+  creates => '/usr/local/icinga-pgsql',
+  require => User['icinga']
+}
+
+file { '/etc/init.d/icinga-pgsql':
+  source  => '/tmp/icinga-pgsql/etc/init.d/icinga',
+  require => Cmmi['icinga-pgsql']
+}
+
+file { '/etc/init.d/ido2db-pgsql':
+  source  => '/tmp/icinga-pgsql/etc/init.d/ido2db',
+  require => Cmmi['icinga-pgsql']
+}
+
+exec { 'populate-icinga-mysql-db':
+  unless  => 'mysql -uicinga -picinga icinga -e "SELECT * FROM icinga_dbversion;" > /dev/null',
+  command => 'mysql -uicinga -picinga icinga < /usr/local/src/icinga-mysql/icinga-1.9.1/module/idoutils/db/mysql/mysql.sql',
+  require => [Cmmi['icinga-mysql'], Exec['create-mysql-icinga-db']]
+}
+
+exec { 'populate-icinga-pgsql-db':
+  unless  => 'psql -U icinga -d icinga -c "SELECT * FROM icinga_dbversion;" > /dev/null',
+  command => 'sudo -u postgres psql -U icinga -d icinga < /usr/local/src/icinga-pgsql/icinga-1.9.1/module/idoutils/db/pgsql/pgsql.sql',
+  require => [Cmmi['icinga-pgsql'], Exec['create-pgsql-icinga-db']]
+}
+
+service { 'icinga-mysql':
+  ensure  => running,
+  require => Cmmi['icinga-mysql']
+}
+
+service { 'ido2db-mysql':
+  ensure  => running,
+  require => Cmmi['icinga-mysql']
+}
+
+file { '/usr/local/icinga-mysql/etc/ido2db.cfg':
+  content => template('icinga/ido2db-mysql.cfg.erb'),
+  owner   => 'icinga',
+  group   => 'icinga',
+  require => Cmmi['icinga-mysql'],
+  notify  => [Service['icinga-mysql'], Service['ido2db-mysql']]
+}
+
+file { '/usr/local/icinga-mysql/etc/idomod.cfg':
+  source  => '/usr/local/icinga-mysql/etc/idomod.cfg-sample',
+  owner   => 'icinga',
+  group   => 'icinga',
+  require => Cmmi['icinga-mysql'],
+  notify  => [Service['icinga-mysql'], Service['ido2db-mysql']]
+}
+
+file { '/usr/local/icinga-mysql/etc/modules/idoutils.cfg':
+  source  => '/usr/local/icinga-mysql/etc/modules/idoutils.cfg-sample',
+  owner   => 'icinga',
+  group   => 'icinga',
+  require => Cmmi['icinga-mysql'],
+  notify  => [Service['icinga-mysql'], Service['ido2db-mysql']]
+}
+
+service { 'icinga-pgsql':
+  ensure  => running,
+  require => Cmmi['icinga-pgsql']
+}
+
+service { 'ido2db-pgsql':
+  ensure  => running,
+  require => Cmmi['icinga-pgsql']
+}
+
+file { '/usr/local/icinga-pgsql/etc/ido2db.cfg':
+  content => template('icinga/ido2db-pgsql.cfg.erb'),
+  owner   => 'icinga',
+  group   => 'icinga',
+  require => Cmmi['icinga-pgsql'],
+  notify  => [Service['icinga-pgsql'], Service['ido2db-pgsql']]
+}
+
+file { '/usr/local/icinga-pgsql/etc/idomod.cfg':
+  source  => '/usr/local/icinga-pgsql/etc/idomod.cfg-sample',
+  owner   => 'icinga',
+  group   => 'icinga',
+  require => Cmmi['icinga-pgsql'],
+  notify  => [Service['icinga-pgsql'], Service['ido2db-pgsql']]
+}
+
+file { '/usr/local/icinga-pgsql/etc/modules/idoutils.cfg':
+  source  => '/usr/local/icinga-pgsql/etc/modules/idoutils.cfg-sample',
+  owner   => 'icinga',
+  group   => 'icinga',
+  require => Cmmi['icinga-pgsql'],
+  notify  => [Service['icinga-pgsql'], Service['ido2db-pgsql']]
+}
---
+++ .vagrant-puppet/modules/cmmi/manifests/init.pp
@@ -0,0 +1,79 @@
+# Define: cmmi
+#
+# This module downloads, extracts, builds and installs tar.gz archives using
+# wget, tar and the autotools stack. Build directory is always /usr/local/src.
+#
+# *Note* make sure to install build essentials before running cmmi.
+#
+# Parameters:
+#   [*url*]         - fetch archive via wget from this url.
+#   [*output*]      - filename to fetch the archive into.
+#   [*flags*]       - configure options.
+#   [*creates*]     - target directory the software will install to.
+#   [*make* ]       - command to make and make install the software.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  cmmi { 'example-software':
+#    url     => 'http://example-software.com/download/',
+#    output  => 'example-software.tar.gz',
+#    flags   => '--prefix=/opt/example-software',
+#    creates => '/opt/example-software',
+#    make    => 'make && make install'
+#  }
+#
+define cmmi(
+  $url,
+  $output,
+  $flags,
+  $creates,
+  $make='make all && make fullinstall && make install-config',
+) {
+
+  Exec { path => '/bin:/usr/bin' }
+
+  if ! defined(Package['wget']) {
+    package{ 'wget':
+      ensure => installed
+    }
+  }
+
+  $cwd = '/usr/local/src'
+
+  exec { "download-${name}":
+    cwd     => $cwd,
+    command => "wget -q ${url} -O ${output}",
+    creates => "${cwd}/${output}",
+    timeout => 120,
+    require => Package['wget']
+  }
+
+  $tld = inline_template('<%= File.basename(output, ".tar.gz") %>')
+  $src = "${cwd}/${name}/${tld}"
+
+  exec { "extract-${name}":
+    cwd     => $cwd,
+    command => "mkdir -p ${name} && tar --no-same-owner \
+                --no-same-permissions -xzf ${output} -C ${name}",
+    creates => $src,
+    require => Exec["download-${name}"]
+  }
+
+  exec { "configure-${name}":
+    cwd     => $src,
+    command => "sh ./configure ${flags}",
+    creates => "${src}/Makefile",
+    require => Exec["extract-${name}"]
+  }
+
+  exec { "make-${name}":
+    cwd     => $src,
+    command => $make,
+    creates => $creates,
+    require => Exec["configure-${name}"]
+  }
+}
