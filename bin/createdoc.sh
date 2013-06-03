420f407df494f8b9119622d4edf0cb1cafc3babe
Johannes Meyer <johannes.meyer@netways.de>
2013-06-03 11:40:42 +0200

    Fix: Doc-runner uses wrong file extension.
    Updated Doc-runner so that it converts files with
    the md-extension instead of the txt-extension.
    
    refs #4214

--- bin/createdoc.sh
+++ bin/createdoc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for filepath in `find ../doc -type f -name "*.txt"`; do
-    markdown $filepath > `dirname $filepath`/`basename $filepath .txt`.html
+for filepath in `find ../doc -type f -name "*.md"`; do
+    markdown $filepath > `dirname $filepath`/`basename $filepath .md`.html
 done
 
 exit 0
