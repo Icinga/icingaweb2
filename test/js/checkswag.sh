4b225eccee4d4ce424f7735cb67312b75f4bd847
Johannes Meyer <johannes.meyer@netways.de>
2013-06-03 16:07:52 +0200

    Fix: Style-checkers report their output to wrong location

--- test/js/checkswag.sh
+++ test/js/checkswag.sh
@@ -8,6 +8,6 @@ DIR=$(dirname $SCRIPTNAME)
 # Make sure that the destination directory for logs and reports exists
 mkdir -p $DIR/../../build/log
 
-jshint --reporter=jslint "$@" $DIR/../.. > $DIR/../../build/log/jshint_results.xm
+jshint --reporter=jslint "$@" $DIR/../.. > $DIR/../../build/log/jshint_results.xml
 
 exit 0
