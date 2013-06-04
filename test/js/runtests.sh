29f032502cb0eaf3322400d920ae6b0abeed3043
Marius Hein <marius.hein@netways.de>
2013-06-04 17:20:18 +0200

    Integrate regression tests
    Add js regression structure and configured mocha

--- test/js/runtests.sh
+++ test/js/runtests.sh
@@ -4,11 +4,23 @@ set -o nounset
 
 SCRIPTNAME=$(readlink -f $0)
 DIR=$(dirname $SCRIPTNAME)
+MOCHA=$(which mocha)
+DEFAULT="--recursive --require should"
+
+if [[ ! -x $MOCHA ]]; then
+    echo "mocha not found!";
+    exit 1
+fi
 
 # Make sure that the destination directory for logs and reports exists
 mkdir -p $DIR/../../build/log
 
-mocha --reporter "xunit" --recursive "$@" . > $DIR/../../build/log/mocha_results.xml
-mocha --reporter "cobertura" --recursive "$@" . > $DIR/../../build/log/mocha_coverage.xml
+cd $DIR
+
+# Don't know where node modules are
+export NODE_PATH=.:/usr/local/lib/node_modules:/usr/lib/node_modules
+
+$MOCHA --reporter "xunit" $DEFAULT "$@" . > $DIR/../../build/log/mocha_results.xml
+$MOCHA --reporter "mocha-cobertura-reporter" $DEFAULT "$@" . > $DIR/../../build/log/mocha_coverage.xml
 
 exit 0
