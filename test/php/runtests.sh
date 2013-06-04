ed24a90c1a66a5bb0b2a5e45fdaac08b840ceede
Marius Hein <marius.hein@netways.de>
2013-06-04 11:16:09 +0200

    Integrate PHPUNIT
    Modify tester, chdir to tests directory and test binary
    
    refs #4068

--- test/php/runtests.sh
+++ test/php/runtests.sh
@@ -4,10 +4,18 @@ set -o nounset
 
 SCRIPTNAME=$(readlink -f $0)
 DIR=$(dirname $SCRIPTNAME)
+PHPUNIT=$(which phpunit)
+
+if [[ ! -x $PHPUNIT ]]; then
+    echo "PHPUnit not found!"
+    exit 1
+fi
 
 # Make sure that the destination directory for logs and reports exists
 mkdir -p $DIR/../../build/log
 
-phpunit "$@" .
+cd $DIR
+
+$PHPUNIT "$@" .
 
 exit 0
