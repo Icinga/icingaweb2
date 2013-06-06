e5df44dc6ac45aaab6431a0fed2a014efc5e8754
Eric Lippmann <eric.lippmann@netways.de>
2013-06-06 17:01:05 +0200

    Vagrant: Add mocha and jshint
    refs #4219

--- .vagrant-puppet/manifests/default.pp
+++ .vagrant-puppet/manifests/default.pp
@@ -276,3 +276,33 @@ exec { 'install nodejs':
   unless  => 'rpm -qa | grep ^npm',
   require => Class['epel']
 }
+
+exec { 'install npm/mocha':
+  command => 'npm install -g mocha',
+  creates => '/usr/lib/node_modules/mocha',
+  require => Exec['install nodejs']
+}
+
+exec { 'install npm/jshint':
+  command => 'npm install -g jshint',
+  creates => '/usr/lib/node_modules/jshint',
+  require => Exec['install nodejs']
+}
+
+exec { 'install npm/expect':
+  command => 'npm install -g expect',
+  creates => '/usr/lib/node_modules/expect',
+  require => Exec['install nodejs']
+}
+
+exec { 'install npm/should':
+  command => 'npm install -g should',
+  creates => '/usr/lib/node_modules/should',
+  require => Exec['install nodejs']
+}
+
+exec { 'install ZendFramework':
+  command => 'yum -d 0 -e 0 -y --enablerepo=epel install php-ZendFramework',
+  unless  => 'rpm -qa | grep php-ZendFramework',
+  require => Class['epel']
+}
