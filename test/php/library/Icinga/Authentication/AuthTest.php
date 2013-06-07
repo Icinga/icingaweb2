d1f6668fe78611c798afacf21f0e75c13b40a806
Jannis Moßhammer <jannis.mosshammer@netways.de>
2013-06-07 12:40:21 +0200

    Add library from incubator and teststubs
    This commit only contains the untested libaries from the incubator
    and autogenerated teststubs and is the basis for writing the tests
    
    refs #4250

---
+++ test/php/library/Icinga/Authentication/AuthTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Icinga\Authentication;
+/**
+*
+* Test class for Auth 
+* Created Fri, 07 Jun 2013 10:38:16 +0000 
+*
+**/
+class AuthTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+    * Test for Auth::IsAuthenticated() 
+    *
+    **/
+    public function testIsAuthenticated()
+    {
+        $this->markTestIncomplete('testIsAuthenticated is not implemented yet');
+    }
+
+    /**
+    * Test for Auth::GetUsername() 
+    *
+    **/
+    public function testGetUsername()
+    {
+        $this->markTestIncomplete('testGetUsername is not implemented yet');
+    }
+
+    /**
+    * Test for Auth::GetEmail() 
+    *
+    **/
+    public function testGetEmail()
+    {
+        $this->markTestIncomplete('testGetEmail is not implemented yet');
+    }
+
+    /**
+    * Test for Auth::SetAuthenticatedUser() 
+    *
+    **/
+    public function testSetAuthenticatedUser()
+    {
+        $this->markTestIncomplete('testSetAuthenticatedUser is not implemented yet');
+    }
+
+    /**
+    * Test for Auth::ForgetAuthentication() 
+    *
+    **/
+    public function testForgetAuthentication()
+    {
+        $this->markTestIncomplete('testForgetAuthentication is not implemented yet');
+    }
+
+    /**
+    * Test for Auth::HasPermission() 
+    *
+    **/
+    public function testHasPermission()
+    {
+        $this->markTestIncomplete('testHasPermission is not implemented yet');
+    }
+
+    /**
+    * Test for Auth::GetInstance() 
+    * Note: This method is static! 
+    *
+    **/
+    public function testGetInstance()
+    {
+        $this->markTestIncomplete('testGetInstance is not implemented yet');
+    }
+
+}
---
+++ test/php/library/Icinga/Authentication/LdapUserBackendTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Icinga\Authentication;
+/**
+*
+* Test class for Ldapuserbackend 
+* Created Fri, 07 Jun 2013 10:38:16 +0000 
+*
+**/
+class LdapuserbackendTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+    * Test for LdapUserBackend::HasUsername() 
+    *
+    **/
+    public function testHasUsername()
+    {
+        $this->markTestIncomplete('testHasUsername is not implemented yet');
+    }
+
+    /**
+    * Test for LdapUserBackend::Authenticate() 
+    *
+    **/
+    public function testAuthenticate()
+    {
+        $this->markTestIncomplete('testAuthenticate is not implemented yet');
+    }
+
+}
