8c6f2a07ae10ed082865adb6eeb3e3f2fdb68e11
Eric Lippmann <eric.lippmann@netways.de>
2013-07-10 11:40:48 +0200

    Remove magic retrieval of configuration files
    This change leads to expected exceptions when bootstrapping the
    application since the \Icinga\Application\Config class was
    refactored but its usage not.
    
    refs #4354
    refs #4353

--- test/php/library/Icinga/Application/ConfigTest.php
+++ test/php/library/Icinga/Application/ConfigTest.php
@@ -1,50 +1,86 @@
 <?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
 
 namespace Tests\Icinga\Application;
-/**
-*
-* Test class for Config 
-* Created Thu, 07 Feb 2013 10:07:13 +0000 
-*
-**/
+
+require 'Zend/Config/Ini.php';
+require dirname(__FILE__) . '/../../../../../library/Icinga/Application/Config.php';
+
+use Icinga\Application\Config as IcingaConfig;
+
 class ConfigTest extends \PHPUnit_Framework_TestCase
 {
-
-    /**
-    * Test for Config::ListAll() 
-    *
-    **/
-    public function testListAll()
+    public function setUp()
     {
-        $this->markTestIncomplete('testListAll is not implemented yet');
+        IcingaConfig::$configDir = dirname(__FILE__) . '/Config/files';
     }
 
-    /**
-    * Test for Config::GetConfigDir() 
-    *
-    **/
-    public function testGetConfigDir()
+    public function testAppConfig()
     {
-        $this->markTestIncomplete('testGetConfigDir is not implemented yet');
+        $config = IcingaConfig::app();
+        $this->assertEquals(1, $config->logging->enable);
+        // Test non-existent property where null is the default value
+        $this->assertEquals(null, $config->logging->get('disable'));
+        // Test non-existent property using zero as the default value
+        $this->assertEquals(0, $config->logging->get('disable', 0));
+        // Test retrieve full section
+        $this->assertEquals(
+            array(
+                'disable' => 1,
+                'db' => array(
+                    'user' => 'user',
+                    'password' => 'password'
+                )
+            ),
+            $config->backend->toArray()
+        );
+        // Test non-existent section using 'default' as default value
+        $this->assertEquals('default', $config->get('magic', 'default'));
+        // Test sub-properties
+        $this->assertEquals('user', $config->backend->db->user);
+        // Test non-existent sub-property using 'UTF-8' as the default value
+        $this->assertEquals('UTF-8', $config->backend->db->get('encoding', 'UTF-8'));
+        // Test invalid property names using false as default value
+        $this->assertEquals(false, $config->backend->get('.', false));
+        $this->assertEquals(false, $config->backend->get('db.', false));
+        $this->assertEquals(false, $config->backend->get('.user', false));
+        // Test retrieve array of sub-properties
+        $this->assertEquals(
+            array(
+                'user' => 'user',
+                'password' => 'password'
+            ),
+            $config->backend->db->toArray()
+        );
+        // Test singleton
+        $this->assertEquals($config, IcingaConfig::app());
+        $this->assertEquals(array('logging', 'backend'), $config->keys());
+        $this->assertEquals(array('enable'), $config->keys('logging'));
     }
 
-    /**
-    * Test for Config::__get() 
-    *
-    **/
-    public function test__get()
+    public function testAppExtraConfig()
     {
-        $this->markTestIncomplete('test__get is not implemented yet');
+        $extraConfig = IcingaConfig::app('extra');
+        $this->assertEquals(1, $extraConfig->meta->version);
+        $this->assertEquals($extraConfig, IcingaConfig::app('extra'));
     }
 
-    /**
-    * Test for Config::GetInstance() 
-    * Note: This method is static! 
-    *
-    **/
-    public function testGetInstance()
+    public function testModuleConfig()
     {
-        $this->markTestIncomplete('testGetInstance is not implemented yet');
+        $moduleConfig = IcingaConfig::module('amodule');
+        $this->assertEquals(1, $moduleConfig->menu->get('breadcrumb'));
+        $this->assertEquals($moduleConfig, IcingaConfig::module('amodule'));
     }
 
+    public function testModuleExtraConfig()
+    {
+        $moduleExtraConfig = IcingaConfig::module('amodule', 'extra');
+        $this->assertEquals(
+            'inetOrgPerson',
+            $moduleExtraConfig->ldap->user->get('ldap_object_class')
+        );
+        $this->assertEquals($moduleExtraConfig, IcingaConfig::module('amodule', 'extra'));
+    }
 }
