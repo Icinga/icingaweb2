008d51034d9bca1cdf046259da70f20d949c50d8
Eric Lippmann <eric.lippmann@netways.de>
2013-07-22 17:24:56 +0200

    Vagrant: Add missing manifest finalize.sh
    refs #4385

--- .vagrant-puppet/modules/php/manifests/init.pp
+++ .vagrant-puppet/modules/php/manifests/init.pp
@@ -8,13 +8,25 @@
 #
 # Requires:
 #
+#   apache
+#
 # Sample Usage:
 #
 #   include php
 #
 class php {
 
+  include apache
+
   package { 'php':
-    ensure  => installed
+    ensure  => installed,
+    require => Package['apache'],
+    notify  => Service['apache']
+  }
+
+  file { '/etc/php.d/error_reporting.ini':
+    content => template('php/error_reporting.ini.erb'),
+    require => Package['php'],
+    notify  => Service['apache']
   }
 }
