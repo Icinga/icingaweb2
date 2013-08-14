e0f1d7dc376c1ae76a706bbff66c0d160c2af061
Eric Lippmann <eric.lippmann@netways.de>
2013-08-14 17:52:11 +0200

    Vagrant: Mount /vagrant/var/log/ writable for apache
    This requires you to run vagrant reload if the vm is already running
    
    refs #4549

--- .vagrant-puppet/manifests/finalize.sh
+++ .vagrant-puppet/manifests/finalize.sh
@@ -11,8 +11,8 @@ installJquery () {
 }
 
 mountIcinga2webConfd () {
-    # Remount /vagrant/config with appropriate permissions since the group apache is missing initially
-    mount -t vboxsf -o uid=`id -u vagrant`,gid=`id -g apache`,dmode=775,fmode=775 /vagrant/config /vagrant/config
+    # Remount /vagrant/config/ with appropriate permissions since the group apache is missing initially
+    mount -t vboxsf -o uid=`id -u vagrant`,gid=`id -g apache`,dmode=775,fmode=775 /vagrant/config/ /vagrant/config/
 }
 
 startServicesWithNonLSBCompliantExitStatusCodes () {
@@ -21,8 +21,14 @@ startServicesWithNonLSBCompliantExitStatusCodes () {
     service ido2db-pgsql start || true
 }
 
+mountIcinga2webVarLog () {
+    # Remount /vagrant/var/log/ with appropriate permissions since the group apache is missing initially
+    mount -t vboxsf -o uid=`id -u vagrant`,gid=`id -g apache`,dmode=775,fmode=775 /vagrant/var/log/ /vagrant/var/log/
+}
+
 installJquery
 mountIcinga2webConfd
 startServicesWithNonLSBCompliantExitStatusCodes
+mountIcinga2webVarLog
 
 exit 0
