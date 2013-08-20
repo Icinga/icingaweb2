d7358c65be8819e4a926b94665bc5fb89ccbc9d5
Jannis Moßhammer <jannis.mosshammer@netways.de>
2013-08-20 20:12:40 +0200

    Remove QUrl and QLink, add Href
    refs #4516

--- application/views/helpers/Href.php
+++ application/views/helpers/Href.php
@@ -1,45 +1,48 @@
 <?php
 // @codingStandardsIgnoreStart
-
 // {{{ICINGA_LICENSE_HEADER}}}
 /**
- * Icinga 2 Web - Head for multiple monitoring frontends
+ * This file is part of Icinga 2 Web.
+ * 
+ * Icinga 2 Web - Head for multiple monitoring backends.
  * Copyright (C) 2013 Icinga Development Team
- *
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
+ * 
  * @copyright 2013 Icinga Development Team <info@icinga.org>
- * @author Icinga Development Team <info@icinga.org>
+ * @license   http://www.gnu.org/licenses/gpl-2.0.txt GPL, version 2
+ * @author    Icinga Development Team <info@icinga.org>
  */
 // {{{ICINGA_LICENSE_HEADER}}}
 
 use Icinga\Web\Url;
 
-class Zend_View_Helper_QUrl extends Zend_View_Helper_Abstract
+/**
+ * Helper class for creating absolute links from relative strings
+ *
+ */
+class Zend_View_Helper_Href extends Zend_View_Helper_Abstract
 {
-    public function qUrl()
-    {
-        $params = func_get_args();
-        $url = array_shift($params);
-        if (isset($params[0])) {
-            $params = $params[0];
-        } else {
-            $params = array();
-        }
-        return Url::fromPath($url, $params);
+
+    /**
+     * Turn the relative link $link into absolute one
+     *
+     * @param $link
+     */
+    public function href($link, array $params = array()) {
+        return Url::fromPath($link, $params)->getAbsoluteUrl();
     }
 }
-
-// @codingStandardsIgnoreStop
+// @codingStandardsIgnoreEnd
