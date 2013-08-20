26be35d4476d1d5f0001cb44c1ffa1e9ba5f07bb
Thomas Gelf <thomas.gelf@netways.de>
2013-08-21 00:50:15 +0200

    Reverting change, this breaks things. No chance to redirect after rendering.

--- modules/monitoring/application/controllers/ShowController.php
+++ modules/monitoring/application/controllers/ShowController.php
@@ -1,5 +1,4 @@
 <?php
-// @codingStandardsIgnoreStart
 // {{{ICINGA_LICENSE_HEADER}}}
 /**
  * This file is part of Icinga 2 Web.
@@ -27,6 +26,7 @@
  */
 // {{{ICINGA_LICENSE_HEADER}}}
 
+// @codingStandardsIgnoreStart
 use \Icinga\Module\Monitoring\Backend;
 use Icinga\Web\Controller\ActionController;
 use Icinga\Web\Hook;
@@ -356,21 +356,6 @@ class Monitoring_ShowController extends ActionController
         ));
     }
 
-    protected function prepareGrapherHook()
-    {
-        if ($grapher = Hook::get('grapher')) {
-            $object = $this->view->object;
-            if ($grapher->hasGraph(
-                $object->host_name,
-                $object->service_description
-            )) {
-                $this->view->preview_image = $grapher->getPreviewImage(
-                    $object->host_name,
-                    $object->service_description
-                );
-            }
-        }
-    }
     /**
      * Creating tabs for this controller
      * @return \Icinga\Web\Widget\AbstractWidget
@@ -444,5 +429,4 @@ class Monitoring_ShowController extends ActionController
 
     }
 }
-
 // @codingStandardsIgnoreEnd
