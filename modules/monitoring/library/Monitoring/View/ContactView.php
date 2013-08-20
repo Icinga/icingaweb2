e95c4fd080508483b7a7896175a7d71e9d7b1827
Thomas Gelf <thomas.gelf@netways.de>
2013-08-21 00:27:30 +0200

    More columns for contacts

--- modules/monitoring/library/Monitoring/View/ContactView.php
+++ modules/monitoring/library/Monitoring/View/ContactView.php
@@ -7,11 +7,31 @@ class ContactView extends AbstractView
     protected $query;
 
     protected $availableColumns = array(
+        'contact_name',
         'contact_alias',
         'contact_email',
         'contact_pager',
         'contact_notify_hosts',
         'contact_notify_services',
+        'contact_has_host_notfications',
+        'contact_has_service_notfications',
+        'contact_can_submit_commands',
+        'contact_notify_service_recovery',
+        'contact_notify_service_warning',
+        'contact_notify_service_critical',
+        'contact_notify_service_unknown',
+        'contact_notify_service_flapping',
+        'contact_notify_service_downtime',
+        'contact_notify_host_recovery',
+        'contact_notify_host_down',
+        'contact_notify_host_unreachable',
+        'contact_notify_host_flapping',
+        'contact_notify_host_downtime',
+        'host_object_id',
+        'host_name',
+        'service_object_id',
+        'service_host_name',
+        'service_description',
     );
 
     protected $specialFilters = array();
