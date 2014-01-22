4473008d65d5aa36141a6668101397b59a844c7a
Eric Lippmann <eric.lippmann@netways.de>
2014-01-22 14:57:54 +0100

    Show error message and stack trace on exceptions
    refs #5507

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -42,26 +42,23 @@ class ErrorController extends ActionController
      */
     public function errorAction()
     {
-        $errors = $this->_getParam('error_handler');
-        switch ($errors->type) {
+        $error = $this->_getParam('error_handler');
+        switch ($error->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-                // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
                 $this->view->message = 'Page not found';
                 break;
             default:
-                // application error
                 $this->getResponse()->setHttpResponseCode(500);
-                $this->view->message = 'Application error';
+                $this->view->message = $error->exception->getMessage();
                 break;
         }
-        // conditionally display exceptions
         if ($this->getInvokeArg('displayExceptions') == true) {
-            $this->view->exception = $errors->exception;
+            $this->view->stackTrace = $error->exception->getTraceAsString();
         }
-        $this->view->request = $errors->request;
+        $this->view->request = $error->request;
     }
 }
 // @codingStandardsIgnoreEnd
