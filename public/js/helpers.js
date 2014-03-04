090974401c1745cf0f3939c1df454a0d4b294ac8
Thomas Gelf <thomas@gelf.net>
2014-03-04 13:06:12 +0000

    Workaround for apply missing in IE console (native object vs function)

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -16,3 +16,28 @@
 
 })(Object);
 
+(function (console) {
+
+    'use strict';
+
+    if ('undefined' === console) {
+        return;
+    }
+
+    /* Fix console for IE9, TBD: test IE8 */
+    if (typeof console.log == 'object' && Function.prototype.bind && console) {
+        [
+            'log',
+            'info',
+            'warn',
+            'error',
+            'assert',
+            'dir',
+            'clear',
+            'profile',
+            'profileEnd'
+        ].forEach(function (method) {
+            console[method] = this.call(console[method], console);
+        }, Function.prototype.bind);
+    }
+})(console);
