a0e3720abb52c8678a7e57664ca3dbf4268ef4d6
Alexander Klimov <Alexander.Klimov@netways.de>
2014-03-07 15:16:32 +0100

    Monitoring backends configuration icon hotfix

--- modules/monitoring/application/forms/Config/ConfirmRemovalForm.php
+++ modules/monitoring/application/forms/Config/ConfirmRemovalForm.php
@@ -86,7 +86,7 @@ class ConfirmRemovalForm extends Form
                 'escape'    => false,
                 'value'     => '1',
                 'class'     => 'btn btn-cta btn-common',
-                'label'     => $this->view->icon('remove.png') . ' Confirm Removal'
+                'label'     => $this->getView()->icon('remove.png') . ' Confirm Removal'
             )
         );
     }
