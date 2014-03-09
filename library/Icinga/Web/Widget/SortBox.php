93dc45241a0a4c9f8f1f753c617ffaf56e130693
Thomas Gelf <thomas@gelf.net>
2014-03-09 13:53:31 +0100

    Fix missplaced autosubmit in sort box

--- library/Icinga/Web/Widget/SortBox.php
+++ library/Icinga/Web/Widget/SortBox.php
@@ -159,8 +159,8 @@ class SortBox extends AbstractWidget
                 'multiOptions'  => array(
                     'desc'      => 'Desc',
                     'asc'       => 'Asc',
+                ),
                 'class' => 'autosubmit'
-                )
 
             )
         );
