a1649a1f2218fa7f462c77ecdf4afd387e6c7599
Johannes Meyer <johannes.meyer@netways.de>
2014-04-07 15:13:28 +0200

    Refactor PreferencesStore and IniStore
    refs #5682

--- library/Icinga/User/Preferences/Store/IniStore.php
+++ library/Icinga/User/Preferences/Store/IniStore.php
@@ -4,48 +4,17 @@
 
 namespace Icinga\User\Preferences\Store;
 
-use Zend_Config;
-use Icinga\Application\Config as IcingaConfig;
+use \Zend_Config;
+use Icinga\Util\File;
 use Icinga\Config\PreservingIniWriter;
 use Icinga\Exception\NotReadableError;
 use Icinga\Exception\NotWritableError;
-use Icinga\User;
+use Icinga\User\Preferences;
 use Icinga\User\Preferences\PreferencesStore;
-use Icinga\Util\File;
+use Icinga\Application\Config as IcingaConfig;
 
 /**
  * Load and save user preferences from and to INI files
- *
- * Usage example:
- * <code>
- * <?php
- *
- * use Zend_Config;
- * use Icinga\User\Preferences;
- * use Icinga\User\Preferences\PreferencesStore;
- * use Icinga\User\Preferences\Store\IniStore;
- *
- * // Create the store from the factory (preferred approach)
- * $store = new PreferencesStore(
- *     new Zend_Config(
- *         'type'       => 'ini',
- *         'configPath' => '/path/to/preferences'
- *     ),
- *     $user // Instance of \Icinga\User
- * );
- *
- * // Create the store directly
- * $store = new IniStore(
- *     new Zend_Config(
- *         'configPath' => '/path/to/preferences'
- *     ),
- *     $user // Instance of \Icinga\User
- * );
- *
- * $preferences = new Preferences($store->load());
- * $preferences->aPreference = 'value';
- * $store->save($preferences);
- * </code>
  */
 class IniStore extends PreferencesStore
 {
@@ -54,93 +23,135 @@ class IniStore extends PreferencesStore
      *
      * @var string
      */
-    private $preferencesFile;
+    protected $preferencesFile;
 
     /**
      * Stored preferences
      *
      * @var array
      */
-    private $preferences;
+    protected $preferences = array();
 
     /**
      * Writer which stores the preferences
      *
      * @var PreservingIniWriter
      */
-    private $writer;
+    protected $writer;
 
     /**
      * Initialize the store
-     *
-     * @throws NotReadableError When the preferences INI file of the given user is not readable
      */
-    public function init()
+    protected function init()
     {
         $this->preferencesFile = sprintf(
             '%s/%s.ini',
             IcingaConfig::resolvePath($this->getStoreConfig()->configPath),
             $this->getUser()->getUsername()
         );
-        if (file_exists($this->preferencesFile)) {
-            if (!is_readable($this->preferencesFile)) {
-                throw new NotReadableError('Preferences INI file ' . $this->preferencesFile . ' for user '
-                    . $this->getUser()->getUsername() . ' is not readable');
-            } else {
-                $this->preferences = parse_ini_file($this->preferencesFile);
-            }
-        }
     }
 
     /**
      * Load preferences from source
      *
-     * @return array
+     * @return  array
+     *
+     * @throws  NotReadableError    When the INI file of the user exists and is not readable
      */
     public function load()
     {
-        return $this->preferences !== null ? $this->preferences : array();
+        if (file_exists($this->preferencesFile)) {
+            if (!is_readable($this->preferencesFile)) {
+                throw new NotReadableError(
+                    'Preferences INI file ' . $this->preferencesFile . ' for user '
+                    . $this->getUser()->getUsername() . ' is not readable'
+                );
+            } else {
+                $this->preferences = parse_ini_file($this->preferencesFile);
+            }
+        }
+
+        return $this->preferences;
     }
 
     /**
-     * Create, update and delete the given preferences
+     * Save the given preferences
      *
-     * @param   array $newPreferences       Key-value array of preferences to create
-     * @param   array $updatedPreferences   Key-value array of preferences to update
-     * @param   array $deletedPreferences   An array of preference names to delete
-     *
-     * @throws  NotWritableError            When either the path to the preferences INI files is not writable or the
-     *                                      preferences INI file for the given user is not writable
+     * @param   Preferences     $preferences    The preferences to save
      */
-    public function cud($newPreferences, $updatedPreferences, $deletedPreferences)
+    public function save(Preferences $preferences)
     {
-        if ($this->preferences === null) {
-            // Preferences INI file does not yet exist
-            if (!is_writable($this->getStoreConfig()->configPath)) {
-                throw new NotWritableError('Path to the preferences INI files ' . $this->getStoreConfig()->configPath
-                    . ' is not writable');
-            }
-            File::create($this->preferencesFile);
-            $this->preferences = array();
-        }
-        if (!is_writable($this->preferencesFile)) {
-            throw new NotWritableError('Preferences INI file ' . $this->preferencesFile . ' for user '
-                . $this->getUser()->getUsername() . ' is not writable');
-        }
-        foreach ($newPreferences as $name => $value) {
-            $this->preferences[$name] = $value;
-        }
-        foreach ($updatedPreferences as $name => $value) {
-            $this->preferences[$name] = $value;
-        }
-        foreach ($deletedPreferences as $name) {
-            unset($this->preferences[$name]);
-        }
+        $preferences = $preferences->toArray();
+        $this->update(
+            array_merge(
+                array_diff_key($preferences, $this->preferences),
+                array_diff_assoc($preferences, $this->preferences)
+            )
+        );
+        $this->delete(array_keys(array_diff_key($this->preferences, $preferences)));
+        $this->write();
+    }
+
+    /**
+     * Write the preferences
+     *
+     * @throws  NotWritableError    In case the INI file cannot be written
+     */
+    public function write()
+    {
         if ($this->writer === null) {
+            if (!file_exists($this->preferencesFile)) {
+                if (!is_writable($this->getStoreConfig()->configPath)) {
+                    throw new NotWritableError(
+                        sprintf(
+                            'Path to the preferences INI files %s is not writable',
+                            $this->getStoreConfig()->configPath
+                        )
+                    );
+                }
+
+                File::create($this->preferencesFile);
+            }
+
+            if (!is_writable($this->preferencesFile)) {
+                throw new NotWritableError(
+                    'Preferences INI file ' . $this->preferencesFile . ' for user '
+                    . $this->getUser()->getUsername() . ' is not writable'
+                );
+            }
+
             $this->writer = new PreservingIniWriter(
-                array('config' => new Zend_Config($this->preferences), 'filename' => $this->preferencesFile)
+                array(
+                    'config'    => new Zend_Config($this->preferences),
+                    'filename'  => $this->preferencesFile
+                )
             );
         }
+
         $this->writer->write();
+    }
+
+    /**
+     * Add or update the given preferences
+     *
+     * @param   array   $preferences    The preferences to set
+     */
+    protected function update(array $preferences)
+    {
+        foreach ($preferences as $key => $value) {
+            $this->preferences[$key] = $value;
+        }
+    }
+
+    /**
+     * Delete the given preferences by name
+     *
+     * @param   array   $preferenceKeys     The preference names to delete
+     */
+    protected function delete(array $preferenceKeys)
+    {
+        foreach ($preferenceKeys as $key) {
+            unset($this->preferences[$key]);
+        }
     }
 }
