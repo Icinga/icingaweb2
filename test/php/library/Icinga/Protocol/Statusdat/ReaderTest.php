e1793c9ea1dd4829bc3ebe0789ab166359ff737d
Johannes Meyer <johannes.meyer@netways.de>
2014-04-09 14:56:25 +0200

    Make the statusdat reader test using its own cache location
    refs #4639

--- test/php/library/Icinga/Protocol/Statusdat/ReaderTest.php
+++ test/php/library/Icinga/Protocol/Statusdat/ReaderTest.php
@@ -69,18 +69,19 @@ class ReaderTest extends \PHPUnit_Framework_TestCase
                 )
             )
         );
+        $cacheDir = realpath(dirname(__FILE__) . '/.cache');
         $object_file = tempnam("./dir", "object");
         $status_file = tempnam("./dir", "status");
         $reader = new Reader(new ConfigMock(array(
-            "cache_path" => "/tmp",
-            "object_file" => $object_file,
-            "status_file" => $status_file
+            "cache_path"    => $cacheDir,
+            "object_file"   => $object_file,
+            "status_file"   => $status_file
         )), $parser);
         unlink($object_file);
         unlink($status_file);
-        $this->assertTrue(file_exists("/tmp/zend_cache---object" . md5($object_file)));
-        $this->assertTrue(file_exists("/tmp/zend_cache---state" . md5($object_file)));
-        system("rm /tmp/zend_cache*");
+        $this->assertTrue(file_exists($cacheDir . '/zend_cache---object' . md5($object_file)));
+        $this->assertTrue(file_exists($cacheDir . '/zend_cache---state' . md5($object_file)));
+        system('rm ' . $cacheDir . '/zend_cache*');
     }
 
     public function testEmptyFileException()
