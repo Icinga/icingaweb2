659e6f774e6fbe7617784ab4f5b8f92e735f67c7
Eric Lippmann <eric.lippmann@netways.de>
2014-04-15 16:23:17 +0200

    Add interface "Queryable" (WIP)
    Interface for classes which act as a data source and thus return or are a Fetchable.
    The name of the interface is not yet final though.

---
+++ library/Icinga/Data/Queryable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Icinga\Data;
+
+/**
+ * Interface for specifying data sources
+ */
+interface Queryable
+{
+    /**
+     * Set the target and fields to query
+     *
+     * @param   string  $target
+     * @param   array   $fields
+     *
+     * @return  Fetchable
+     */
+    public function from($target, array $fields = null);
+}
