3e0d254dfd8d1beeda6020f5c03b1aa9be9dff52
Eric Lippmann <eric.lippmann@netways.de>
2014-04-15 15:48:51 +0200

    Implement "Limitable" interface for retrieving just a portion of a result set
    Defines how to set a limit count and offset and should be used everywhere
    where setting limit and offset is supported.

---
+++ library/Icinga/Data/Limitable.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Icinga\Data;
+
+/**
+ * Interface for retrieving just a portion of a result set
+ */
+interface Limitable
+{
+    /**
+     * Set a limit count and offset
+     *
+     * @param   int $count  Number of rows to return
+     * @param   int $offset Start returning after this many rows
+     *
+     * @return  self
+     */
+    public function limit($count = null, $offset = null);
+
+    /**
+     * Whether a limit is set
+     *
+     * @return bool
+     */
+    public function hasLimit();
+
+    /**
+     * Get the limit if any
+     *
+     * @return int|null
+     */
+    public function getLimit();
+
+    /**
+     * Whether an offset is set
+     *
+     * @return bool
+     */
+    public function hasOffset();
+
+    /**
+     * Get the offset if any
+     *
+     * @return int|null
+     */
+    public function getOffset();
+}
