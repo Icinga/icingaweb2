e78d98a60786b14e2cb7fbb498587013f1046704
Eric Lippmann <eric.lippmann@netways.de>
2014-05-27 15:07:49 +0200

    Doc: Add action to display a chapter of Icinga Web 2's documentation
    Note that the FULL documentation is displayed yet.
    
    refs #4820

--- modules/doc/application/controllers/IcingawebController.php
+++ modules/doc/application/controllers/IcingawebController.php
@@ -2,6 +2,7 @@
 // {{{ICINGA_LICENSE_HEADER}}}
 // {{{ICINGA_LICENSE_HEADER}}}
 
+use \Zend_Controller_Action_Exception;
 use Icinga\Application\Icinga;
 use Icinga\Module\Doc\DocController;
 
@@ -13,5 +14,19 @@ class Doc_IcingawebController extends DocController
     public function tocAction()
     {
         $this->renderToc(Icinga::app()->getApplicationDir('/../doc'), 'Icinga Web 2');
+    }
+
+    /**
+     * View a chapter of Icinga Web 2's documentation
+     *
+     * @throws Zend_Controller_Action_Exception
+     */
+    public function chapterAction()
+    {
+        $chapterName = $this->getParam('chapterName');
+        if ($chapterName === null) {
+            throw new Zend_Controller_Action_Exception('Missing parameter "chapterName"', 404);
+        }
+        $this->renderChapter($chapterName, Icinga::app()->getApplicationDir('/../doc'));
     }
 }
