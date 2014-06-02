5559cf6c2b21a41fddbda847a23a0f4aa96b7819
Eric Lippmann <eric.lippmann@netways.de>
2014-06-02 14:07:59 +0200

    lib: Add AuthenticationException for exceptions thrown during authentication
    Our user backends log exceptions thrown during authentication but they don't throw
    a exception to inform the caller that an error occured. The new `AuthenticationException'
    class should be thrown in that case.

---
+++ library/Icinga/Exception/AuthenticationException.php
@@ -0,0 +1,14 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception thrown if an error occurs during authentication
+ */
+class AuthenticationException extends RuntimeException
+{
+}
