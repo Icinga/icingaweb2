02007cf9fb9208c1b6238d59d59b85446321f3f1
Eric Lippmann <eric.lippmann@netways.de>
2014-06-03 13:11:28 +0200

    Vagrant: No longer remount the config/ directory
    The virtual machine uses its own config files beneath /etc/icingaweb.

--- .vagrant-puppet/manifests/finalize.sh
+++ .vagrant-puppet/manifests/finalize.sh
@@ -10,11 +10,6 @@ installJquery () {
     fi
 }
 
-mountIcinga2webConfd () {
-    # Remount /vagrant/config/ with appropriate permissions since the group apache is missing initially
-    mount -t vboxsf -o uid=`id -u vagrant`,gid=`id -g apache`,dmode=775,fmode=775 /vagrant/config/ /vagrant/config/
-}
-
 startServicesWithNonLSBCompliantExitStatusCodes () {
     # Unfortunately the ido2db init script is not LSB compliant and hence not started via puppet
     service ido2db-mysql start || true
@@ -27,7 +22,6 @@ mountIcinga2webVarLog () {
 }
 
 installJquery
-mountIcinga2webConfd
 startServicesWithNonLSBCompliantExitStatusCodes
 mountIcinga2webVarLog
 
