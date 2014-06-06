e01629fe0b3d09554ca98eef67d6247e66ad6fe1
Thomas Gelf <thomas@gelf.net>
2014-06-06 08:31:00 +0000

    ActionController: fix initial redirect
    refs #6419

--- library/Icinga/Web/Controller/ActionController.php
+++ library/Icinga/Web/Controller/ActionController.php
@@ -370,17 +370,14 @@ class ActionController extends Zend_Controller_Action
     **/
     public function redirectNow($url)
     {
-        if (! $url instanceof Url) {
-            $url = Url::fromPath($url);
-        }
-        $url = preg_replace('~&amp;~', '&', $url);
+        $url = Url::fromPath(preg_replace('~&amp;~', '&', $url));
         if ($this->_request->isXmlHttpRequest()) {
             header('X-Icinga-Redirect: ' . rawurlencode($url));
             // $this->getResponse()->sendHeaders() ??
             // Session shutdown
             exit; // Really?
         } else {
-            $this->_helper->Redirector->gotoUrlAndExit($url);
+            $this->_helper->Redirector->gotoUrlAndExit($url->getRelativeUrl());
         }
         $this->isRedirect = true; // pretty useless right now
     }
