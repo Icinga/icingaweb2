89ae3713cd81b57d656b8b49809ad04747d1fa00
Marius Hein <marius.hein@netways.de>
2014-06-06 11:01:20 +0200

    Vagrant/finalize: Do not remount /vagrant/var/log
    If you do multiple provisioning calls mount point
    /vagrant/var/log is mounted multiple times.

--- .vagrant-puppet/manifests/finalize.sh
+++ .vagrant-puppet/manifests/finalize.sh
@@ -17,8 +17,13 @@ startServicesWithNonLSBCompliantExitStatusCodes () {
 }
 
 mountIcinga2webVarLog () {
-    # Remount /vagrant/var/log/ with appropriate permissions since the group apache is missing initially
-    mount -t vboxsf -o uid=`id -u vagrant`,gid=`id -g apache`,dmode=775,fmode=664 /vagrant/var/log/ /vagrant/var/log/
+    if ! $(/bin/mount | /bin/grep -q "/vagrant/var/log"); then
+        # Remount /vagrant/var/log/ with appropriate permissions since the group apache is missing initially
+        /bin/mount -t vboxsf -o \
+            uid=`id -u vagrant`,gid=`id -g apache`,dmode=775,fmode=664 \
+            /vagrant/var/log/ \
+            /vagrant/var/log/
+    fi
 }
 
 installJquery
