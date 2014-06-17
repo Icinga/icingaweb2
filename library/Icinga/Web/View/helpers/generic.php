d63d8fe451ee39bf7db79256c1f93ccef73a8dd9
Thomas Gelf <thomas@gelf.net>
2014-06-17 13:53:05 +0000

    view/helpers: add $this->widget()

--- library/Icinga/Web/View/helpers/generic.php
+++ library/Icinga/Web/View/helpers/generic.php
@@ -3,8 +3,13 @@
 namespace Icinga\Web\View;
 
 use Icinga\Authentication\Manager;
+use Icinga\Web\Widget;
 
 $this->addHelperFunction('auth', function () {
     return Manager::getInstance();
+});
+
+$this->addHelperFunction('widget', function ($name, $options) {
+    return Widget::create($name, $options);
 });
 
