45fa8ca2253e102555472c8e54e21eff68776422
Thomas Gelf <thomas@gelf.net>
2014-06-20 16:39:01 +0200

    CSS/forms: style them
    * login form style should only affect login form
    * accessibility: add labels on login page

--- application/forms/Authentication/LoginForm.php
+++ application/forms/Authentication/LoginForm.php
@@ -1,32 +1,4 @@
 <?php
-// @codeCoverageIgnoreStart
-// {{{ICINGA_LICENSE_HEADER}}}
-/**
- * This file is part of Icinga Web 2.
- *
- * Icinga Web 2 - Head for multiple monitoring backends.
- * Copyright (C) 2013 Icinga Development Team
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * @copyright  2013 Icinga Development Team <info@icinga.org>
- * @license    http://www.gnu.org/licenses/gpl-2.0.txt GPL, version 2
- * @author     Icinga Development Team <info@icinga.org>
- *
- */
-// {{{ICINGA_LICENSE_HEADER}}}
 
 namespace Icinga\Form\Authentication;
 
@@ -49,32 +21,23 @@ class LoginForm extends Form
     protected function create()
     {
         $this->setName('form_login');
-        $this->addElement(
-            'text',
-            'username',
-            array(
-                'required' => true,
-                'placeholder' => t('Username'),
-            )
-        );
+        $this->addElement('text', 'username', array(
+            'label'       => t('Username'),
+            'placeholder' => t('Please enter your username...'),
+            'required'    => true,
+        ));
 
-        $this->addElement(
-            'password',
-            'password',
-            array(
-                'placeholder'    => t('Password'),
-                'required' => true
-            )
-        );
-        $user = $this->getElement('username');
+        $this->addElement('password', 'password', array(
+            'label'       => t('Password'),
+            'placeholder' => t('...and your password'),
+            'required'    => true
+        ));
         // TODO: We need a place to intercept filled forms before rendering
         if (isset($_POST['username'])) {
             $this->getElement('password')->setAttrib('class', 'autofocus');
         } else {
-            $user->setAttrib('class', 'autofocus');
+            $this->getElement('username')->setAttrib('class', 'autofocus');
         }
         $this->setSubmitLabel('Login');
-
     }
 }
-// @codeCoverageIgnoreEnd
