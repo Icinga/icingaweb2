93cc24a93a87d5a2548486021e816884f1db6ff7
Eric Lippmann <eric.lippmann@netways.de>
2014-06-30 15:18:22 +0200

    doc module: Move `DocException' beneath `Exception' namespace
    There are more exceptions to come which also will be beneath the `Exception' namespace.
    
    refs #4820

--- modules/doc/library/Doc/Exception/DocException.php
+++ modules/doc/library/Doc/Exception/DocException.php
@@ -2,10 +2,13 @@
 // {{{ICINGA_LICENSE_HEADER}}}
 // {{{ICINGA_LICENSE_HEADER}}}
 
-namespace Icinga\Module\Doc;
+namespace Icinga\Module\Doc\Exception;
 
 use Exception;
 
+/**
+ * Exception thrown if an error in the documentation module occurs
+ */
 class DocException extends Exception
 {
 }
