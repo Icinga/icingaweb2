3cfdf4c9779391bec32ad829f99dff2f3622938d
Thomas Gelf <thomas@gelf.net>
2014-07-04 12:35:33 +0200

    Web\Response: add redirection function
    The response now provides redirectAndExit(). One more step to clean up
    our action controller. Please not that this is still unused right now.

--- library/Icinga/Web/Response.php
+++ library/Icinga/Web/Response.php
@@ -3,7 +3,23 @@
 namespace Icinga\Web;
 
 use Zend_Controller_Response_Http;
+use Icinga\Application\Icinga;
 
 class Response extends Zend_Controller_Response_Http
 {
+    public function redirectAndExit($url)
+    {
+        if (! $url instanceof Url) {
+            $url = Url::fromPath($url);
+        }
+        $url->getParams()->setSeparator('&');
+
+        if (Icinga::app()->getFrontController()->getRequest()->isXmlHttpRequest()) {
+            $this->setHeader('X-Icinga-Redirect', rawurlencode($url));
+        } else {
+            $this->setRedirect($url->getAbsoluteUrl());
+        }
+        $this->sendHeaders();
+        exit;
+    }
 }
