77f8a3d67dc0d49c213d3572c68c9cb0e98a80f5
Eric Lippmann <eric.lippmann@netways.de>
2014-07-28 18:55:54 +0200

    doc/DocEmptyException: Remove unnecessary line breaks
    refs #4820

--- modules/doc/library/Doc/Exception/DocEmptyException.php
+++ modules/doc/library/Doc/Exception/DocEmptyException.php
@@ -7,6 +7,4 @@ namespace Icinga\Module\Doc\Exception;
 /**
  * Exception thrown if a documentation directory is empty
  */
-class DocEmptyException extends DocException
-{
-}
+class DocEmptyException extends DocException {}
