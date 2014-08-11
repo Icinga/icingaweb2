be14844fa816bc1693c6cca029d1ac27ed8441b5
Johannes Meyer <johannes.meyer@netways.de>
2014-08-11 14:49:42 +0200

    Merge EditInstanceForm with CreateInstanceForm and rename it
    As with the backend form, a user might like to change the name of an
    instance. The new form and its actions are also adjusted to suit the new form interface.
    
    refs #5525

--- modules/monitoring/application/controllers/ConfigController.php
+++ modules/monitoring/application/controllers/ConfigController.php
@@ -9,8 +9,7 @@ use Icinga\Web\Controller\ModuleActionController;
 use Icinga\Web\Notification;
 use Icinga\Form\Config\ConfirmRemovalForm;
 use Icinga\Module\Monitoring\Form\Config\BackendForm;
-use Icinga\Module\Monitoring\Form\Config\Instance\EditInstanceForm;
-use Icinga\Module\Monitoring\Form\Config\Instance\CreateInstanceForm;
+use Icinga\Module\Monitoring\Form\Config\InstanceForm;
 use Icinga\Exception\NotReadableError;
 
 /**
@@ -95,6 +94,7 @@ class Monitoring_ConfigController extends ModuleActionController
                 $this->redirectNow('monitoring/config');
             } else {
                 $this->render('show-configuration');
+                return;
             }
         }
 
@@ -123,6 +123,7 @@ class Monitoring_ConfigController extends ModuleActionController
                 $this->redirectNow('monitoring/config');
             } else {
                 $this->render('show-configuration');
+                return;
             }
         }
 
@@ -135,26 +136,24 @@ class Monitoring_ConfigController extends ModuleActionController
     public function removeinstanceAction()
     {
         $instance = $this->getParam('instance');
-        if (!$this->isExistingInstance($instance)) {
-            $this->view->error = 'Unknown instance ' . $instance;
-            return;
+        $instancesConfig = $this->Config('instances')->toArray();
+        if (false === array_key_exists($instance, $instancesConfig)) {
+            // TODO: Should behave as in the app's config controller (Specific redirect to an error action)
+            Notification::error(sprintf($this->translate('Cannot remove "%s". Instance not found.'), $instance));
+            $this->redirectNow('monitoring/config');
         }
 
         $form = new ConfirmRemovalForm();
-        $form->setRequest($this->getRequest());
-        $form->setRemoveTarget('instance', $instance);
-
-        if ($form->isSubmittedAndValid()) {
-            $configArray = $this->Config('instances')->toArray();
-            unset($configArray[$instance]);
-
-            if ($this->writeConfiguration(new Zend_Config($configArray), 'instances')) {
-                Notification::success('Instance "' . $instance . '" Removed');
+        $request = $this->getRequest();
+        if ($request->isPost() && $form->isValid($request->getPost())) {
+            unset($instancesConfig[$instance]);
+            if ($this->writeConfiguration($instancesConfig, 'instances')) {
+                Notification::success(sprintf($this->translate('Instance "%s" successfully removed.'), $instance));
                 $this->redirectNow('monitoring/config');
             } else {
                 $this->render('show-configuration');
+                return;
             }
-            return;
         }
 
         $this->view->form = $form;
@@ -167,24 +166,36 @@ class Monitoring_ConfigController extends ModuleActionController
     public function editinstanceAction()
     {
         $instance = $this->getParam('instance');
-        if (!$this->isExistingInstance($instance)) {
-            $this->view->error = 'Unknown instance ' . htmlentities($instance);
-            return;
+        $instancesConfig = $this->Config('instances')->toArray();
+        if (false === array_key_exists($instance, $instancesConfig)) {
+            // TODO: Should behave as in the app's config controller (Specific redirect to an error action)
+            Notification::error(sprintf($this->translate('Cannot edit "%s". Instance not found.'), $instance));
+            $this->redirectNow('monitoring/config');
         }
-        $form = new EditInstanceForm();
-        $form->setInstanceConfiguration($this->Config('instances')->get($instance));
-        $form->setRequest($this->getRequest());
-        if ($form->isSubmittedAndValid()) {
-            $instanceConfig = $this->Config('instances')->toArray();
-            $instanceConfig[$instance] = $form->getConfig();
-            if ($this->writeConfiguration(new Zend_Config($instanceConfig), 'instances')) {
-                Notification::success('Instance Modified');
-                $this->redirectNow('monitoring/config');
-            } else {
-                $this->render('show-configuration');
-                return;
+
+        $form = new InstanceForm();
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                list($newName, $config) = $form->getInstanceConfig();
+
+                if ($newName !== $instance) {
+                    unset($instancesConfig[$instance]); // We can safely use unset as all values are part of the form
+                }
+
+                $instancesConfig[$newName] = $config;
+                if ($this->writeConfiguration($instancesConfig, 'instances')) {
+                    Notification::success(sprintf($this->translate('Instance "%s" successfully modified.'), $instance));
+                    $this->redirectNow('monitoring/config');
+                } else {
+                    $this->render('show-configuration');
+                    return;
+                }
             }
+        } else {
+            $form->setInstanceConfig($instance, $instancesConfig[$instance]);
         }
+
         $this->view->form = $form;
     }
 
@@ -193,26 +204,22 @@ class Monitoring_ConfigController extends ModuleActionController
      */
     public function createinstanceAction()
     {
-        $form = new CreateInstanceForm();
-        $form->setRequest($this->getRequest());
-        if ($form->isSubmittedAndValid()) {
-            $instanceConfig = $this->Config('instances');
-            if ($instanceConfig === null) {
-                $instanceConfig = array();
-            } else {
-                $instanceConfig = $instanceConfig->toArray();
-            }
-            $instanceConfig[$form->getInstanceName()] = $form->getConfig()->toArray();
-            if ($this->writeConfiguration(new Zend_Config($instanceConfig), 'instances')) {
-                Notification::success('Instance Creation Succeeded');
+        $form = new InstanceForm();
+        $request = $this->getRequest();
+        if ($request->isPost() && $form->isValid($request->getPost())) {
+            list($name, $config) = $form->getInstanceConfig();
+            $instancesConfig = $this->Config('instances')->toArray();
+            $instancesConfig[$name] = $config;
+            if ($this->writeConfiguration($instancesConfig, 'instances')) {
+                Notification::success(sprintf($this->translate('Instance "%s" created successfully.'), $name));
                 $this->redirectNow('monitoring/config');
             } else {
                 $this->render('show-configuration');
+                return;
             }
-            return;
         }
+
         $this->view->form = $form;
-        $this->render('editinstance');
     }
 
     /**
