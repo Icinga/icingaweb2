f6a9c67fa21ea78b289d965442976f99b8892319
Alexander Klimov <alexander.klimov@netways.de>
2014-08-28 13:08:42 +0200

    Clean up `icinga2::feature'
    refs #6842

--- .vagrant-puppet/modules/icinga2/manifests/feature.pp
+++ .vagrant-puppet/modules/icinga2/manifests/feature.pp
@@ -1,9 +1,11 @@
 define icinga2::feature ($feature = $title) {
+  include icinga2
+
   exec { "icinga2-feature-${feature}":
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
     unless => "readlink /etc/icinga2/features-enabled/${feature}.conf",
     command => "icinga2-enable-feature ${feature}",
-    require => [ Package['icinga2'] ],
+    require => Package['icinga2'],
     notify => Service['icinga2']
   }
 }
