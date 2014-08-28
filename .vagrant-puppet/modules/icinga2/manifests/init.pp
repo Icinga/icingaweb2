75ea24361db6ab1fc34a1501f287b16bd57da882
Alexander Klimov <alexander.klimov@netways.de>
2014-08-28 12:38:52 +0200

    Clean up module `icinga2'
    refs #6842

--- .vagrant-puppet/modules/icinga2/manifests/init.pp
+++ .vagrant-puppet/modules/icinga2/manifests/init.pp
@@ -1,36 +1,19 @@
 class icinga2 {
+  include icinga-packages
+
   service { 'icinga2':
     ensure  => running,
-    require => [
-    Package['icinga2'],
-    File['/etc/icinga2/features-enabled/ido-mysql.conf'],
-    File['/etc/icinga2/conf.d/test-config.conf'],
-    File['/etc/icinga2/conf.d/commands.conf']
-    ]
+    enable  => true,
+    require => Package['icinga2']
   }
 
   package { 'icinga2':
     ensure => latest,
-    require => Yumrepo['icinga2-repo'],
-    alias => 'icinga2'
-  }
-
-  package { 'icinga2-bin':
-    ensure => latest,
-    require => [ Yumrepo['icinga2-repo'], Package['icinga2'] ],
-    alias => 'icinga2-bin'
+    require => Class['icinga-packages'],
   }
 
   package { 'icinga2-doc':
     ensure => latest,
-    require => Yumrepo['icinga2-repo'],
-    alias => 'icinga2-doc'
-  }
-
-  package { 'icinga2-classicui-config':
-    ensure => latest,
-    before => Package["icinga-gui"],
-    require => [ Yumrepo['icinga2-repo'], Package['icinga2'] ],
-    notify => Service['apache'],
+    require => Class['icinga-packages'],
   }
 }
