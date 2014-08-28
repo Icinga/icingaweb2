cc4907bae6e58be2c18aaf12f29158d6ff8d8337
Alexander Klimov <alexander.klimov@netways.de>
2014-08-28 16:56:10 +0200

    icinga2-mysql: remove file '/etc/icinga2/features-enabled/ido-mysql.conf'
    refs #6842

--- .vagrant-puppet/modules/icinga2-mysql/manifests/init.pp
+++ .vagrant-puppet/modules/icinga2-mysql/manifests/init.pp
@@ -1,6 +1,10 @@
 class icinga2-mysql {
   include icinga-packages
-  include icinga2
+
+  package { 'icinga2-ido-mysql':
+    ensure => latest,
+    require => Class['icinga-packages'],
+  }
 
   mysql::database::populate { 'icinga2':
     username => 'icinga2',
@@ -10,27 +14,16 @@ class icinga2-mysql {
     require => Package['icinga2-ido-mysql'],
   }
 
-  icinga2::feature { 'ido-mysql':
-    require => Mysql::Database::Populate['icinga2'],
-  }
-
-  package { 'icinga2-ido-mysql':
-    ensure => latest,
-    require => Class['icinga-packages'],
-  }
-
   file { '/etc/icinga2/features-available/ido-mysql.conf':
     source  => 'puppet:///modules/icinga2-mysql/etc/icinga2/features-available/ido-mysql.conf',
     owner   => 'icinga',
     group   => 'icinga',
-    notify => Service['icinga2'],
   }
 
-  file { '/etc/icinga2/features-enabled/ido-mysql.conf':
-    ensure  => 'link',
-    target  => '/etc/icinga2/features-available/ido-mysql.conf',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['icinga2-ido-mysql'],
+  icinga2::feature { 'ido-mysql':
+    require => [
+      Mysql::Database::Populate['icinga2'],
+      File['/etc/icinga2/features-available/ido-mysql.conf']
+    ],
   }
 }
