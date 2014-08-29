249099348e87d966e9d1c4dd39ebb19b216c7dac
Eric Lippmann <eric.lippmann@netways.de>
2014-08-29 15:08:58 +0200

    monitoring/commands: Replace `SecureShell' with `RemoteCommandFile'
    `RemoteCommandFile' is configured via property setters instead of the too general `setEndpoint' function.
    The ssh command to be executed only has the option 'BatchMode' set to 'yes' as this is enough to disable
    interactive authentication methods. Further, all arguments become espaced.
    
    refs #6593

---
+++ modules/monitoring/library/Monitoring/Command/Transport/RemoteCommandFile.php
@@ -0,0 +1,188 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Module\Monitoring\Command\Transport;
+
+use LogicException;
+use Icinga\Logger\Logger;
+use Icinga\Module\Monitoring\Command\Exception\TransportException;
+use Icinga\Module\Monitoring\Command\IcingaCommand;
+
+/**
+ * A remote Icinga command file
+ *
+ * Key-based SSH login must be possible for the user to log in as on the remote host
+ */
+class RemoteCommandFile implements CommandTransportInterface
+{
+    /**
+     * Remote host
+     *
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * Port to connect to on the remote host
+     *
+     * @var int
+     */
+    protected $port = 22;
+
+    /**
+     * User to log in as on the remote host
+     *
+     * Defaults to current PHP process' user
+     *
+     * @var string
+     */
+    protected $user;
+
+    /**
+     * Path to the Icinga command file on the remote host
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Set the remote host
+     *
+     * @param   string $host
+     *
+     * @return  self
+     */
+    public function setHost($host)
+    {
+        $this->host = (string) $host;
+        return $this;
+    }
+
+    /**
+     * Get the remote host
+     *
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * Set the port to connect to on the remote host
+     *
+     * @param   int $port
+     *
+     * @return  self
+     */
+    public function setPort($port)
+    {
+        $this->port = (int) $port;
+        return $this;
+    }
+
+    /**
+     * Get the port to connect on the remote host
+     *
+     * @return int
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * Set the user to log in as on the remote host
+     *
+     * @param   string $user
+     *
+     * @return  self
+     */
+    public function setUser($user)
+    {
+        $this->user = (string) $user;
+        return $this;
+    }
+
+    /**
+     * Get the user to log in as on the remote host
+     *
+     * Defaults to current PHP process' user
+     *
+     * @return string|null
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the path to the Icinga command file on the remote host
+     *
+     * @param   string $path
+     *
+     * @return  self
+     */
+    public function setPath($path)
+    {
+        $this->path = (string) $path;
+        return $this;
+    }
+
+    /**
+     * Get the path to the Icinga command file on the remote host
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Write the command to the Icinga command file on the remote host
+     *
+     * @param   IcingaCommand $command
+     *
+     * @throws  LogicException
+     * @throws  TransportException
+     */
+    public function send(IcingaCommand $command)
+    {
+        if (! isset($this->path)) {
+            throw new LogicException;
+        }
+        Logger::debug(
+            sprintf(
+                mt('monitoring', 'Sending external Icinga command "%s" to the remote command file "%s:%u%s"'),
+                $command,
+                $this->host,
+                $this->port,
+                $this->path
+            )
+        );
+        $ssh = sprintf('ssh -o BatchMode=yes -p %u', $this->port);  // -o BatchMode=yes for disabling interactive
+                                                                    // authentication methods
+        if (isset($this->user)) {
+            $ssh .= sprintf(' -l %s', escapeshellarg($this->user));
+        }
+        $ssh .= sprintf(
+            ' %s "echo %s > %s" 2>&1',  // Redirect stderr to stdout
+            escapeshellarg($this->host),
+            escapeshellarg($command),
+            escapeshellarg($this->path)
+        );
+        exec($ssh, $output, $status);
+        if ($status !== 0) {
+            throw new TransportException(
+                mt(
+                    'monitoring',
+                    'Can\'t send external Icinga command "%s": %s'
+                ),
+                $ssh,
+                implode(' ', $output)
+            );
+        }
+    }
+}
