0edd14559c299a5b7b34d2d4cf31079a2cefc1e8
Eric Lippmann <eric.lippmann@netways.de>
2014-09-01 15:18:40 +0200

    monitoring/commands: Add (ENABLE|DISABLE)_PASSIVE_SERVICE_CHECKS commands
    `TogglePassiveServiceChecks' is the command object for enabling/disabling passive service checks on an Icinga instance.
    
    refs #6593

---
+++ modules/monitoring/library/Monitoring/Command/Instance/TogglePassiveServiceChecks.php
@@ -0,0 +1,22 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Module\Monitoring\Command\Instance;
+
+use Icinga\Module\Monitoring\Command\Common\ToggleFeature;
+
+/**
+ * Enable/disable passive service checks on an Icinga instance
+ */
+class TogglePassiveServiceChecks extends ToggleFeature
+{
+    /**
+     * (non-PHPDoc)
+     * @see \Icinga\Module\Monitoring\Command\IcingaCommand::getCommandString() For the method documentation.
+     */
+    public function getCommandString()
+    {
+        return $this->enable === true ? 'ENABLE_PASSIVE_SVC_CHECKS' : 'DISABLE_PASSIVE_SVC_CHECKS';
+    }
+}
