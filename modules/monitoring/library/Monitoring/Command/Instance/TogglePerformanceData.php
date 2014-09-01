920c0b9c6d60b348e995f813e813f7043ec835ef
Eric Lippmann <eric.lippmann@netways.de>
2014-09-01 15:20:14 +0200

    monitoring/commands: Add (ENABLE|DISABLE)_PERFORMANCE_DATA commands
    `TogglePerformanceData' is the command object for enabling/disabling the processing of host and service performance data on an Icinga instance.
    
    refs #6593

---
+++ modules/monitoring/library/Monitoring/Command/Instance/TogglePerformanceData.php
@@ -0,0 +1,22 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Module\Monitoring\Command\Instance;
+
+use Icinga\Module\Monitoring\Command\Common\ToggleFeature;
+
+/**
+ * Enable/disable the processing of host and service performance data on an Icinga instance
+ */
+class TogglePerformanceData extends ToggleFeature
+{
+    /**
+     * (non-PHPDoc)
+     * @see \Icinga\Module\Monitoring\Command\IcingaCommand::getCommandString() For the method documentation.
+     */
+    public function getCommandString()
+    {
+        return $this->enable === true ? 'ENABLE_PERFORMANCE_DATA' : 'DISABLE_PERFORMANCE_DATA';
+    }
+}
