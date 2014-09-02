e8dfbe688773cccf0a66f3d56ed3a41934ef6317
Alexander Klimov <alexander.klimov@netways.de>
2014-09-02 12:12:43 +0200

    Puppet style guide: Avoid double quoted strings containing no variables
    refs #6842

--- .vagrant-puppet/modules/icinga_packages/manifests/init.pp
+++ .vagrant-puppet/modules/icinga_packages/manifests/init.pp
@@ -1,6 +1,6 @@
 class icinga_packages {
   yumrepo { 'icinga_packages':
-    baseurl   => "http://packages.icinga.org/epel/6/snapshot/",
+    baseurl   => 'http://packages.icinga.org/epel/6/snapshot/',
     enabled   => '1',
     gpgcheck  => '1',
     gpgkey    => 'http://packages.icinga.org/icinga.key',
