65f305159d340618b0bf6f906753f29db4daa7e3
Alexander Klimov <alexander.klimov@netways.de>
2014-09-02 16:18:06 +0200

    Module 'mysql': add documentation
    refs #6842

--- .vagrant-puppet/modules/mysql/manifests/database/create.pp
+++ .vagrant-puppet/modules/mysql/manifests/database/create.pp
@@ -1,3 +1,25 @@
+# Define: mysql::database::create
+#
+#   Create a MySQL database
+#
+# Parameters:
+#
+#   [*username*]   - name of the user the database belongs to
+#   [*password*]   - password of the user the database belongs to
+#   [*privileges*] - privileges of the user the database belongs to
+#
+# Requires:
+#
+#   mysql
+#
+# Sample Usage:
+#
+# mysql::database::create { 'icinga2':
+#   username   => 'icinga2',
+#   password   => 'icinga2',
+#   privileges => 'SELECT,INSERT,UPDATE,DELETE',
+# }
+#
 define mysql::database::create ($username, $password, $privileges) {
   include mysql
 
--- .vagrant-puppet/modules/mysql/manifests/database/populate.pp
+++ .vagrant-puppet/modules/mysql/manifests/database/populate.pp
@@ -1,3 +1,27 @@
+# Define: mysql::database::populate
+#
+#   Create and populate a MySQL database
+#
+# Parameters:
+#
+#   [*username*]   - name of the user the database belongs to
+#   [*password*]   - password of the user the database belongs to
+#   [*privileges*] - privileges of the user the database belongs to
+#   [*schemafile*] - file with the schema for the database
+#
+# Requires:
+#
+#   mysql::database::create
+#
+# Sample Usage:
+#
+# mysql::database::populate { 'icinga2':
+#   username   => 'icinga2',
+#   password   => 'icinga2',
+#   privileges => 'SELECT,INSERT,UPDATE,DELETE',
+#   schemafile => '/usr/share/icinga2-ido-mysql/schema/mysql.sql',
+# }
+#
 define mysql::database::populate ($username, $password, $privileges, $schemafile) {
   Exec { path => '/usr/bin' }
 
