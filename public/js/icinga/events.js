e5be8d47ef388bdd405200a4ff334051f7e869c8
Matthias Jentsch <matthias.jentsch@netways.de>
2014-09-04 09:53:57 +0200

    Fix SVG bound calculation for tooltips in Firefox

--- public/js/icinga/events.js
+++ public/js/icinga/events.js
@@ -118,7 +118,7 @@
                 var $el = $(this);
                 $el.attr('title', $el.attr('title-rich') || $el.attr('title'));
             });
-            $('svg rect.chart-data[title]', el).tipsy({ gravity: 'e', html: true });
+            $('svg rect.chart-data[title]', el).tipsy({ gravity: 'se', html: true });
             $('.historycolorgrid a[title]', el).tipsy({ gravity: 's', offset: 2 });
             $('img.icon[title]', el).tipsy({ gravity: $.fn.tipsy.autoNS, offset: 2 });
             $('[title]', el).tipsy({ gravity: $.fn.tipsy.autoNS, delayIn: 500 });
