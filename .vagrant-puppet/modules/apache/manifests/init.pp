5e3f60d42b089dd49fa4946cadb6f6c4b1b5fd00
Alexander Klimov <alexander.klimov@netways.de>
2014-09-10 11:38:10 +0200

    Module `apache': add user `apache'
    refs #6842

--- .vagrant-puppet/modules/apache/manifests/init.pp
+++ .vagrant-puppet/modules/apache/manifests/init.pp
@@ -13,20 +13,28 @@
 #   include apache
 #
 class apache {
-
   $apache = $::operatingsystem ? {
     /(Debian|Ubuntu)/ => 'apache2',
     /(RedHat|CentOS|Fedora)/ => 'httpd'
   }
 
+  $user = $::operatingsystem ? {
+    /(Debian|Ubuntu)/ => 'www-data',
+    /(RedHat|CentOS|Fedora)/ => 'apache'
+  }
+
   package { $apache:
     ensure => installed,
-    alias  => 'apache'
+    alias  => 'apache',
   }
 
   service { $apache:
     ensure  => running,
     alias   => 'apache',
-    require => Package['apache']
+    require => Package['apache'],
+  }
+
+  user { $user:
+    alias => 'apache',
   }
 }
