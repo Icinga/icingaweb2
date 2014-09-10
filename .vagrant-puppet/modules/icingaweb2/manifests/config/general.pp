d61e883422f536f2a53e61c815906a74226d1d11
Alexander Klimov <alexander.klimov@netways.de>
2014-09-10 11:45:57 +0200

    icingaweb2::config::general: require module `apache'
    refs #6842

--- .vagrant-puppet/modules/icingaweb2/manifests/config/general.pp
+++ .vagrant-puppet/modules/icingaweb2/manifests/config/general.pp
@@ -1,9 +1,12 @@
 define icingaweb2::config::general ($source, $replace = true) {
+  include apache
+
   $path = "/etc/icingaweb/${name}.ini"
   file { $path:
     source  => "${source}${path}",
     owner   => 'apache',
     group   => 'apache',
     replace => $replace,
+    require => Class['apache'],
   }
 }
