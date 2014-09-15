d7a0be72e208af6a5653c869db587ed11d8a438f
Alexander Klimov <alexander.klimov@netways.de>
2014-09-15 12:26:06 +0200

    icingaweb2::config::general: require File[icingaweb2cfgDir] (from Class[icingaweb2])
    refs #6842

--- .vagrant-puppet/modules/icingaweb2/manifests/config/general.pp
+++ .vagrant-puppet/modules/icingaweb2/manifests/config/general.pp
@@ -1,11 +1,13 @@
 define icingaweb2::config::general ($source, $replace = true) {
   include apache
+  include icingaweb2
 
   $path = "/etc/icingaweb/${name}.ini"
 
-  parent_dirs { $path: }
-
-  file { $path:
+  parent_dirs { $path:
+    require => File['icingaweb2cfgDir'],
+  }
+  -> file { $path:
     source  => "${source}/${name}.ini",
     owner   => 'apache',
     group   => 'apache',
