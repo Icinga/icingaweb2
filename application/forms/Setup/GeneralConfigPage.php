b6b7814e8bc37e19b61f09529b7d9244d3530385
Johannes Meyer <johannes.meyer@netways.de>
2014-09-29 12:29:13 +0200

    Add GeneralConfig page
    refs #7163

---
+++ application/forms/Setup/GeneralConfigPage.php
@@ -0,0 +1,35 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Form\Setup;
+
+use Icinga\Web\Form;
+use Icinga\Form\Config\General\LoggingConfigForm;
+use Icinga\Form\Config\General\ApplicationConfigForm;
+
+/**
+ * Wizard page to define the application and logging configuration
+ */
+class GeneralConfigPage extends Form
+{
+    /**
+     * Initialize this page
+     */
+    public function init()
+    {
+        $this->setName('setup_application_config');
+    }
+
+    /**
+     * @see Form::createElements()
+     */
+    public function createElements(array $formData)
+    {
+        $appForm = new ApplicationConfigForm();
+        $this->addElement($appForm->createElements($formData)->getElement('global_modulePath'));
+
+        $loggingForm = new LoggingConfigForm();
+        $this->addElements($loggingForm->createElements($formData)->getElements());
+    }
+}
