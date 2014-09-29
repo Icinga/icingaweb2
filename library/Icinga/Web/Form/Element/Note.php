ab8436dea81c658e4b9ca6e5dcfae3c167f41f98
Johannes Meyer <johannes.meyer@netways.de>
2014-09-29 16:12:22 +0200

    Fix Icinga\Web\Form\Element\Note using its default decorators

--- library/Icinga/Web/Form/Element/Note.php
+++ library/Icinga/Web/Form/Element/Note.php
@@ -4,13 +4,24 @@
 
 namespace Icinga\Web\Form\Element;
 
-use Zend_Form_Element_Xhtml;
+use Zend_Form_Element;
 
 /**
  * Implements note element for Zend forms
  */
-class Note extends Zend_Form_Element_Xhtml
+class Note extends Zend_Form_Element
 {
+    /**
+     * Disable default decorators
+     *
+     * \Icinga\Web\Form sets default decorators for elements.
+     *
+     * @var bool
+     *
+     * @see \Icinga\Web\Form::__construct() For default element decorators.
+     */
+    protected $_disableLoadDefaultDecorators = true;
+
     /**
      * Name of the view helper
      *
