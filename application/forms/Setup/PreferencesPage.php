61f0ce65e0e88ece58806eb7db5d1654bde1df83
Johannes Meyer <johannes.meyer@netways.de>
2014-09-29 12:27:26 +0200

    Add preferences page
    refs #7163

---
+++ application/forms/Setup/PreferencesPage.php
@@ -0,0 +1,70 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Form\Setup;
+
+use Icinga\Web\Form;
+use Icinga\Application\Platform;
+
+/**
+ * Wizard page to choose a preference backend
+ */
+class PreferencesPage extends Form
+{
+    /**
+     * Initialize this page
+     */
+    public function init()
+    {
+        $this->setName('setup_preferences_type');
+    }
+
+    /**
+     * Pre-select "db" as preference backend and add a hint to the select element
+     *
+     * @return  self
+     */
+    public function showDatabaseNote()
+    {
+        $this->getElement('type')
+            ->setValue('db')
+            ->setDescription(
+                t('Note that choosing "Database" causes Icinga Web 2 to use the same database as for authentication.')
+            );
+        return $this;
+    }
+
+    /**
+     * @see Form::createElements()
+     */
+    public function createElements(array $formData)
+    {
+        $this->addElement(
+            'note',
+            'description',
+            array(
+                'value' => t('Please choose how Icinga Web 2 should store user preferences.')
+            )
+        );
+
+        $storageTypes = array();
+        $storageTypes['ini'] = t('File System (INI Files)');
+        if (Platform::extensionLoaded('pdo') && (Platform::zendClassExists('Zend_Db_Adapter_Pdo_Mysql')
+            || Platform::zendClassExists('Zend_Db_Adapter_Pdo_Pgsql')))
+        {
+            $storageTypes['db'] = t('Database');
+        }
+        $storageTypes['null'] = t('Don\'t Store Preferences');
+
+        $this->addElement(
+            'select',
+            'type',
+            array(
+                'required'      => true,
+                'label'         => t('User Preference Storage Type'),
+                'multiOptions'  => $storageTypes
+            )
+        );
+    }
+}
