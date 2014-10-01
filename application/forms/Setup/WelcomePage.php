3a11182bf071540a740368bb19f2cefe8dfe8904
Johannes Meyer <johannes.meyer@netways.de>
2014-10-01 10:33:06 +0200

    Fix welcome page name not conforming the "standard"
    refs #7163

--- application/forms/Setup/WelcomePage.php
+++ application/forms/Setup/WelcomePage.php
@@ -19,7 +19,7 @@ class WelcomePage extends Form
      */
     public function init()
     {
-        $this->setName('form_setup_welcome');
+        $this->setName('setup_welcome');
     }
 
     /**
