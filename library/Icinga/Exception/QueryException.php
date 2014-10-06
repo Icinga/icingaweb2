36681bb55ac226bc871730df9882897ffe82cd24
Alexander Fuhr <alexander.fuhr@netways.de>
2014-10-06 11:30:48 +0200

    Add new QueryException

---
+++ library/Icinga/Exception/QueryException.php
@@ -0,0 +1,12 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Exception;
+
+/**
+ * Exception thrown if a query contains invalid parameters
+ */
+class QueryException extends IcingaException
+{
+}
