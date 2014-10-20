c2a3770869f7e8c987070971d8a8dd8499eb431b
Eric Lippmann <eric.lippmann@netways.de>
2014-10-20 16:32:31 +0200

    schema/mysql: Add user and user_preference tables

--- etc/schema/mysql.schema.sql
+++ etc/schema/mysql.schema.sql
@@ -11,5 +11,25 @@ CREATE TABLE `icingaweb_group_membership`(
   `username`   varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `ctime`      timestamp NULL DEFAULT NULL,
   `mtime`      timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`group_name`,`username`)
+  PRIMARY KEY (`group_name`,`username`),
+  CONSTRAINT `fk_icingaweb_group_membership_icingaweb_group` FOREIGN KEY (`group_name`)
+    REFERENCES `icingaweb_group` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `icingaweb_user`(
+  `name`          varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `active`        tinyint(1) NOT NULL,
+  `password_hash` varbinary(255) NOT NULL,
+  `ctime`         timestamp NULL DEFAULT NULL,
+  `mtime`         timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `icingaweb_user_preference`(
+  `username` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `name`     varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `value`    varchar(255) NOT NULL,
+  `ctime`    timestamp NULL DEFAULT NULL,
+  `mtime`    timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
