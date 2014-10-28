b5468a122dbf63d24f9aa898e02c4d5b6539f734
Matthias Jentsch <matthias.jentsch@netways.de>
2014-10-28 17:32:49 +0100

    Explain token generation on the welcome page
    Add token generation manual to the welcome page. Display example code
    containing the current configuration dir, to generate tokens.
    
    fixes #7408

--- application/forms/Setup/WelcomePage.php
+++ application/forms/Setup/WelcomePage.php
@@ -15,11 +15,20 @@ use Icinga\Web\Form\Validator\TokenValidator;
 class WelcomePage extends Form
 {
     /**
+     * The configuration directory displayed to the user in the "generate security token" infobox
+     *
+     * @var string
+     */
+    public $configDir = '/etc/icingaweb';
+
+    /**
      * Initialize this page
      */
     public function init()
     {
         $this->setName('setup_welcome');
+        $this->setViewScript('form/setup-welcome.phtml');
+        $this->configDir = preg_replace('_/$_', '', Icinga::app()->getConfigDir());
     }
 
     /**
@@ -28,104 +37,14 @@ class WelcomePage extends Form
     public function createElements(array $formData)
     {
         $this->addElement(
-            new Note(
-                'welcome',
-                array(
-                    'value'         => t('Welcome to the installation of Icinga Web 2!'),
-                    'decorators'    => array(
-                        'ViewHelper',
-                        array('HtmlTag', array('tag' => 'h2'))
-                    )
-                )
-            )
-        );
-        $this->addElement( // TODO: Remove this once we release the first public version
-            new Note(
-                'wip',
-                array(
-                    'value'         => 'Icinga Web 2 is still in development and not meant for production deployment.'
-                    . ' Watch the <a href="https://dev.icinga.org/projects/icingaweb2/roadmap">development roadmap</a>'
-                    . ' and <a href="https://www.icinga.org/">Icinga website</a> for release schedule updates!',
-                    'decorators'    => array(
-                        'ViewHelper',
-                        array(
-                            'HtmlTag',
-                            array(
-                                'tag'   => 'div',
-                                'style' => 'border:1px solid #777;border-radius:1em;background-color:beige;'
-                                . 'padding:1em;margin-bottom:1em;display:inline-block;'
-                            )
-                        )
-                    )
-                )
-            )
-        );
-        $this->addElement(
-            new Note(
-                'description',
-                array(
-                    'value' => sprintf(
-                        t(
-                            'Icinga Web 2 is the next generation monitoring web interface,'
-                            . ' framework and CLI tool developed by the %s.'
-                        ),
-                        '<a href="https://www.icinga.org/community/team/">' . t('Icinga Project') . '</a>'
-                    )
-                )
-            )
-        );
-        $this->addElement(
-            new Note(
-                'facts',
-                array(
-                    'value' => t(
-                        'Responsive and fast, rewritten from scratch supporting multiple backends and'
-                        . ' providing a CLI tool. Compatible with Icinga Core 2.x and 1.x.'
-                    )
-                )
-            )
-        );
-        $this->addElement(
-            new Note(
-                'insights',
-                array(
-                    'value' => sprintf(
-                        t('Check the Icinga website for some %s.', 'setup.welcome.screenshots'),
-                        '<a href="https://www.icinga.org/icinga/screenshots/icinga-web-2/">'
-                        . t('insights', 'setup.welcome.screenshots.label') . '</a>'
-                    )
-                )
-            )
-        );
-        $this->addDisplayGroup(
-            array('description', 'facts', 'insights'),
-            'info',
-            array(
-                'decorators' => array(
-                    'FormElements',
-                    array('HtmlTag', array('tag' => 'div', 'class' => 'info'))
-                )
-            )
-        );
-        $this->addElement(
-            new Note(
-                'purpose',
-                array(
-                    'value' => t(
-                        'This wizard will guide you through the installation of Icinga Web 2.'
-                        . ' Once completed and successfully finished you are able to log in '
-                        . 'and to explore all the new and stunning features!'
-                    )
-                )
-            )
-        );
-        $this->addElement(
             'text',
             'token',
             array(
                 'required'      => true,
                 'label'         => t('Setup Token'),
-                'description'   => t('Please enter the setup token you\'ve created earlier by using the icingacli'),
+                'description'   => t(
+                    'For security reasons, we need to check that you are permitted to execute this setup. Please provide the token from the file "setup.token".'
+                ),
                 'validators'    => array(new TokenValidator(Icinga::app()->getConfigDir() . '/setup.token'))
             )
         );
