0992f6cc15d6312291328d2caf8be89b77fc90ec
Thomas Gelf <thomas@gelf.net>
2014-11-11 21:50:49 +0100

    Monitoring/MenuRenderers: unify logic, perftuning
    This reduces duplicate code, query will be fired only once right now.
    
    fixes #7554

--- library/Icinga/Web/Menu/UnhandledHostMenuItemRenderer.php
+++ library/Icinga/Web/Menu/UnhandledHostMenuItemRenderer.php
@@ -1,38 +1,12 @@
 <?php
-// {{{ICINGA_LICENSE_HEADER}}}
-// {{{ICINGA_LICENSE_HEADER}}}
 
 namespace Icinga\Web\Menu;
 
-use Icinga\Module\Monitoring\Backend;
 use Icinga\Web\Menu;
-use Icinga\Web\Url;
 
-class UnhandledHostMenuItemRenderer implements MenuItemRenderer {
-
-    public function render(Menu $menu)
-    {
-        $statusSummary = Backend::createBackend()
-            ->select()->from(
-                'statusSummary',
-                array(
-                    'hosts_down_unhandled'
-                )
-            )->getQuery()->fetchRow();
-        $badge = '';
-        if ($statusSummary->hosts_down_unhandled) {
-            $badge = sprintf(
-                '<div title="%s" class="badge-container"><span class="badge badge-critical">%s</span></div>',
-                t(sprintf('%d unhandled host problems', $statusSummary->hosts_down_unhandled)),
-                $statusSummary->hosts_down_unhandled
-            );
-        }
-        return sprintf(
-            '<a href="%s">%s%s %s</a>',
-            $menu->getUrl() ?: '#',
-            $menu->getIcon() ? '<img src="' . Url::fromPath($menu->getIcon()) . '" class="icon" /> ' : '',
-            htmlspecialchars($menu->getTitle()),
-            $badge
-        );
-    }
+class UnhandledHostMenuItemRenderer extends MonitoringMenuItemRenderer
+{
+    protected $columns = array(
+        'hosts_down_unhandled',
+    );
 }
--- library/Icinga/Web/Menu/UnhandledServiceMenuItemRenderer.php
+++ library/Icinga/Web/Menu/UnhandledServiceMenuItemRenderer.php
@@ -1,38 +1,12 @@
 <?php
-// {{{ICINGA_LICENSE_HEADER}}}
-// {{{ICINGA_LICENSE_HEADER}}}
 
 namespace Icinga\Web\Menu;
 
-use Icinga\Module\Monitoring\Backend;
 use Icinga\Web\Menu;
-use Icinga\Web\Url;
 
-class UnhandledServiceMenuItemRenderer implements MenuItemRenderer {
-
-    public function render(Menu $menu)
-    {
-        $statusSummary = Backend::createBackend()
-            ->select()->from(
-                'statusSummary',
-                array(
-                    'services_critical_unhandled'
-                )
-            )->getQuery()->fetchRow();
-        $badge = '';
-        if ($statusSummary->services_critical_unhandled) {
-            $badge = sprintf(
-                '<div title="%s" class="badge-container"><span class="badge badge-critical">%s</span></div>',
-                t(sprintf('%d unhandled service problems', $statusSummary->services_critical_unhandled)),
-                $statusSummary->services_critical_unhandled
-            );
-        }
-        return sprintf(
-            '<a href="%s">%s%s %s</a>',
-            $menu->getUrl() ?: '#',
-            $menu->getIcon() ? '<img src="' . Url::fromPath($menu->getIcon()) . '" class="icon" /> ' : '',
-            htmlspecialchars($menu->getTitle()),
-            $badge
-        );
-    }
+class UnhandledServiceMenuItemRenderer extends MonitoringMenuItemRenderer
+{
+    protected $columns = array(
+        'services_critical_unhandled'
+    );
 }
