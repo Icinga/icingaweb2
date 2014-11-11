32d483f69f4675bea1462a4a5c65c0ec2ad8586b
Eric Lippmann <eric.lippmann@netways.de>
2014-11-11 15:22:21 +0100

    Level up index.php
    This is the preparation for having the Icinga library in PHP's include path.

--- public/index.php
+++ public/index.php
@@ -2,4 +2,12 @@
 // {{{ICINGA_LICENSE_HEADER}}}
 // {{{ICINGA_LICENSE_HEADER}}}
 
-require_once dirname(__DIR__). '/library/Icinga/Application/webrouter.php';
+define('ICINGAWEB_BASEDIR', dirname(__DIR__));
+// ICINGAWEB_BASEDIR is the parent folder for at least application, bin, modules and public
+
+
+if (! @include_once ICINGAWEB_BASEDIR . '/library/Icinga/Application/webrouter.php') {
+    // If the Icinga library wasn't found under ICINGAWEB_BASEDIR, require that the Icinga library is found in PHP's
+    // include path which is the case if Icinga Web 2 is installed via packages
+    require_once 'Icinga/Application/webrouter.php';
+}
