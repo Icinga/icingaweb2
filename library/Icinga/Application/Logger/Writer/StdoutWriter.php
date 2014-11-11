f347a7498219aa00ebcf87e78e50b114367a2dac
Thomas Gelf <thomas@gelf.net>
2014-11-11 19:40:59 +0100

    Logger\StdoutWriter: initial implementation
    This log writer writes to STDOUT and uses ANSI colors on real terminals
    
    fixes #7636

---
+++ library/Icinga/Application/Logger/Writer/StdoutWriter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Icinga\Application\Logger\Writer;
+
+use Icinga\Cli\Screen;
+use Icinga\Application\Logger\Logger;
+use Icinga\Application\Logger\LogWriter;
+use Zend_Config;
+
+/**
+ * Class to write log messages to STDOUT
+ */
+class StdoutWriter extends LogWriter
+{
+    protected $screen;
+
+    protected function screen()
+    {
+        if ($this->screen === null) {
+            $this->screen = Screen::instance();
+        }
+        return $this->screen;
+    }
+
+    /**
+     * Log a message with the given severity
+     *
+     * @param   int     $severity   The severity to use
+     * @param   string  $message    The message to log
+     */
+    public function log($severity, $message)
+    {
+        $color = 'black';
+        switch ($severity) {
+            case Logger::$ERROR:
+                $color = 'red';
+                break;
+            case Logger::$WARNING:
+                $color = 'orange';
+                break;
+            case Logger::$INFO:
+                $color = 'green';
+                break;
+            case Logger::$DEBUG:
+                $color = 'blue';
+                break;
+        }
+        file_put_contents('php://stderr', $this->screen()->colorize($message, $color) . "\n");
+    }
+}
