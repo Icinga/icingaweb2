8b1419bb04c20feb9fb7650d73799f61b013396c
Eric Lippmann <eric.lippmann@netways.de>
2014-11-12 15:41:04 +0100

    Do not try to detect the library path in index.php
    Packagers will use a custom index.php.

--- public/index.php
+++ public/index.php
@@ -2,8 +2,4 @@
 // {{{ICINGA_LICENSE_HEADER}}}
 // {{{ICINGA_LICENSE_HEADER}}}
 
-if (! @include_once dirname(__DIR__) . '/library/Icinga/Application/webrouter.php') {
-    // If the Icinga library wasn't found under ICINGAWEB_BASEDIR, require that the Icinga library is found in PHP's
-    // include path which is the case if Icinga Web 2 is installed via packages
-    require_once 'Icinga/Application/webrouter.php';
-}
+require_once dirname(__DIR__) . '/library/Icinga/Application/webrouter.php';
