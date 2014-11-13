28c239c6fd6d5ce28386a34f96d0d14b8af5e8ef
Thomas Gelf <thomas@gelf.net>
2014-11-13 15:42:25 +0100

    Web\Controller: initial commit
    * This is the controller all modules should inherit from.
    * We will flip code with the ModuleActionController as soon as a couple
      of pending feature branches are merged back to the master.

---
+++ library/Icinga/Web/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Icinga\Web;
+
+use Icinga\Web\Controller\ModuleActionController;
+
+/**
+ * This is the controller all modules should inherit from
+ * We will flip code with the ModuleActionController as soon as a couple
+ * of pending feature branches are merged back to the master.
+ */
+class Controller extends ModuleActionController
+{
+}
