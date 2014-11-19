2c143ff348b0e34d43686e932c4ac790570f9788
Matthias Jentsch <matthias.jentsch@netways.de>
2014-11-19 11:03:06 +0100

    Fix alert summary page layout

--- modules/monitoring/application/controllers/AlertsummaryController.php
+++ modules/monitoring/application/controllers/AlertsummaryController.php
@@ -64,7 +64,7 @@ class Monitoring_AlertsummaryController extends Controller
      */
     public function indexAction()
     {
-        $this->addTitleTab('alertsummary');
+        $this->addTitleTab('alertsummary', t('Alert Summary'));
         $this->view->intervalBox = $this->createIntervalBox();
         $this->view->recentAlerts = $this->createRecentAlerts();
         $this->view->interval = $this->getInterval();
