31a49a7c95926c3fe7036e5b43459a54c4c08028
Alexander Klimov <alexander.klimov@netways.de>
2014-11-25 15:24:32 +0100

    Don't start ido2db-*sql

--- .vagrant-puppet/manifests/finalize.sh
+++ .vagrant-puppet/manifests/finalize.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-startServicesWithNonLSBCompliantExitStatusCodes () {
-    # Unfortunately the ido2db init script is not LSB compliant and hence not started via puppet
-    service ido2db-mysql start || true
-    service ido2db-pgsql start || true
-}
-
 mountIcinga2webVarLog () {
     if ! $(/bin/mount | /bin/grep -q "/vagrant/var/log"); then
         # Remount /vagrant/var/log/ with appropriate permissions since the group apache is missing initially
@@ -18,7 +12,6 @@ mountIcinga2webVarLog () {
     fi
 }
 
-startServicesWithNonLSBCompliantExitStatusCodes
 mountIcinga2webVarLog
 
 exit 0
