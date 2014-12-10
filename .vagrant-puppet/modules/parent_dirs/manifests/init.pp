cccab1d3e9186f966176f5e4110d20d2dbe22443
Eric Lippmann <eric.lippmann@netways.de>
2014-12-10 15:17:37 +0100

    puppet: Add note to parent_dirs that it has to removed

--- .vagrant-puppet/modules/parent_dirs/manifests/init.pp
+++ .vagrant-puppet/modules/parent_dirs/manifests/init.pp
@@ -1,3 +1,4 @@
+# TODO(el): Remove this. It's always executed and hackish
 define parent_dirs ($user = 'root') {
   exec { "parent_dirs-${name}":
     command => "mkdir -p \"\$(dirname \"\$(readlink -m '${name}')\")\"",
