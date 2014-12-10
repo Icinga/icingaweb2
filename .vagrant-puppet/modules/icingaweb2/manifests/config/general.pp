513fbe6461cd5bf8e4f4f9f341ebd098cf35d2d5
Eric Lippmann <eric.lippmann@netways.de>
2014-12-10 15:16:19 +0100

    puppet: Let icingaweb2::config::general() use the config path defined by hiera

--- .vagrant-puppet/modules/icingaweb2/manifests/config/general.pp
+++ .vagrant-puppet/modules/icingaweb2/manifests/config/general.pp
@@ -1,21 +1,15 @@
-define icingaweb2::config::general ($source, $replace = true) {
-  include apache
-  include icingaweb2
+define icingaweb2::config::general (
+  $source,
+  $config   = hiera('icingaweb2::config'),
+  $replace  = true
+) {
+  include icingaweb2::config
 
-  $path = "/etc/icingaweb/${name}.ini"
-
-  parent_dirs { $path:
-    user    => 'apache',
-    require => [
-      Class['apache'],
-      File['icingaweb2cfgDir']
-    ],
-  }
-  -> file { $path:
-    source  => "${source}/${name}.ini",
-    owner   => 'apache',
-    group   => 'apache',
-    replace => $replace,
-    require => Class['apache'],
+  file { "${config}/${name}.ini":
+     source  => "${source}/${name}.ini",
+     owner   => 'root',
+     group   => 'icingaweb',
+     mode    => 0660,
+     replace => $replace,
   }
 }
