37ad4f7623e610a3e26f12b5f7625eb489e8e6cb
Eric Lippmann <eric.lippmann@netways.de>
2014-12-15 12:11:41 +0100

    puppet: Add /usr/local/bin to PATH
    refs #6842

--- .puppet/manifests/site.pp
+++ .puppet/manifests/site.pp
@@ -1,5 +1,8 @@
 node 'localhost' {
-    include icinga2_dev
-    include icingaweb2_dev
-    include motd
+  include icinga2_dev
+  include icingaweb2_dev
+  include motd
+  file { '/etc/profile.d/env.sh':
+    source => 'puppet:////vagrant/.puppet/files/etc/profile.d/env.sh'
+  }
 }
