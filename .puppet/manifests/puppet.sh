8dc61fc709cd68d33c7352b320b53d8adbe25471
Eric Lippmann <eric.lippmann@netways.de>
2014-12-15 18:00:01 +0100

    puppet: Add a manifest for provisioning puppet
    refs #6842

---
+++ .puppet/manifests/puppet.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if which puppet >/dev/null 2>&1; then
+  exit 0
+fi
+
+RELEASEVER=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release))
+
+case $RELEASEVER in
+    6|7)
+        PUPPET="http://yum.puppetlabs.com/puppetlabs-release-el-${RELEASEVER}.noarch.rpm"
+        ;;
+    *)
+        echo "Unknown release version: $RELEASEVER" >&2
+        exit 1
+        ;;
+esac
+
+echo "Adding puppet repository.."
+rpm --import "https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs"
+rpm -ivh $PUPPET >/dev/null
+
+echo "Installing puppet.."
+yum install -y puppet >/dev/null
