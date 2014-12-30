24b2369adba047d49568f15c678946404e878e45
Eric Lippmann <eric.lippmann@netways.de>
2014-12-30 09:45:24 +0100

    monitoring: Fix PHPDoc of BackendConfigForm::setResourceConfig()

--- modules/monitoring/application/forms/Config/BackendConfigForm.php
+++ modules/monitoring/application/forms/Config/BackendConfigForm.php
@@ -34,7 +34,7 @@ class BackendConfigForm extends ConfigForm
     /**
      * Set the resource configuration to use
      *
-     * @param   Config      $resources      The resource configuration
+     * @param   Config  $resourceConfig     The resource configuration
      *
      * @return  $this
      *
