abc74c7fb505b9b089b51a3695f6f8597c0b597a
Eric Lippmann <eric.lippmann@netways.de>
2015-01-19 11:11:59 +0100

    monitoring/setup: Fix directory mode of the monitoring configuration directory
    We should introduce a CreateConfigDirectoryStep because module developers must not
    provide a directory mode but use our default.

--- modules/monitoring/library/Monitoring/MonitoringWizard.php
+++ modules/monitoring/library/Monitoring/MonitoringWizard.php
@@ -124,7 +124,7 @@ class MonitoringWizard extends Wizard implements SetupWizard
         $pageData = $this->getPageData();
         $setup = new Setup();
 
-        $setup->addStep(new MakeDirStep(array($this->getConfigDir() . '/modules/monitoring'), 0775));
+        $setup->addStep(new MakeDirStep(array($this->getConfigDir() . '/modules/monitoring'), 2770));
 
         $setup->addStep(
             new BackendStep(array(
