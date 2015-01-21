7d967d25b3fdef60b610107f5d1fb46c4aaeac25
Eric Lippmann <eric.lippmann@netways.de>
2015-01-21 10:12:14 +0100

    monitoring: Fix code style in the StatehistoryQuery

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/StatehistoryQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/StatehistoryQuery.php
@@ -1,6 +1,4 @@
 <?php
-// {{{ICINGA_LICENSE_HEADER}}}
-// {{{ICINGA_LICENSE_HEADER}}}
 
 namespace Icinga\Module\Monitoring\Backend\Ido\Query;
 
@@ -8,27 +6,27 @@ class StatehistoryQuery extends IdoQuery
 {
     protected $types = array(
         'soft_state' => 0,
-        'hard_state' => 1,
+        'hard_state' => 1
     );
 
     protected $columnMap = array(
         'statehistory' => array(
-            'raw_timestamp' => 'sh.state_time',
-            'timestamp'  => 'UNIX_TIMESTAMP(sh.state_time)',
-            'state_time' => 'sh.state_time',
-            'object_id'  => 'sho.object_id',
-            'type'       => "(CASE WHEN sh.state_type = 1 THEN 'hard_state' ELSE 'soft_state' END)",
-            'state'      => 'sh.state',
-            'state_type' => 'sh.state_type',
-            'output'     => 'sh.output',
-            'attempt'      => 'sh.current_check_attempt',
-            'max_attempts' => 'sh.max_check_attempts',
-            'host'                => 'sho.name1 COLLATE latin1_general_ci',
-            'host_name'           => 'sho.name1 COLLATE latin1_general_ci',
-            'service'             => 'sho.name2 COLLATE latin1_general_ci',
-            'service_description' => 'sho.name2 COLLATE latin1_general_ci',
-            'service_host_name'   => 'sho.name1 COLLATE latin1_general_ci',
-            'object_type'         => "CASE WHEN sho.objecttype_id = 1 THEN 'host' ELSE 'service' END"
+            'raw_timestamp'         => 'sh.state_time',
+            'timestamp'             => 'UNIX_TIMESTAMP(sh.state_time)',
+            'state_time'            => 'sh.state_time',
+            'object_id'             => 'sho.object_id',
+            'type'                  => "(CASE WHEN sh.state_type = 1 THEN 'hard_state' ELSE 'soft_state' END)",
+            'state'                 => 'sh.state',
+            'state_type'            => 'sh.state_type',
+            'output'                => 'sh.output',
+            'attempt'               => 'sh.current_check_attempt',
+            'max_attempts'          => 'sh.max_check_attempts',
+            'host'                  => 'sho.name1 COLLATE latin1_general_ci',
+            'host_name'             => 'sho.name1 COLLATE latin1_general_ci',
+            'service'               => 'sho.name2 COLLATE latin1_general_ci',
+            'service_description'   => 'sho.name2 COLLATE latin1_general_ci',
+            'service_host_name'     => 'sho.name1 COLLATE latin1_general_ci',
+            'object_type'           => "CASE WHEN sho.objecttype_id = 1 THEN 'host' ELSE 'service' END"
         )
     );
 
