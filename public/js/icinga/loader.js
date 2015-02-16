ae68e8aec42703c988ca2d5f2620e8a23ac4e4b1
Marius Hein <marius.hein@netways.de>
2015-02-16 11:35:44 +0100

    Ajax onFailure: Write component URL to container
    This is needed that history can push the correct detail url

--- public/js/icinga/loader.js
+++ public/js/icinga/loader.js
@@ -420,8 +420,6 @@
                     var $el = $(el);
                     if ($el.hasClass('dashboard')) {
                         return;
-                    } else {
-
                     }
                     var url = $el.data('icingaUrl');
                     targets[i].data('icingaUrl', url);
@@ -564,7 +562,7 @@
                 } else {
                     // Request wasn't for a container, so it's usually the body
                     // or the full layout. Push request URL to history:
-                    this.icinga.history.pushUrl(req.url);
+                    this.icinga.history.pushCurrentState();
                 }
             }
 
@@ -593,6 +591,8 @@
             var url = req.url;
 
             req.failure = true;
+
+            req.$target.data('icingaUrl', req.url);
 
             /*
              * Test if a manual actions comes in and autorefresh is active: Stop refreshing
