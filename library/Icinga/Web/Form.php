e2887df03e78e1ebffb3b7aecaae6051f37c3ccd
Johannes Meyer <johannes.meyer@netways.de>
2015-03-03 15:01:09 +0100

    Form: It's a field, not a control

--- library/Icinga/Web/Form.php
+++ library/Icinga/Web/Form.php
@@ -684,7 +684,7 @@ class Form extends Zend_Form
             if ($this->getUseFormAutosubmit()) {
                 $warningId = 'autosubmit_warning_' . $el->getId();
                 $warningText = $this->getView()->escape($this->translate(
-                    'Upon its value has changed, this control issues an automatic update of this page.'
+                    'Upon its value has changed, this field issues an automatic update of this page.'
                 ));
                 $autosubmitDecorator = $this->_getDecorator('Callback', array(
                     'placement' => 'PREPEND',
