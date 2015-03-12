7212434856723f16425420aa243bac307227da00
Eric Lippmann <eric.lippmann@netways.de>
2015-03-12 16:32:05 +0100

    Remove COLLATE from host_name and service_description in the Contact query
    refs #8614
    refs #8613

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/ContactQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/ContactQuery.php
@@ -34,12 +34,12 @@ class ContactQuery extends IdoQuery
         ),
         'hosts' => array(
             'host'      => 'ho.name1 COLLATE latin1_general_ci',
-            'host_name' => 'ho.name1 COLLATE latin1_general_ci',
+            'host_name' => 'ho.name1',
         ),
         'services' => array(
             'service'               => 'so.name1 COLLATE latin1_general_ci',
-            'service_description'   => 'so.name2 COLLATE latin1_general_ci',
-            'service_host_name'     => 'so.name1 COLLATE latin1_general_ci',
+            'service_description'   => 'so.name2',
+            'service_host_name'     => 'so.name1',
         )
     );
 
