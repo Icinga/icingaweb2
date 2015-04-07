0fc5801e5e8aa2998ad5af1189ff53b8645c827f
Eric Lippmann <eric.lippmann@netways.de>
2015-04-07 12:15:15 +0200

    lib: Add MissingParameterException::getParameter and ::setParameter ...
    ... for getting/setting the name of the missing parameter which caused the exception.
    
    refs #8886

--- library/Icinga/Exception/MissingParameterException.php
+++ library/Icinga/Exception/MissingParameterException.php
@@ -8,4 +8,33 @@ namespace Icinga\Exception;
  */
 class MissingParameterException extends IcingaException
 {
+    /**
+     * Name of the missing parameter
+     *
+     * @var string
+     */
+    protected $parameter;
+
+    /**
+     * Get the name of the missing parameter
+     *
+     * @return string
+     */
+    public function getParameter()
+    {
+        return $this->parameter;
+    }
+
+    /**
+     * Set the name of the missing parameter
+     *
+     * @param   string  $name
+     *
+     * @return  $this
+     */
+    public function setParameter($name)
+    {
+        $this->parameter = (string) $name;
+        return $this;
+    }
 }
