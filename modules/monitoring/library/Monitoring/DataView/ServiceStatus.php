c56c5655bd7bc3054c1a4c65971153443646d02a
Johannes Meyer <johannes.meyer@netways.de>
2015-04-08 14:51:04 +0200

    Do not permit querying columns with changed collation and a valid alternative
    refs #8613

--- modules/monitoring/library/Monitoring/DataView/ServiceStatus.php
+++ modules/monitoring/library/Monitoring/DataView/ServiceStatus.php
@@ -72,7 +72,6 @@ class ServiceStatus extends DataView
             'host_action_url',
             'host_notes_url',
             'host_last_comment',
-            'host',
             'host_display_name',
             'host_alias',
             'host_ipv4',
@@ -87,7 +86,6 @@ class ServiceStatus extends DataView
             'host_last_time_down',
             'host_last_time_unreachable',
             'host_modified_host_attributes',
-            'service',
             'service_hard_state',
             'service_problem',
             'service_perfdata',
@@ -165,7 +163,7 @@ class ServiceStatus extends DataView
 
     public function getFilterColumns()
     {
-        return array('hostgroup', 'servicegroup', 'service_problems');
+        return array('host', 'hostgroup', 'service', 'service_host', 'servicegroup');
     }
 
     public function isValidFilterTarget($column)
