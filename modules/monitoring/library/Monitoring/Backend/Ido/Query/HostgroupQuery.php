9f075e0691eca381afce71af4d43a43f20de6b12
Johannes Meyer <johannes.meyer@netways.de>
2015-04-10 15:19:55 +0200

    HostgroupQuery: Remove COLLATE from query column `hostgroup_name'
    refs #8613

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/HostgroupQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/HostgroupQuery.php
@@ -8,7 +8,7 @@ class HostgroupQuery extends IdoQuery
     protected $columnMap = array(
         'hostgroups' => array(
             'hostgroups'      => 'hgo.name1 COLLATE latin1_general_ci',
-            'hostgroup_name'  => 'hgo.name1 COLLATE latin1_general_ci',
+            'hostgroup_name'  => 'hgo.name1',
             'hostgroup_alias' => 'hg.alias',
             'id'              => 'hg.hostgroup_id',
         ),
