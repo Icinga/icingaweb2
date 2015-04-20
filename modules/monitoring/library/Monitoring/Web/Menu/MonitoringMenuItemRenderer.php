c76e4e35837326538d64afcde0f1ad68bc681d1a
Alexander Fuhr <alexander.fuhr@netways.de>
2015-04-20 13:05:23 +0200

    Change the global namespace to the module specific namespace
    refs #7642

--- modules/monitoring/library/Monitoring/Web/Menu/MonitoringMenuItemRenderer.php
+++ modules/monitoring/library/Monitoring/Web/Menu/MonitoringMenuItemRenderer.php
@@ -1,10 +1,11 @@
 <?php
 /* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
 
-namespace Icinga\Web\Menu;
+namespace Icinga\Module\Monitoring\Web\Menu;
 
+use Icinga\Web\Menu as Menu;
 use Icinga\Module\Monitoring\Backend\MonitoringBackend;
-use Icinga\Web\Menu;
+use Icinga\Web\Menu\MenuItemRenderer;
 use Icinga\Web\Url;
 
 class MonitoringMenuItemRenderer implements MenuItemRenderer {
