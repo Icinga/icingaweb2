41222fd34ebe1301b9853335598dc10f32a0f9a9
Johannes Meyer <johannes.meyer@netways.de>
2015-04-20 14:59:20 +0200

    Service multiselection view: Add dashboard action
    refs #7876

--- modules/monitoring/application/controllers/ServicesController.php
+++ modules/monitoring/application/controllers/ServicesController.php
@@ -16,6 +16,7 @@ use Icinga\Module\Monitoring\Object\Service;
 use Icinga\Module\Monitoring\Object\ServiceList;
 use Icinga\Web\Url;
 use Icinga\Web\Widget\Chart\InlinePie;
+use Icinga\Web\Widget\Tabextension\DashboardAction;
 
 class Monitoring_ServicesController extends Controller
 {
@@ -103,7 +104,7 @@ class Monitoring_ServicesController extends Controller
                 'label' => $this->translate('Services'),
                 'url'   => Url::fromRequest()
             )
-        )->activate('show');
+        )->extend(new DashboardAction())->activate('show');
         $this->setAutorefreshInterval(15);
         $checkNowForm = new CheckNowCommandForm();
         $checkNowForm
