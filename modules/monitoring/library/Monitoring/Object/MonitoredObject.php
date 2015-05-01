9c87ab6c12150007addda0b63d640518949a28ee
jweakley <j.weakley@nib.com.au>
2015-05-01 16:20:55 +1000

    Fix SQL error in /monitoring/service/show
    Fixed SQL error which occurs as a result of an empty filter being
    applied to a Filterable.
    
    refs #9211

--- modules/monitoring/library/Monitoring/Object/MonitoredObject.php
+++ modules/monitoring/library/Monitoring/Object/MonitoredObject.php
@@ -143,7 +143,9 @@ abstract class MonitoredObject implements Filterable
 
     public function applyFilter(Filter $filter)
     {
-        $this->getFilter()->addFilter($filter);
+		if (strlen($filter->toQueryString()) > 0){
+			$this->getFilter()->addFilter($filter);
+		}
         return $this;
     }
 
