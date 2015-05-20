80b174f526538d2fc28cf3a01d18a6c6bc90184a
Eric Lippmann <eric.lippmann@netways.de>
2015-05-20 16:16:21 +0200

    monitoring: Servicegroupsummary must not extend Groupsummary which is subject to remove
    refs #8241

--- modules/monitoring/library/Monitoring/DataView/Servicegroupsummary.php
+++ modules/monitoring/library/Monitoring/DataView/Servicegroupsummary.php
@@ -3,8 +3,42 @@
 
 namespace Icinga\Module\Monitoring\DataView;
 
-class Servicegroupsummary extends Groupsummary
+class Servicegroupsummary extends DataView
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getColumns()
+    {
+        return array(
+            'hosts_down_handled',
+            'hosts_down_unhandled',
+            'hosts_pending',
+            'hosts_unreachable_handled',
+            'hosts_unreachable_unhandled',
+            'hosts_up',
+            'servicegroup_alias',
+            'servicegroup_name',
+            'services_critical_handled',
+            'services_critical_last_state_change_handled',
+            'services_critical_last_state_change_unhandled',
+            'services_critical_unhandled',
+            'services_ok',
+            'services_ok_last_state_change',
+            'services_pending',
+            'services_pending_last_state_change',
+            'services_total',
+            'services_unknown_handled',
+            'services_unknown_last_state_change_handled',
+            'services_unknown_last_state_change_unhandled',
+            'services_unknown_unhandled',
+            'services_warning_handled',
+            'services_warning_last_state_change_handled',
+            'services_warning_last_state_change_unhandled',
+            'services_warning_unhandled'
+        );
+    }
+
     /**
      * {@inheritdoc}
      */
