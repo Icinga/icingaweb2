d379b27c245523a39813f8c06bb025fc87175518
Eric Lippmann <eric.lippmann@netways.de>
2015-05-20 15:40:49 +0200

    monitoring: Hostgroupsummary must not extend Groupsummary which is subject to remove
    refs #8241

--- modules/monitoring/library/Monitoring/DataView/Hostgroupsummary.php
+++ modules/monitoring/library/Monitoring/DataView/Hostgroupsummary.php
@@ -3,8 +3,46 @@
 
 namespace Icinga\Module\Monitoring\DataView;
 
-class Hostgroupsummary extends Groupsummary
+/**
+ * Data view for the host group summary
+ */
+class Hostgroupsummary extends DataView
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getColumns()
+    {
+        return array(
+            'hostgroup_alias',
+            'hostgroup_name',
+            'hosts_down_handled',
+            'hosts_down_last_state_change_handled',
+            'hosts_down_last_state_change_unhandled',
+            'hosts_down_unhandled',
+            'hosts_pending',
+            'hosts_pending_last_state_change',
+            'hosts_unreachable_handled',
+            'hosts_unreachable_last_state_change_handled',
+            'hosts_unreachable_last_state_change_unhandled',
+            'hosts_unreachable_unhandled',
+            'hosts_up',
+            'hosts_up_last_state_change',
+            'services_critical_handled',
+            'services_critical_unhandled',
+            'services_ok',
+            'services_pending',
+            'services_total',
+            'services_unknown_handled',
+            'services_unknown_unhandled',
+            'services_warning_handled',
+            'services_warning_unhandled'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getFilterColumns()
     {
         return array('hostgroup');
