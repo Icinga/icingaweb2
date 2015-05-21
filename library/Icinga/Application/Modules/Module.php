0002c0be260c0299ab92e7368f82e17ce4e53b45
Marius Hein <marius.hein@netways.de>
2015-05-21 14:40:52 +0200

    Provide default config name for Module::getConfig()

--- library/Icinga/Application/Modules/Module.php
+++ library/Icinga/Application/Modules/Module.php
@@ -600,7 +600,7 @@ class Module
      *
      * @return Config
      */
-    public function getConfig($file = null)
+    public function getConfig($file = 'config')
     {
         return $this->app->getConfig()->module($this->name, $file);
     }
