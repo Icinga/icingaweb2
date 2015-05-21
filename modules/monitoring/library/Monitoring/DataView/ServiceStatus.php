195275a716be571bb2c94cf868c3f7364fe0ecfb
Eric Lippmann <eric.lippmann@netways.de>
2015-05-21 12:17:25 +0200

    monitoring: Don't hardcode sort direction of the severity columns
    fixes #9059

--- modules/monitoring/library/Monitoring/DataView/ServiceStatus.php
+++ modules/monitoring/library/Monitoring/DataView/ServiceStatus.php
@@ -130,19 +130,21 @@ class ServiceStatus extends DataView
             ),
             'service_severity' => array(
                 'columns' => array(
-                    'service_severity DESC',
+                    'service_severity',
                     'service_last_state_change DESC',
                     'service_display_name ASC',
                     'host_display_name ASC'
-                )
+                ),
+                'order' => self::SORT_DESC
             ),
             'host_severity' => array(
                 'columns' => array(
-                    'host_severity DESC',
+                    'host_severity',
                     'host_last_state_change DESC',
                     'host_display_name ASC',
                     'service_display_name ASC'
-                )
+                ),
+                'order' => self::SORT_DESC
             ),
             'host_display_name' => array(
                 'columns' => array(
