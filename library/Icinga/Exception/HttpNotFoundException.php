c4ed49cb1a0a12ca153606cb1949b42ba740011b
Eric Lippmann <eric.lippmann@netways.de>
2015-05-21 16:43:58 +0200

    lib: Add HttpNotFoundException
    The HttpNotFoundException should be used for sending a HTTP 404 response w/ a custom message.
    
    There's also Zend_Controller_Action_Exception but this exception will always show 'Page not found' because we
    want to hide messages generated by Zend, like 'Action "foobar" does not exist and was not trapped in __call()'.
    
    refs #6281

---
+++ library/Icinga/Exception/HttpNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Icinga\Exception;
+
+/**
+ * Exception thrown for sending a HTTP 404 response w/ a custom message
+ */
+class HttpNotFoundException extends IcingaException
+{
+
+}
