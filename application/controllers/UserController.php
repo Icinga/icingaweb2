00ae69a694e846596bc7c179ec3867da1cf8d4f9
Michael Friedrich <michael.friedrich@netways.de>
2015-06-02 16:36:01 +0200

    Fix PHP 5.6.9 error: The use statement with non-compound name 'Exception' has no effect
    refs #9281

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,7 +1,6 @@
 <?php
 /* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
 
-use \Exception;
 use Icinga\Application\Logger;
 use Icinga\Exception\ConfigurationError;
 use Icinga\Exception\NotFoundError;
