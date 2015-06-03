fc2a50a9ffa40c1b67f78d7e5bea208c3c7e99d8
Eric Lippmann <eric.lippmann@netways.de>
2015-06-03 14:39:43 +0200

    monitoring: Add missing restrictable filter columns to the host status summary data view
    refs #9009

--- modules/monitoring/library/Monitoring/DataView/Hoststatussummary.php
+++ modules/monitoring/library/Monitoring/DataView/Hoststatussummary.php
@@ -30,7 +30,7 @@ class Hoststatussummary extends DataView
     public function getFilterColumns()
     {
         return array(
-            'host', 'host_alias', 'host_name',
+            'host', 'host_alias', 'host_display_name', 'host_name',
             'hostgroup', 'hostgroup_alias', 'hostgroup_name',
             'service', 'service_description', 'service_display_name',
             'servicegroup', 'servicegroup_alias', 'servicegroup_name'
@@ -42,6 +42,12 @@ class Hoststatussummary extends DataView
      */
     public function isValidFilterTarget($column)
     {
-        return in_array($column, $this->getFilterColumns());
+        if ($column[0] === '_'
+            && preg_match('/^_(?:host|service)_/', $column)
+        ) {
+            return true;
+        } else {
+            return in_array($column, $this->getFilterColumns());
+        }
     }
 }
