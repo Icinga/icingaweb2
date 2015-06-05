edeb24792e668319ae0e427319f436bfd255a417
Thomas Gelf <thomas@gelf.net>
2015-06-05 12:49:07 +0200

    CSS: move monitoring styles to monitoring module

--- library/Icinga/Web/StyleSheet.php
+++ library/Icinga/Web/StyleSheet.php
@@ -24,7 +24,6 @@ class StyleSheet
         'css/icinga/setup.less',
         'css/icinga/widgets.less',
         'css/icinga/pagination.less',
-        'css/icinga/monitoring-colors.less',
         'css/icinga/selection-toolbar.less',
         'css/icinga/login.less',
         'css/icinga/controls.less'
