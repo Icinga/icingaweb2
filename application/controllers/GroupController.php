91cee3a957cf0d272aa9cfcaad788c36e9828b3a
Johannes Meyer <johannes.meyer@netways.de>
2015-06-08 13:28:12 +0200

    GroupController: Behave nicely when not any backend is available

--- application/controllers/GroupController.php
+++ application/controllers/GroupController.php
@@ -28,6 +28,10 @@ class GroupController extends AuthBackendController
             function ($b) { return $b->getName(); },
             $this->loadUserGroupBackends('Icinga\Data\Selectable')
         );
+        if (empty($backendNames)) {
+            return;
+        }
+
         $this->view->backendSelection = new Form();
         $this->view->backendSelection->setAttrib('class', 'backend-selection');
         $this->view->backendSelection->setUidDisabled();
