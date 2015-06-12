c35be4023b39882c8276ea281b9c70f8016b8f26
Johannes Meyer <johannes.meyer@netways.de>
2015-06-12 12:45:59 +0200

    DowntimeendhistoryQuery: Use subqueries to fetch host and service downtimes
    refs #9009

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/DowntimeendhistoryQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/DowntimeendhistoryQuery.php
@@ -3,49 +3,151 @@
 
 namespace Icinga\Module\Monitoring\Backend\Ido\Query;
 
+use Zend_Db_Expr;
+use Zend_Db_Select;
+use Icinga\Data\Filter\Filter;
+
+/**
+ * Query for host and service downtime end history records
+ */
 class DowntimeendhistoryQuery extends IdoQuery
 {
+    /**
+     * {@inheritdoc}
+     */
     protected $columnMap = array(
         'downtimehistory' => array(
-            'state_time'            => 'h.actual_end_time',
-            'timestamp'             => 'UNIX_TIMESTAMP(h.actual_end_time)',
-            'raw_timestamp'         => 'h.actual_end_time',
-            'object_id'             => 'h.object_id',
-            'type'                  => "('dt_end')",
-            'state'                 => '(NULL)',
-            'state_type'            => '(NULL)',
-            'output'                => "('[' || h.author_name || '] ' || h.comment_data)",
-            'attempt'               => '(NULL)',
-            'max_attempts'          => '(NULL)',
-
-            'host'                  => 'o.name1 COLLATE latin1_general_ci',
-            'service'               => 'o.name2 COLLATE latin1_general_ci',
-            'host_name'             => 'o.name1',
-            'service_description'   => 'o.name2',
-            'object_type'           => "CASE WHEN o.objecttype_id = 1 THEN 'host' ELSE 'service' END"
+            'object_type' => 'deh.object_type'
+        ),
+        'history' => array(
+            'type'      => 'deh.type',
+            'timestamp' => 'deh.timestamp',
+            'object_id' => 'deh.object_id',
+            'state'     => 'deh.state',
+            'output'    => 'deh.output'
+        ),
+        'hosts' => array(
+            'host_display_name' => 'deh.host_display_name',
+            'host_name'         => 'deh.host_name'
+        ),
+        'services' => array(
+            'service_description'   => 'deh.service_description',
+            'service_display_name'  => 'deh.service_display_name',
+            'service_host_name'     => 'deh.service_host_name'
         )
     );
 
-    public function whereToSql($col, $sign, $expression)
-    {
-        if ($col === 'UNIX_TIMESTAMP(h.actual_end_time)') {
-            return 'h.actual_end_time ' . $sign . ' ' . $this->timestampForSql($this->valueToTimestamp($expression));
-        } else {
-            return parent::whereToSql($col, $sign, $expression);
-        }
-    }
+    /**
+     * The union
+     *
+     * @var Zend_Db_Select
+     */
+    protected $downtimeEndHistoryQuery;
 
+    /**
+     * Subqueries used for the downtime end history query
+     *
+     * @var IdoQuery[]
+     */
+    protected $subQueries = array();
+
+    /**
+     * Whether to additionally select all history columns
+     *
+     * @var bool
+     */
+    protected $fetchHistoryColumns = false;
+
+    /**
+     * {@inheritdoc}
+     */
     protected function joinBaseTables()
     {
+        $this->downtimeEndHistoryQuery = $this->db->select();
         $this->select->from(
-            array('o' => $this->prefix . 'objects'),
+            array('deh' => $this->downtimeEndHistoryQuery),
             array()
-        )->join(
-            array('h' => $this->prefix . 'downtimehistory'),
-            'o.' . $this->object_id . ' = h.' . $this->object_id . ' AND o.is_active = 1',
-            array()
-        )->where('h.actual_end_time > ?', '1970-01-02 00:00:00');
-        $this->joinedVirtualTables = array('downtimehistory' => true);
+        );
+        $this->joinedVirtualTables['downtimehistory'] = true;
+    }
+
+    /**
+     * Join history related columns and tables
+     */
+    protected function joinHistory()
+    {
+        // TODO: Ensure that one is selecting the history columns first...
+        $this->fetchHistoryColumns = true;
+        $this->requireVirtualTable('hosts');
+        $this->requireVirtualTable('services');
+    }
+
+    /**
+     * Join hosts
+     */
+    protected function joinHosts()
+    {
+        $columns = array_keys(
+            $this->columnMap['downtimehistory'] + $this->columnMap['hosts']
+        );
+        foreach ($this->columnMap['services'] as $column => $_) {
+            $columns[$column] = new Zend_Db_Expr('NULL');
+        }
+        if ($this->fetchHistoryColumns) {
+            $columns = array_merge($columns, array_keys($this->columnMap['history']));
+        }
+        $hosts = $this->createSubQuery('Hostdowntimeendhistory', $columns);
+        $this->subQueries[] = $hosts;
+        $this->downtimeEndHistoryQuery->union(array($hosts), Zend_Db_Select::SQL_UNION_ALL);
+    }
+
+    /**
+     * Join services
+     */
+    protected function joinServices()
+    {
+        $columns = array_keys(
+            $this->columnMap['downtimehistory'] + $this->columnMap['hosts'] + $this->columnMap['services']
+        );
+        if ($this->fetchHistoryColumns) {
+            $columns = array_merge($columns, array_keys($this->columnMap['history']));
+        }
+        $services = $this->createSubQuery('Servicedowntimeendhistory', $columns);
+        $this->subQueries[] = $services;
+        $this->downtimeEndHistoryQuery->union(array($services), Zend_Db_Select::SQL_UNION_ALL);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function order($columnOrAlias, $dir = null)
+    {
+        foreach ($this->subQueries as $sub) {
+            $sub->requireColumn($columnOrAlias);
+        }
+        return parent::order($columnOrAlias, $dir);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function where($condition, $value = null)
+    {
+        $this->requireColumn($condition);
+        foreach ($this->subQueries as $sub) {
+            $sub->where($condition, $value);
+        }
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addFilter(Filter $filter)
+    {
+        foreach ($this->subQueries as $sub) {
+            $sub->applyFilter(clone $filter);
+        }
+        return $this;
     }
 }
-
