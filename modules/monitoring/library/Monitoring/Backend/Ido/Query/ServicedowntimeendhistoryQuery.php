95b5bba65c8b2b8ef5329be60c29c5e82d938ed7
Johannes Meyer <johannes.meyer@netways.de>
2015-06-12 12:45:02 +0200

    Introduce query ServicedowntimeendhistoryQuery
    refs #9009

---
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicedowntimeendhistoryQuery.php
@@ -0,0 +1,39 @@
+<?php
+/* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Monitoring\Backend\Ido\Query;
+
+/**
+ * Query for host downtime end history records
+ */
+class ServicedowntimeendhistoryQuery extends ServicedowntimestarthistoryQuery
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function whereToSql($col, $sign, $expression)
+    {
+        if ($col === 'UNIX_TIMESTAMP(sdh.actual_end_time)') {
+            return 'sdh.actual_end_time ' . $sign . ' ' . $this->timestampForSql(
+                $this->valueToTimestamp($expression)
+            );
+        } else {
+            return parent::whereToSql($col, $sign, $expression);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function joinBaseTables()
+    {
+        parent::joinBaseTables(true);
+        $this->select->where("sdh.actual_end_time > '1970-01-02 00:00:00'");
+        $this->columnMap['history']['type'] = "('dt_end')";
+        $this->columnMap['history']['timestamp'] = str_replace(
+            'actual_start_time',
+            'actual_end_time',
+            $this->columnMap['history']['timestamp']
+        );
+    }
+}
