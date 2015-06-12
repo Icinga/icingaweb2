18d3fcbbf7500fdd3ed618ac2bf71d9ebb8c4e9f
Johannes Meyer <johannes.meyer@netways.de>
2015-06-12 12:44:25 +0200

    Introduce query HostdowntimestarthistoryQuery
    refs #9009

---
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/HostdowntimestarthistoryQuery.php
@@ -0,0 +1,189 @@
+<?php
+/* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Monitoring\Backend\Ido\Query;
+
+/**
+ * Query for host downtime start history records
+ */
+class HostdowntimestarthistoryQuery extends IdoQuery
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $allowCustomVars = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $columnMap = array(
+        'downtimehistory' => array(
+            'host'          => 'ho.name1 COLLATE latin1_general_ci',
+            'host_name'     => 'ho.name1',
+            'object_type'   => '(\'host\')'
+        ),
+        'history' => array(
+            'type'      => "('dt_start')",
+            'timestamp' => 'UNIX_TIMESTAMP(hdh.actual_start_time)',
+            'object_id' => 'hdh.object_id',
+            'state'     => '(NULL)',
+            'output'    => "('[' || hdh.author_name || '] ' || hdh.comment_data)",
+        ),
+        'hostgroups' => array(
+            'hostgroup'         => 'hgo.name1 COLLATE latin1_general_ci',
+            'hostgroup_alias'   => 'hg.alias COLLATE latin1_general_ci',
+            'hostgroup_name'    => 'hgo.name1'
+        ),
+        'hosts' => array(
+            'host_alias'        => 'h.alias',
+            'host_display_name' => 'h.display_name COLLATE latin1_general_ci'
+        ),
+        'servicegroups' => array(
+            'servicegroup'          => 'sgo.name1 COLLATE latin1_general_ci',
+            'servicegroup_name'     => 'sgo.name1',
+            'servicegroup_alias'    => 'sg.alias COLLATE latin1_general_ci'
+        ),
+        'services' => array(
+            'service'               => 'so.name2 COLLATE latin1_general_ci',
+            'service_description'   => 'so.name2',
+            'service_display_name'  => 's.display_name COLLATE latin1_general_ci',
+            'service_host_name'     => 'so.name1'
+        )
+    );
+
+    /**
+     * {@inheritdoc}
+     */
+    public function whereToSql($col, $sign, $expression)
+    {
+        if ($col === 'UNIX_TIMESTAMP(hdh.actual_start_time)') {
+            return 'hdh.actual_start_time ' . $sign . ' ' . $this->timestampForSql(
+                $this->valueToTimestamp($expression)
+            );
+        } else {
+            return parent::whereToSql($col, $sign, $expression);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function joinBaseTables()
+    {
+        $this->select->from(
+            array('hdh' => $this->prefix . 'downtimehistory'),
+            array()
+        )->join(
+            array('ho' => $this->prefix . 'objects'),
+            'ho.object_id = hdh.object_id',
+            array()
+        )->where(
+            'ho.is_active = ?',
+            1
+        )->where(
+            'ho.objecttype_id = ?',
+            1
+        );
+
+        if (@func_get_arg(0) === false) {
+            $this->select->where(
+                "hdh.actual_start_time > '1970-01-02 00:00:00'"
+            );
+        }
+
+        $this->joinedVirtualTables['downtimehistory'] = true;
+        $this->joinedVirtualTables['history'] = true;
+    }
+
+    /**
+     * Join host groups
+     */
+    protected function joinHostgroups()
+    {
+        $this->select->join(
+            array('hgm' => $this->prefix . 'hostgroup_members'),
+            'hgm.host_object_id = ho.object_id',
+            array()
+        )->join(
+            array('hg' => $this->prefix . 'hostgroups'),
+            'hg.hostgroup_id = hgm.hostgroup_id',
+            array()
+        )->join(
+            array('hgo' => $this->prefix . 'objects'),
+            'hgo.object_id = hg.hostgroup_object_id',
+            array()
+        )->where(
+            'hgo.is_active = ?',
+            1
+        )
+        ->where(
+            'hgo.objecttype_id = ?',
+            3
+        );
+        $this->select->group(array('hdh.downtimehistory_id', 'ho.name1'));
+    }
+
+    /**
+     * Join hosts
+     */
+    protected function joinHosts()
+    {
+        $this->select->join(
+            array('h' => $this->prefix . 'hosts'),
+            'h.host_object_id = ho.object_id',
+            array()
+        );
+    }
+
+    /**
+     * Join service groups
+     */
+    protected function joinServicegroups()
+    {
+        $this->requireVirtualTable('services');
+        $this->select->join(
+            array('sgm' => $this->prefix . 'servicegroup_members'),
+            'sgm.service_object_id = s.service_object_id',
+            array()
+        )->join(
+            array('sg' => $this->prefix . 'servicegroups'),
+            'sg.' . $this->servicegroup_id . ' = sgm.servicegroup_id',
+            array()
+        )->join(
+            array('sgo' => $this->prefix . 'objects'),
+            'sgo.object_id = sg.servicegroup_object_id',
+            array()
+        )->where(
+            'sgo.is_active = ?',
+            1
+        )
+        ->where(
+            'sgo.objecttype_id = ?',
+            4
+        );
+    }
+
+    /**
+     * Join services
+     */
+    protected function joinServices()
+    {
+        $this->select->join(
+            array('s' => $this->prefix . 'services'),
+            's.host_object_id = ho.object_id',
+            array()
+        )->join(
+            array('so' => $this->prefix . 'objects'),
+            'so.object_id = s.service_object_id',
+            array()
+        )->where(
+            'so.is_active = ?',
+            1
+        )
+        ->where(
+            'so.objecttype_id = ?',
+            2
+        );
+        $this->select->group(array('hdh.downtimehistory_id', 'ho.name1'));
+    }
+}
