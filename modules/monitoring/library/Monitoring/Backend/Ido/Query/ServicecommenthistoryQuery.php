104c8e7a2b03b09fe8f9cfea10aac7c253772718
Johannes Meyer <johannes.meyer@netways.de>
2015-06-16 08:11:29 +0200

    ServicecommenthistoryQuery: Improve JOIN performance
    refs #9009

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicecommenthistoryQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicecommenthistoryQuery.php
@@ -74,14 +74,8 @@ class ServicecommenthistoryQuery extends IdoQuery
             array()
         )->join(
             array('so' => $this->prefix . 'objects'),
-            'so.object_id = sch.object_id',
+            'so.object_id = sch.object_id AND so.is_active = 1 AND so.objecttype_id = 2',
             array()
-        )->where(
-            'so.is_active = ?',
-            1
-        )->where(
-            'so.objecttype_id = ?',
-            2
         );
         $this->joinedVirtualTables['commenthistory'] = true;
         $this->joinedVirtualTables['history'] = true;
@@ -103,15 +97,8 @@ class ServicecommenthistoryQuery extends IdoQuery
             array()
         )->join(
             array('hgo' => $this->prefix . 'objects'),
-            'hgo.object_id = hg.hostgroup_object_id',
+            'hgo.object_id = hg.hostgroup_object_id AND hgo.is_active = 1 AND hgo.objecttype_id = 3',
             array()
-        )->where(
-            'hgo.is_active = ?',
-            1
-        )
-        ->where(
-            'hgo.objecttype_id = ?',
-            3
         );
         $this->select->group(array('sch.commenthistory_id', 'so.name1', 'so.name2'));
     }
@@ -144,15 +131,8 @@ class ServicecommenthistoryQuery extends IdoQuery
             array()
         )->join(
             array('sgo' => $this->prefix . 'objects'),
-            'sgo.object_id = sg.servicegroup_object_id',
+            'sgo.object_id = sg.servicegroup_object_id AND sgo.is_active = 1 AND sgo.objecttype_id = 4',
             array()
-        )->where(
-            'sgo.is_active = ?',
-            1
-        )
-        ->where(
-            'sgo.objecttype_id = ?',
-            4
         );
         $this->select->group(array('sch.commenthistory_id', 'so.name1', 'so.name2'));
     }
