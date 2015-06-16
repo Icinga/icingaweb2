e73409eb6d440634204cf62f7cbcf33a42a376ca
Johannes Meyer <johannes.meyer@netways.de>
2015-06-16 08:10:32 +0200

    HostdowntimeQuery: Improve JOIN performance
    refs #9009

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/HostdowntimeQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/HostdowntimeQuery.php
@@ -70,14 +70,8 @@ class HostdowntimeQuery extends IdoQuery
             array()
         )->join(
             array('ho' => $this->prefix . 'objects'),
-            'sd.object_id = ho.object_id',
+            'sd.object_id = ho.object_id AND ho.is_active = 1 AND ho.objecttype_id = 1',
             array()
-        )->where(
-            'ho.is_active = ?',
-            1
-        )->where(
-            'ho.objecttype_id = ?',
-            1
         );
         $this->joinedVirtualTables['downtimes'] = true;
     }
@@ -97,15 +91,8 @@ class HostdowntimeQuery extends IdoQuery
             array()
         )->join(
             array('hgo' => $this->prefix . 'objects'),
-            'hgo.object_id = hg.hostgroup_object_id',
+            'hgo.object_id = hg.hostgroup_object_id AND hgo.is_active = 1 AND hgo.objecttype_id = 3',
             array()
-        )->where(
-            'hgo.is_active = ?',
-            1
-        )
-        ->where(
-            'hgo.objecttype_id = ?',
-            3
         );
         $this->group(array('sd.scheduleddowntime_id', 'ho.name1'));
     }
@@ -150,15 +137,8 @@ class HostdowntimeQuery extends IdoQuery
             array()
         )->join(
             array('sgo' => $this->prefix . 'objects'),
-            'sgo.object_id = sg.servicegroup_object_id',
+            'sgo.object_id = sg.servicegroup_object_id AND sgo.is_active = 1 AND sgo.objecttype_id = 4',
             array()
-        )->where(
-            'sgo.is_active = ?',
-            1
-        )
-        ->where(
-            'sgo.objecttype_id = ?',
-            4
         );
     }
 
@@ -173,15 +153,8 @@ class HostdowntimeQuery extends IdoQuery
             array()
         )->join(
             array('so' => $this->prefix . 'objects'),
-            'so.object_id = s.service_object_id AND so.is_active = 1',
+            'so.object_id = s.service_object_id AND so.is_active = 1 AND so.objecttype_id = 2',
             array()
-        )->where(
-            'so.is_active = ?',
-            1
-        )
-        ->where(
-            'so.objecttype_id = ?',
-            2
         );
         $this->group(array('sd.scheduleddowntime_id', 'ho.name1'));
     }
