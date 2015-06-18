d66b7e85e745159bd49cc02001e16bd77c6fb531
Johannes Meyer <johannes.meyer@netways.de>
2015-06-18 11:17:36 +0200

    ServicecommentQuery: PostgreSQL grouping...
    refs #9009

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicecommentQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicecommentQuery.php
@@ -93,7 +93,6 @@ class ServicecommentQuery extends IdoQuery
             'hgo.object_id = hg.hostgroup_object_id AND hgo.is_active = 1 AND hgo.objecttype_id = 3',
             array()
         );
-        $this->group(array('c.comment_id', 'so.name2', 'so.name1'));
     }
 
     /**
@@ -140,7 +139,6 @@ class ServicecommentQuery extends IdoQuery
             'sgo.object_id = sg.servicegroup_object_id AND sgo.is_active = 1 AND sgo.objecttype_id = 4',
             array()
         );
-        $this->group(array('c.comment_id', 'so.name2', 'so.name1'));
     }
 
     /**
@@ -165,5 +163,33 @@ class ServicecommentQuery extends IdoQuery
             'ss.service_object_id = so.object_id',
             array()
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGroup()
+    {
+        $group = array();
+        if ($this->hasJoinedVirtualTable('hostgroups') || $this->hasJoinedVirtualTable('servicegroups')) {
+            $group = array('c.comment_id', 'so.object_id');
+            if ($this->hasJoinedVirtualTable('hosts')) {
+                $group[] = 'h.host_id';
+            }
+
+            if ($this->hasJoinedVirtualTable('services')) {
+                $group[] = 's.service_id';
+            }
+
+            if ($this->hasJoinedVirtualTable('hoststatus')) {
+                $group[] = 'hs.hoststatus_id';
+            }
+
+            if ($this->hasJoinedVirtualTable('servicestatus')) {
+                $group[] = 'ss.servicestatus_id';
+            }
+        }
+
+        return $group;
     }
 }
