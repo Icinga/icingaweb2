e43abd37a11c9d112fe81c0399c1b9eb583a34e6
Johannes Meyer <johannes.meyer@netways.de>
2015-07-02 10:31:03 +0200

    DbResourcePage: Drop hidden element `prefix'
    That's not a valid configuration option anymore.

--- modules/setup/application/forms/DbResourcePage.php
+++ modules/setup/application/forms/DbResourcePage.php
@@ -55,14 +55,6 @@ class DbResourcePage extends Form
         $resourceForm = new DbResourceForm();
         $this->addElements($resourceForm->createElements($formData)->getElements());
         $this->getElement('name')->setValue('icingaweb_db');
-        $this->addElement(
-            'hidden',
-            'prefix',
-            array(
-                'required'  => true,
-                'value'     => 'icingaweb_'
-            )
-        );
     }
 
     /**
