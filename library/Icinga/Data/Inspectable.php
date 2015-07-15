6762ef053e3f49d1896497efcff7b40b9023bcc9
Matthias Jentsch <matthias.jentsch@netways.de>
2015-07-15 17:30:39 +0200

    Upgrade Inspection API
    Reduce code duplication and stateffullnes by using InspectionException to indicate that an error was thrown, and only using one inspect function.
    
    refs #9630

--- library/Icinga/Data/Inspectable.php
+++ library/Icinga/Data/Inspectable.php
@@ -2,7 +2,7 @@
 /* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
 
 namespace Icinga\Data;
-
+use Icinga\Exception\InspectionException;
 
 /**
  * An object for which the user can retrieve status information
@@ -12,15 +12,8 @@ interface Inspectable
     /**
      * Get information about this objects state
      *
-     * @return array    An array of strings that describe the state in a human-readable form, each array element
-     *                  represents one fact about this object
+     * @return Inspection
+     * @throws InspectionException  When inspection of the object was not possible
      */
-    public function getInfo();
-
-    /**
-     * If this object is working in its current configuration
-     *
-     * @return Bool     True if the object is working, false if not
-     */
-    public function isHealthy();
+    public function inspect();
 }
