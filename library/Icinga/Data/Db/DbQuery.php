a0d5509f191ef79094b3841b8927ab249528efda
Johannes Meyer <johannes.meyer@netways.de>
2015-08-05 10:54:14 +0200

    DbQuery: Drop attribute $maxCount
    It's not used anywhere and completely useless the way it's implemented.

--- library/Icinga/Data/Db/DbQuery.php
+++ library/Icinga/Data/Db/DbQuery.php
@@ -48,16 +48,6 @@ class DbQuery extends SimpleQuery
     protected $useSubqueryCount = false;
 
     /**
-     * Set the count maximum
-     *
-     * If the count maximum is set, count queries will not count more than that many rows. You should set this
-     * property only for really heavy queries.
-     *
-     * @var int
-     */
-    protected $maxCount;
-
-    /**
      * Count query result
      *
      * Count queries are only executed once
@@ -338,9 +328,6 @@ class DbQuery extends SimpleQuery
             $count->columns($this->columns);
             $columns = array('cnt' => 'COUNT(*)');
             return $this->db->select()->from($count, $columns);
-        }
-        if ($this->maxCount !== null) {
-            return $this->db->select()->from($count->limit($this->maxCount));
         }
 
         $count->columns(array('cnt' => 'COUNT(*)'));
