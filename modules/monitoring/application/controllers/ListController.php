9dd13d2a82b105fca54811aa96c5c1f201c68b8b
Eric Lippmann <eric.lippmann@netways.de>
2015-08-21 11:19:00 +0200

    monitoring: Apply restriction on comments before calling filterQuery which also handles format
    refs #9965

--- modules/monitoring/application/controllers/ListController.php
+++ modules/monitoring/application/controllers/ListController.php
@@ -411,10 +411,8 @@ class Monitoring_ListController extends Controller
             'host_display_name',
             'service_display_name'
         ));
-        $this->filterQuery($query);
-
         $this->applyRestriction('monitoring/filter/objects', $query);
-
+        $this->filterQuery($query):
         $this->view->comments = $query;
 
         $this->setupLimitControl();
