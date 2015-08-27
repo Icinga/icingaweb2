d638868524e8463b467c98e0cc5fe12a45288a1c
Eric Lippmann <eric.lippmann@netways.de>
2015-08-27 14:08:12 +0200

    Namespace the StaticController
    refs #5786

--- application/controllers/StaticController.php
+++ application/controllers/StaticController.php
@@ -1,6 +1,8 @@
 <?php
 /* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
 
+namespace Icinga\Controllers;
+
 use Icinga\Web\Controller;
 use Icinga\Application\Icinga;
 use Icinga\Application\Logger;
