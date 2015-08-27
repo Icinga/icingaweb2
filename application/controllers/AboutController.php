5d8424535f0978b2608c64ae7f9d598d8e258b53
Eric Lippmann <eric.lippmann@netways.de>
2015-08-27 13:05:20 +0200

    Namespace the AboutController
    refs #5786

--- application/controllers/AboutController.php
+++ application/controllers/AboutController.php
@@ -1,7 +1,7 @@
 <?php
 /* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
 
-# namespace Icinga\Application\Controllers;
+namespace Icinga\Controllers;
 
 use Icinga\Web\Controller\ActionController;
 use Icinga\Application\Version;
