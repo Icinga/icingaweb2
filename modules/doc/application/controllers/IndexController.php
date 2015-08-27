77b9881a38e2a8ddf24c958e27ccb085a705f4d7
Eric Lippmann <eric.lippmann@netways.de>
2015-08-27 14:37:44 +0200

    doc: Namespace IndexController
    refs #5786

--- modules/doc/application/controllers/IndexController.php
+++ modules/doc/application/controllers/IndexController.php
@@ -1,9 +1,11 @@
 <?php
 /* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
 
+namespace Icinga\Module\Doc\Controllers;
+
 use Icinga\Module\Doc\DocController;
 
-class Doc_IndexController extends DocController
+class IndexController extends DocController
 {
     public function indexAction()
     {
