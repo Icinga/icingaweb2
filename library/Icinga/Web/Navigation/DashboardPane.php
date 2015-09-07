e63e15e471a19941b9eae586ac0e04fca9ec3628
Johannes Meyer <johannes.meyer@netways.de>
2015-09-07 09:06:40 +0200

    Add class DashboardPane
    refs #5600

---
+++ library/Icinga/Web/Navigation/DashboardPane.php
@@ -0,0 +1,50 @@
+<?php
+/* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Web\Navigation;
+
+use Icinga\Web\Url;
+
+/**
+ * A dashboard pane
+ */
+class DashboardPane extends NavigationItem
+{
+    /**
+     * This pane's dashlets
+     *
+     * @var array
+     */
+    protected $dashlets;
+
+    /**
+     * Set this pane's dashlets
+     *
+     * @param   array   $dashlets
+     *
+     * @return  $this
+     */
+    public function setDashlets(array $dashlets)
+    {
+        $this->dashlets = $dashlets;
+        return $this;
+    }
+
+    /**
+     * Return this pane's dashlets
+     *
+     * @return  array
+     */
+    public function getDashlets()
+    {
+        return $this->dashlets ?: array();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        $this->setUrl(Url::fromPath('dashboard', array('pane' => $this->getName())));
+    }
+}
