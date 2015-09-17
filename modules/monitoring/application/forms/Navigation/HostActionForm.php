25307fc1a057a21e06778ae861c59d28cb50d13a
Johannes Meyer <johannes.meyer@netways.de>
2015-09-17 09:21:33 +0200

    Introduce form class HostActionForm
    refs #5600

---
+++ modules/monitoring/application/forms/Navigation/HostActionForm.php
@@ -0,0 +1,30 @@
+<?php
+/* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Monitoring\Forms\Navigation;
+
+use Icinga\Forms\Navigation\NavigationItemForm;
+
+class HostActionForm extends NavigationItemForm
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function createElements(array $formData)
+    {
+        parent::createElements($formData);
+
+        $this->addElement(
+            'text',
+            'filter',
+            array(
+                'allowEmpty'    => true,
+                'label'         => $this->translate('Filter'),
+                'description'   => $this->translate(
+                    'Display this action only for hosts matching this filter. Leave'
+                    . ' blank if you want this action being displayed for all hosts'
+                )
+            )
+        );
+    }
+}
