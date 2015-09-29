0d0480afea390a7a466821aa59e9d0a3dfa55b97
Johannes Meyer <johannes.meyer@netways.de>
2015-09-29 13:43:56 +0200

    Introduce controller DashboardsController

---
+++ application/controllers/DashboardsController.php
@@ -0,0 +1,13 @@
+<?php
+/* Icinga Web 2 | (c) 2013-2015 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Controllers;
+
+use Icinga\Web\Controller;
+
+/**
+ * Dashboards
+ */
+class DashboardsController extends Controller
+{
+}
