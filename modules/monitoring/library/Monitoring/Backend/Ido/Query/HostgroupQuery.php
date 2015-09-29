73152e924435d7a8cba345c788c152a872753ae1
Eric Lippmann <eric.lippmann@netways.de>
2015-09-29 22:50:49 +0200

    monitoring: Rearrange code in the HostgroupQuery
    refs #9956

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/HostgroupQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/HostgroupQuery.php
@@ -27,9 +27,6 @@ class HostgroupQuery extends IdoQuery
      * {@inheritdoc}
      */
     protected $columnMap = array(
-        'instances' => array(
-            'instance_name' => 'i.instance_name'
-        ),
         'hostgroups' => array(
             'hostgroup'         => 'hgo.name1 COLLATE latin1_general_ci',
             'hostgroup_alias'   => 'hg.alias COLLATE latin1_general_ci',
@@ -42,6 +39,9 @@ class HostgroupQuery extends IdoQuery
         'hosts' => array(
             'host_alias'            => 'h.alias',
             'host_display_name'     => 'h.display_name COLLATE latin1_general_ci',
+        ),
+        'instances' => array(
+            'instance_name' => 'i.instance_name'
         ),
         'servicegroups' => array(
             'servicegroup'          => 'sgo.name1 COLLATE latin1_general_ci',
@@ -102,6 +102,18 @@ class HostgroupQuery extends IdoQuery
     }
 
     /**
+     * Join instances
+     */
+    protected function joinInstances()
+    {
+        $this->select->join(
+            array('i' => $this->prefix . 'instances'),
+            'i.instance_id = hg.instance_id',
+            array()
+        );
+    }
+
+    /**
      * Join service groups
      */
     protected function joinServicegroups()
@@ -135,18 +147,6 @@ class HostgroupQuery extends IdoQuery
         )->joinLeft(
             array('so' => $this->prefix . 'objects'),
             'so.object_id = s.service_object_id AND so.is_active = 1 AND so.objecttype_id = 2',
-            array()
-        );
-    }
-
-    /**
-     * Join instances
-     */
-    protected function joinInstances()
-    {
-        $this->select->join(
-            array('i' => $this->prefix . 'instances'),
-            'i.instance_id = hg.instance_id',
             array()
         );
     }
