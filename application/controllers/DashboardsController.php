2be8835f0609ba886678699e3e472a1d426ddc53
Johannes Meyer <johannes.meyer@netways.de>
2015-09-29 13:45:12 +0200

    DashboardsController: Add index action

--- application/controllers/DashboardsController.php
+++ application/controllers/DashboardsController.php
@@ -4,10 +4,34 @@
 namespace Icinga\Controllers;
 
 use Icinga\Web\Controller;
+use Icinga\Web\Widget\Dashboard;
 
 /**
  * Dashboards
  */
 class DashboardsController extends Controller
 {
+    /**
+     * Display the dashboard with the pane set in the "pane" request parameter
+     *
+     * If no pane is given or the given one doesn't exist, the default pane is displayed
+     */
+    public function indexAction()
+    {
+        $dashboard = new Dashboard();
+        $dashboard->setUser($this->Auth()->getUser());
+        $dashboard->load();
+
+        $this->view->title = 'Dashboard';
+        $this->view->tabs = $dashboard->getTabs();
+
+        if ($dashboard->hasPanes()) {
+            if (($pane = $this->params->get('pane')) !== null) {
+                $dashboard->activate($pane);
+            }
+
+            $this->view->dashboard = $dashboard;
+            $this->view->title = $dashboard->getActivePane()->getTitle() . ' :: Dashboard';
+        }
+    }
 }
