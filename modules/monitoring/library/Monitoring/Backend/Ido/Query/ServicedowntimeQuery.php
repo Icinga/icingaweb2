f4e4dbe0693606003d8f05a6003af206a010129b
Eric Lippmann <eric.lippmann@netways.de>
2015-09-30 09:29:34 +0200

    monitoring: Remove ServicedowntimeQuery::getGroup()
    refs #9956

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicedowntimeQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicedowntimeQuery.php
@@ -16,6 +16,16 @@ class ServicedowntimeQuery extends IdoQuery
     /**
      * {@inheritdoc}
      */
+    protected $groupBase = array('downtimes', array('sd.scheduleddowntime_id', 'so.object_id'));
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $groupOrigin = array('hostgroups', 'servicegroups');
+
+    /**
+     * {@inheritdoc}
+     */
     protected $columnMap = array(
         'instances' => array(
             'instance_name' => 'i.instance_name'
@@ -184,38 +194,5 @@ class ServicedowntimeQuery extends IdoQuery
             'i.instance_id = sd.instance_id',
             array()
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getGroup()
-    {
-        $group = array();
-        if ($this->hasJoinedVirtualTable('hostgroups') || $this->hasJoinedVirtualTable('servicegroups')) {
-            $group = array('sd.scheduleddowntime_id', 'so.object_id');
-
-            if ($this->hasJoinedVirtualTable('hosts')) {
-                $group[] = 'h.host_id';
-            }
-
-            if ($this->hasJoinedVirtualTable('hoststatus')) {
-                $group[] = 'hs.hoststatus_id';
-            }
-
-            if ($this->hasJoinedVirtualTable('services')) {
-                $group[] = 's.service_id';
-            }
-
-            if ($this->hasJoinedVirtualTable('servicestatus')) {
-                $group[] = 'ss.servicestatus_id';
-            }
-
-            if ($this->hasJoinedVirtualTable('instances')) {
-                $group[] = 'i.instance_id';
-            }
-        }
-
-        return $group;
     }
 }
