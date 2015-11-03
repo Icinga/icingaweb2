ba55ad7c5a94d77845a216929f8ca3d7aa352dc7
Eric Lippmann <eric.lippmann@netways.de>
2015-11-03 17:17:27 +0100

    monitoring: Fix unhandled service counter in the hosts overview
    Group by was wrong.
    
    fixes #10490

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/HostserviceproblemsummaryQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/HostserviceproblemsummaryQuery.php
@@ -75,7 +75,7 @@ class HostserviceproblemsummaryQuery extends IdoQuery
             's.service_object_id = so.object_id AND so.is_active = 1',
             array()
         );
-        $this->select->group(array('so.object_id'));
+        $this->select->group(array('s.host_object_id'));
         $this->joinedVirtualTables['services'] = true;
     }
 
