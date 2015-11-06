d7a9a55ad608995bfdee8284403cac75cf2072e0
Thomas Gelf <thomas@gelf.net>
2015-11-06 01:52:04 +0100

    style/guide: add js to get rid of styles

---
+++ modules/doc/public/js/module.js
@@ -0,0 +1,29 @@
+
+(function(Icinga) {
+
+    var Doc = function(module) {
+        this.module = module;
+        this.initialize();
+        this.module.icinga.logger.debug('Doc module loaded');
+    };
+
+    Doc.prototype = {
+
+        initialize: function()
+        {
+            this.module.on('rendered',     this.rendered);
+            this.module.icinga.logger.debug('Doc module initialized');
+        },
+
+        rendered: function(event) {
+            var $container = $(event.currentTarget);
+            if ($('> .content.styleguide', $container).length) {
+                $container.removeClass('module-doc');
+            }
+        }
+    };
+
+    Icinga.availableModules.doc = Doc;
+
+}(Icinga));
+
