92e3895c945f18d62a141d0c1a6478117bdee50e
Eric Lippmann <eric.lippmann@netways.de>
2015-11-10 14:05:48 +0100

    Revert user menu to its original state
    refs #5543

--- library/Icinga/Application/Web.php
+++ library/Icinga/Application/Web.php
@@ -292,19 +292,20 @@ class Web extends EmbeddedWeb
                     'cssClass'  => 'user-nav-item',
                     'label'     => $this->user->getUsername(),
                     'icon'      => 'user',
-                    'url'       => 'preference',
-                    'priority'  => 900
-                ),
-                'logout' => array(
-                    'cssClass'  => 'user-nav-item',
-                    'label'     => t('Logout'),
-                    'icon'	=> 'starttime',
-                    'priority'  => 990,
-                    'renderer'  => array(
-                        'LogoutNavigationItemRenderer',
-                        'target' => '_self'
-                    ),
-                    'url'       => 'authentication/logout'
+                    'priority'  => 900,
+                    'children'  => array(
+                        'preferences' => array(
+                            'label'     => t('Preferences'),
+                            'priority'  => 100,
+                            'url'       => 'preference'
+                        ),
+                        'logout' => array(
+                            'label'     => t('Logout'),
+                            'priority'  => 200,
+                            'target'    => '_self',
+                            'url'       => 'authentication/logout'
+                        )
+                    )
                 )
             );
 
