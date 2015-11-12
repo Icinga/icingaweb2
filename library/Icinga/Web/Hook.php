d903f850dade4f2842d4de32b8263bbd5fd1f725
Thomas Gelf <thomas@gelf.net>
2015-11-12 17:56:32 +0100

    Application\Hook: move existing ones, keep compat

---
+++ library/Icinga/Web/Hook.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Icinga\Web;
+
+use Icinga\Application\Hook as NewHookImplementation;
+
+/**
+ * Icinga Web Hook registry
+ *
+ * Deprecated, please use Icinga\Application\Hook instead
+ */
+class Hook extends NewHookImplementation
+{
+}
--- library/Icinga/Web/Hook/GrapherHook.php
+++ library/Icinga/Web/Hook/GrapherHook.php
@@ -3,109 +3,11 @@
 
 namespace Icinga\Web\Hook;
 
-use Icinga\Exception\ProgrammingError;
-use Icinga\Module\Monitoring\Object\MonitoredObject;
+use Icinga\Application\Hook\GrapherHook as BaseHook;
 
 /**
- * Icinga Web Grapher Hook base class
+ * Deprecated, compat only.
  *
- * Extend this class if you want to integrate your graphing solution nicely into
- * Icinga Web.
+ * Please implement hooks in Icinga\Application\Hook
  */
-abstract class GrapherHook extends WebBaseHook
-{
-    /**
-     * Whether this grapher provides previews
-     *
-     * @var bool
-     */
-    protected $hasPreviews = false;
-
-    /**
-     * Whether this grapher provides tiny previews
-     *
-     * @var bool
-     */
-    protected $hasTinyPreviews = false;
-
-    /**
-     * Constructor must live without arguments right now
-     *
-     * Therefore the constructor is final, we might change our opinion about
-     * this one far day
-     */
-    final public function __construct()
-    {
-        $this->init();
-    }
-
-    /**
-     * Overwrite this function if you want to do some initialization stuff
-     *
-     * @return void
-     */
-    protected function init()
-    {
-    }
-
-    /**
-     * Whether this grapher provides previews
-     *
-     * @return bool
-     */
-    public function hasPreviews()
-    {
-        return $this->hasPreviews;
-    }
-
-    /**
-     * Whether this grapher provides tiny previews
-     *
-     * @return bool
-     */
-    public function hasTinyPreviews()
-    {
-        return $this->hasTinyPreviews;
-    }
-
-    /**
-     * Whether a graph for the monitoring object exist
-     *
-     * @param   MonitoredObject $object
-     *
-     * @return  bool
-     */
-    abstract public function has(MonitoredObject $object);
-
-    /**
-     * Get a preview for the given object
-     *
-     * This function must return an empty string if no graph exists.
-     *
-     * @param   MonitoredObject $object
-     *
-     * @return  string
-     * @throws  ProgrammingError
-     *
-     */
-    public function getPreviewHtml(MonitoredObject $object)
-    {
-        throw new ProgrammingError('This hook provide previews but it is not implemented');
-    }
-
-
-    /**
-     * Get a tiny preview for the given object
-     *
-     * This function must return an empty string if no graph exists.
-     *
-     * @param   MonitoredObject $object
-     *
-     * @return  string
-     * @throws  ProgrammingError
-     */
-    public function getTinyPreviewHtml(MonitoredObject $object)
-    {
-        throw new ProgrammingError('This hook provide tiny previews but it is not implemented');
-    }
-}
+abstract class GrapherHook extends BaseHook {}
