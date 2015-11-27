3a22168b64a89bea9566d8d1386fab00138b2693
Eric Lippmann <eric.lippmann@netways.de>
2015-11-27 16:51:19 +0100

    Handle no-cache control in JavaScript and StyleSheet, not in FileCache

--- library/Icinga/Web/JavaScript.php
+++ library/Icinga/Web/JavaScript.php
@@ -55,6 +55,13 @@ class JavaScript
         self::send();
     }
 
+    /**
+     * Send the client side script code to the client
+     *
+     * Does not cache the client side script code if the HTTP header Cache-Control or Pragma is set to no-cache.
+     *
+     * @param   bool    $minified   Whether to compress the client side script code
+     */
     public static function send($minified = false)
     {
         header('Content-Type: application/javascript');
@@ -86,7 +93,10 @@ class JavaScript
         }
         $files = array_merge($vendorFiles, $jsFiles);
 
-        if ($etag = FileCache::etagMatchesFiles($files)) {
+        $request = Icinga::app()->getRequest();
+        $noCache = $request->getHeader('Cache-Control') === 'no-cache' || $request->getHeader('Pragma') === 'no-cache';
+
+        if (! $noCache && FileCache::etagMatchesFiles($files)) {
             header("HTTP/1.1 304 Not Modified");
             return;
         } else {
@@ -98,7 +108,7 @@ class JavaScript
 
         $cacheFile = 'icinga-' . $etag . $min . '.js';
         $cache = FileCache::instance();
-        if ($cache->has($cacheFile)) {
+        if (! $noCache && $cache->has($cacheFile)) {
             $cache->send($cacheFile);
             return;
         }
