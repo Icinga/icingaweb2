23b51e8b52654717af3b617aabf99aa7888ac4ac
Eric Lippmann <eric.lippmann@netways.de>
2015-12-23 14:07:19 +0100

    Re-introduce getValues() override for subform values in wizard pages
    Wizard pages don't subclass ConfigForm for which the patch was intended. Sorry for the bug.
    
    refs #10905

--- modules/setup/application/forms/UserGroupBackendPage.php
+++ modules/setup/application/forms/UserGroupBackendPage.php
@@ -129,4 +129,19 @@ class UserGroupBackendPage extends Form
             )
         );
     }
+
+    /**
+     * Retrieve all form element values
+     *
+     * @param   bool    $suppressArrayNotation  Ignored
+     *
+     * @return  array
+     */
+    public function getValues($suppressArrayNotation = false)
+    {
+        $values = parent::getValues();
+        $values = array_merge($values, $values['backend_form']);
+        unset($values['backend_form']);
+        return $values;
+    }
 }
