2e1e0bb89f85fb71fd466ed91aeeaae2925466bb
Eric Lippmann <eric.lippmann@netways.de>
2016-01-25 07:32:18 +0100

    Setup/Monitoring: Remove obsolete sockets requirement
    fixes #10805

--- modules/monitoring/library/Monitoring/MonitoringWizard.php
+++ modules/monitoring/library/Monitoring/MonitoringWizard.php
@@ -172,18 +172,6 @@ class MonitoringWizard extends Wizard implements SetupWizard
     public function getRequirements()
     {
         $set = new RequirementSet();
-
-        // TODO(8254): Add this to the $backendSet
-        $set->add(new PhpModuleRequirement(array(
-            'optional'      => true,
-            'condition'     => 'Sockets',
-            'description'   => mt(
-                'monitoring',
-                'In case it\'s desired that a TCP connection is being used by Icinga Web 2 to'
-                . ' access a Livestatus interface, the Sockets module for PHP is required.'
-            )
-        )));
-
         $backendSet = new RequirementSet(false, RequirementSet::MODE_OR);
         $mysqlSet = new RequirementSet(true);
         $mysqlSet->add(new PhpModuleRequirement(array(
