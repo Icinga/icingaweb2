78c1e17c20ffce4f27b0171d9143fb2f51de2369
Eric Lippmann <eric.lippmann@netways.de>
2016-01-27 09:14:11 +0100

    lib/Response: Allow to control the auto-refresh interval

--- library/Icinga/Web/Response.php
+++ library/Icinga/Web/Response.php
@@ -13,6 +13,13 @@ use Icinga\Web\Response\JsonResponse;
 class Response extends Zend_Controller_Response_Http
 {
     /**
+     * Auto-refresh interval
+     *
+     * @var int
+     */
+    protected $autoRefreshInterval;
+
+    /**
      * Set of cookies which are to be sent to the client
      *
      * @var CookieSet
@@ -46,6 +53,29 @@ class Response extends Zend_Controller_Response_Http
      * @var bool
      */
     protected $rerenderLayout = false;
+
+    /**
+     * Get the auto-refresh interval
+     *
+     * @return int
+     */
+    public function getAutoRefreshInterval()
+    {
+        return $this->autoRefreshInterval;
+    }
+
+    /**
+     * Set the auto-refresh interval
+     *
+     * @param   int $autoRefreshInterval
+     *
+     * @return  $this
+     */
+    public function setAutoRefreshInterval($autoRefreshInterval)
+    {
+        $this->autoRefreshInterval = $autoRefreshInterval;
+        return $this;
+    }
 
     /**
      * Get the set of cookies which are to be sent to the client
@@ -203,6 +233,9 @@ class Response extends Zend_Controller_Response_Http
             }
             if ($this->isReloadCss()) {
                 $this->setHeader('X-Icinga-Reload-Css', 'now', true);
+            }
+            if (($autoRefreshInterval = $this->getAutoRefreshInterval()) !== null) {
+                $this->setHeader('X-Icinga-Refresh', $autoRefreshInterval, true);
             }
         } else {
             if ($redirectUrl !== null) {
