c7443924bd94a08952369ecc635e2653e40defeb
Johannes Meyer <johannes.meyer@netways.de>
2016-02-22 11:13:23 +0100

    NavigationItemRenderer: Ignore target for '#' urls
    fixes #11197

--- library/Icinga/Web/Navigation/Renderer/NavigationItemRenderer.php
+++ library/Icinga/Web/Navigation/Renderer/NavigationItemRenderer.php
@@ -218,7 +218,7 @@ class NavigationItemRenderer
     protected function renderTargetAttribute()
     {
         $target = $this->getItem()->getTarget();
-        if ($target === null) {
+        if ($target === null || $this->getItem()->getUrl()->getAbsoluteUrl() == '#') {
             return '';
         }
 
