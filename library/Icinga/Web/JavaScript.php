0c146ac895d376df71f97d714df488a11c24f3c2
Florian Strohmaier <hello@florianstrohmaier.com>
2016-03-08 15:10:19 +0100

    Load collapsible-container behavior

--- library/Icinga/Web/JavaScript.php
+++ library/Icinga/Web/JavaScript.php
@@ -31,7 +31,8 @@ class JavaScript
         'js/icinga/behavior/navigation.js',
         'js/icinga/behavior/form.js',
         'js/icinga/behavior/actiontable.js',
-        'js/icinga/behavior/selectable.js'
+        'js/icinga/behavior/selectable.js',
+        'js/icinga/behavior/collapsible-container.js'
     );
 
     protected static $vendorFiles = array(
