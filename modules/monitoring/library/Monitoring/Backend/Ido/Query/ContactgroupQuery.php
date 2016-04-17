f66daa622c2987f92c51c5ffcecdb9f87e3f7bb1
Eric Lippmann <eric.lippmann@netways.de>
2016-04-18 00:59:19 +0200

    Fix unwanted change of ContactgroupQuery::$groupBase
    Oops!
    
    fixes #11598

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/ContactgroupQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/ContactgroupQuery.php
@@ -16,7 +16,7 @@ class ContactgroupQuery extends IdoQuery
     /**
      * {@inheritdoc}
      */
-    protected $groupBase = array('contactgroups' => array('cg.contactgroup_id'));
+    protected $groupBase = array('contactgroups' => array('cg.contactgroup_id', 'cgo.object_id'));
 
     /**
      * {@inheritdoc}
