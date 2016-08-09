c16dde8246efae0d27d7861ea0b9abe886bcadd7
Eric Lippmann <eric.lippmann@netways.de>
2016-08-09 15:23:03 +0200

    IdoOpt: Remove no longer used columns from the Servicegroupsummary data view
    refs #12732

--- modules/monitoring/library/Monitoring/DataView/Servicegroupsummary.php
+++ modules/monitoring/library/Monitoring/DataView/Servicegroupsummary.php
@@ -17,37 +17,14 @@ class Servicegroupsummary extends DataView
             'servicegroup_alias',
             'servicegroup_name',
             'services_critical_handled',
-            'services_critical_handled_last_state_change',
             'services_critical_unhandled',
-            'services_critical_unhandled_last_state_change',
             'services_ok',
-            'services_ok_last_state_change',
             'services_pending',
-            'services_pending_last_state_change',
             'services_total',
             'services_unknown_handled',
-            'services_unknown_handled_last_state_change',
             'services_unknown_unhandled',
-            'services_unknown_unhandled_last_state_change',
             'services_warning_handled',
-            'services_warning_handled_last_state_change',
-            'services_warning_unhandled',
-            'services_warning_unhandled_last_state_change'
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getStaticFilterColumns()
-    {
-        return array(
-            'instance_name',
-            'services_severity',
-            'host', 'host_alias', 'host_display_name', 'host_name',
-            'hostgroup', 'hostgroup_alias', 'hostgroup_name',
-            'service', 'service_description', 'service_display_name',
-            'servicegroup'
+            'services_warning_unhandled'
         );
     }
 
@@ -67,21 +44,17 @@ class Servicegroupsummary extends DataView
         return array(
             'servicegroup_alias' => array(
                 'order' => self::SORT_ASC
-            ),
-            'services_severity' => array(
-                'columns' => array(
-                    'services_severity',
-                    'servicegroup_alias ASC'
-                ),
-                'order' => self::SORT_DESC
-            ),
-            'services_total' => array(
-                'columns' => array(
-                    'services_total',
-                    'servicegroup_alias ASC'
-                ),
-                'order' => self::SORT_ASC
             )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStaticFilterColumns()
+    {
+        return array(
+            'host_name', 'hostgroup_name', 'service_description'
         );
     }
 }
