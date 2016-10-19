a387f0524fe85a98d70570345d58221868b193d5
Alexander A. Klimov <alexander.klimov@icinga.com>
2016-10-19 15:29:51 +0200

    Add log writer PhpWriter
    refs #11652

---
+++ library/Icinga/Application/Logger/Writer/PhpWriter.php
@@ -0,0 +1,22 @@
+<?php
+/* Icinga Web 2 | (c) 2016 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Application\Logger\Writer;
+
+use Icinga\Application\Logger;
+use Icinga\Application\Logger\LogWriter;
+
+/**
+ * Log to the webserver log, a file or syslog
+ *
+ * @see https://secure.php.net/manual/en/errorfunc.configuration.php#ini.error-log
+ */
+class PhpWriter extends LogWriter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function log($severity, $message) {
+        error_log(Logger::$levels[$severity] . ' - ' . str_replace("\n", '    ', $message));
+    }
+}
