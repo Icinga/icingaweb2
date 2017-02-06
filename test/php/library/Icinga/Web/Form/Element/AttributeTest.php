9b0d4f0bf19f678eeb6bcda06b996f3c91550e7e
Noah Hilverling <noah.hilverling@icinga.com>
2017-02-06 13:16:22 +0100

    Add tests for form element attributes
    refs #594

---
+++ test/php/library/Icinga/Web/Form/Element/AttributeTest.php
@@ -0,0 +1,61 @@
+<?php
+/* Icinga Web 2 | (c) 2017 Icinga Development Team | GPLv2+ */
+
+namespace Tests\Icinga\Web\Form\Element;
+
+use Icinga\Web\Form;
+use Icinga\Test\BaseTestCase;
+
+class AttributeTest extends BaseTestCase
+{
+    public function testWhetherDefaultValuesAreStoredCorrectly()
+    {
+        $form = new Form();
+        $form->addElement(
+            'text',
+            'test',
+            array(
+                'value' => 'testvalue'
+            )
+        );
+
+        $this->assertEquals(
+            'testvalue',
+            $form->getDefaultValue('test'),
+            'Form: Default values are not stored correctly'
+        );
+    }
+
+    /**
+     * @depends testWhetherDefaultValuesAreCachedCorrectly
+     */
+    public function testWhetherDefaultValuesAreIgnoredCorrectlyInMethodGetValues()
+    {
+        $form = new Form();
+        $form->addElement(
+            'text',
+            'test',
+            array(
+                'value'         => 'testvalue',
+                'ignoreDefault' => true
+            )
+        );
+        $form->addElement(
+            'text',
+            'test2',
+            array(
+                'value' => 'testvalue2'
+            )
+        );
+
+        $this->assertNull(
+            $form->getValues()['test'],
+            'Form: getValues does not ignore default values correctly'
+        );
+        $this->assertEquals(
+            'testvalue2',
+            $form->getValues()['test2'],
+            'Form: getValues should not ignore default values without attribute ignoreDefault'
+        );
+    }
+}
