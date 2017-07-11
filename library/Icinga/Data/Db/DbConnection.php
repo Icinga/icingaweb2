a9e681478523b15bd5db05e75b2df5e20c32ebc0
Eric Lippmann <eric.lippmann@icinga.com>
2017-07-11 17:19:16 +0200

    Revert "DbConnection: Fix "Driver does not support setting attributes" for the pdolib extension"
    This reverts commit 3e743a8799d5720c19e23c9e5e56eec42103ca29.
    
    Referenced the wrong issue number.

--- library/Icinga/Data/Db/DbConnection.php
+++ library/Icinga/Data/Db/DbConnection.php
@@ -139,14 +139,7 @@ class DbConnection implements Selectable, Extensible, Updatable, Reducible, Insp
         switch ($this->dbType) {
             case 'mssql':
                 $adapter = 'Pdo_Mssql';
-                $pdoType = $this->config->get('pdoType', 'dblib');
-                if ($pdoType === 'dblib') {
-                    // Driver does not support setting attributes
-                    unset($adapterParamaters['persistent']);
-                    unset($adapterParamaters['options']);
-                    unset($adapterParamaters['driver_options']);
-                }
-                $adapterParamaters['pdoType'] = $pdoType;
+                $adapterParamaters['pdoType'] = $this->config->get('pdoType', 'dblib');
                 $defaultPort = 1433;
                 break;
             case 'mysql':
