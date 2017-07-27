e42d86a4e185fc4f8d7e38e64f23705177ee1852
Eric Lippmann <eric.lippmann@icinga.com>
2017-07-27 15:03:12 +0200

    Remove unncessary column definitions in the Hostgroupsummary and Servicegroupsummary data views

--- modules/monitoring/library/Monitoring/DataView/Hostgroupsummary.php
+++ modules/monitoring/library/Monitoring/DataView/Hostgroupsummary.php
@@ -8,27 +8,18 @@ namespace Icinga\Module\Monitoring\DataView;
  */
 class Hostgroupsummary extends DataView
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getColumns()
     {
         return array(
             'hostgroup_alias',
             'hostgroup_name',
             'hosts_down_handled',
-            'hosts_down_handled_last_state_change',
             'hosts_down_unhandled',
-            'hosts_down_unhandled_last_state_change',
             'hosts_pending',
-            'hosts_pending_last_state_change',
             'hosts_total',
             'hosts_unreachable_handled',
-            'hosts_unreachable_handled_last_state_change',
             'hosts_unreachable_unhandled',
-            'hosts_unreachable_unhandled_last_state_change',
             'hosts_up',
-            'hosts_up_last_state_change',
             'services_critical_handled',
             'services_critical_unhandled',
             'services_ok',
@@ -41,59 +32,28 @@ class Hostgroupsummary extends DataView
         );
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getStaticFilterColumns()
-    {
-        return array(
-            'instance_name',
-            'hosts_severity',
-            'host', 'host_alias', 'host_display_name', 'host_name',
-            'hostgroup',
-            'service', 'service_description', 'service_display_name',
-            'servicegroup', 'servicegroup_alias', 'servicegroup_name'
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function getSearchColumns()
     {
-        return array('hostgroup_alias');
+        return array('hostgroup_name', 'hostgroup_alias');
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getSortRules()
     {
         return array(
             'hostgroup_alias' => array(
                 'order' => self::SORT_ASC
-            ),
-            'hosts_severity' => array(
-                'columns' => array(
-                    'hosts_severity',
-                    'hostgroup_alias ASC'
-                ),
-                'order' => self::SORT_DESC
-            ),
-            'hosts_total' => array(
-                'columns' => array(
-                    'hosts_total',
-                    'hostgroup_alias ASC'
-                ),
-                'order' => self::SORT_ASC
-            ),
-            'services_total' => array(
-                'columns' => array(
-                    'services_total',
-                    'hostgroup_alias ASC'
-                ),
-                'order' => self::SORT_ASC
             )
+        );
+    }
+
+    public function getStaticFilterColumns()
+    {
+        return array(
+            'instance_name',
+            'host', 'host_alias', 'host_display_name', 'host_name',
+            'hostgroup',
+            'service', 'service_description', 'service_display_name',
+            'servicegroup', 'servicegroup_alias', 'servicegroup_name'
         );
     }
 }
--- modules/monitoring/library/Monitoring/DataView/Servicegroupsummary.php
+++ modules/monitoring/library/Monitoring/DataView/Servicegroupsummary.php
@@ -8,37 +8,37 @@ namespace Icinga\Module\Monitoring\DataView;
  */
 class Servicegroupsummary extends DataView
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getColumns()
     {
         return array(
             'servicegroup_alias',
             'servicegroup_name',
             'services_critical_handled',
-            'services_critical_handled_last_state_change',
             'services_critical_unhandled',
-            'services_critical_unhandled_last_state_change',
             'services_ok',
-            'services_ok_last_state_change',
             'services_pending',
-            'services_pending_last_state_change',
             'services_total',
             'services_unknown_handled',
-            'services_unknown_handled_last_state_change',
             'services_unknown_unhandled',
-            'services_unknown_unhandled_last_state_change',
             'services_warning_handled',
-            'services_warning_handled_last_state_change',
-            'services_warning_unhandled',
-            'services_warning_unhandled_last_state_change'
+            'services_warning_unhandled'
         );
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    public function getSearchColumns()
+    {
+        return array('servicegroup_name', 'servicegroup_alias');
+    }
+
+    public function getSortRules()
+    {
+        return array(
+            'servicegroup_alias' => array(
+                'order' => self::SORT_ASC
+            )
+        );
+    }
+
     public function getStaticFilterColumns()
     {
         return array(
@@ -48,40 +48,6 @@ class Servicegroupsummary extends DataView
             'hostgroup', 'hostgroup_alias', 'hostgroup_name',
             'service', 'service_description', 'service_display_name',
             'servicegroup'
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getSearchColumns()
-    {
-        return array('servicegroup_alias');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getSortRules()
-    {
-        return array(
-            'servicegroup_alias' => array(
-                'order' => self::SORT_ASC
-            ),
-            'services_severity' => array(
-                'columns' => array(
-                    'services_severity',
-                    'servicegroup_alias ASC'
-                ),
-                'order' => self::SORT_DESC
-            ),
-            'services_total' => array(
-                'columns' => array(
-                    'services_total',
-                    'servicegroup_alias ASC'
-                ),
-                'order' => self::SORT_ASC
-            )
         );
     }
 }
