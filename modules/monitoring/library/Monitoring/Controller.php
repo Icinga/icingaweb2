7bf2c4bf70839eae2e38b1c6d2894811ebf31b34
Mikesch-mp <Mikesch-mp@koebbes.de>
2017-09-29 19:29:59 +0200

    Update pull request for csv export

--- modules/monitoring/library/Monitoring/Controller.php
+++ modules/monitoring/library/Monitoring/Controller.php
@@ -45,7 +45,12 @@ class Controller extends IcingaWebController
         }
         if ($this->_getParam('format') === 'csv'
             || $this->_request->getHeader('Accept') === 'text/csv') {
-            Csv::fromQuery($query)->dump();
+            $response = $this->getResponse();
+            $response->setHeader('Content-Type', 'text/csv')
+                     ->setHeader('Cache-Control', 'no-proxy')
+                     ->setHeader('Content-Disposition', 'attachment; filename=' . $this->getRequest()->getActionName() . '.csv');
+            $response->appendBody(Csv::fromQuery($query)->dump());
+            $response->sendResponse();
             exit;
         }
     }
