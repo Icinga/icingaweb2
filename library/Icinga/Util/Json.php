414ba16d8aa971f5fe42126986666199526ecfb7
Markus Frosch <markus.frosch@icinga.com>
2017-10-06 12:42:34 +0200

    Util/Json: Make encode compatible to PHP < 5.5

--- library/Icinga/Util/Json.php
+++ library/Icinga/Util/Json.php
@@ -23,7 +23,13 @@ class Json
      */
     public static function encode($value, $options = 0, $depth = 512)
     {
-        $encoded = json_encode($value, $options, $depth);
+        if (version_compare(phpversion(), '5.4.0', '<')) {
+            $encoded = json_encode($value);
+        } else if (version_compare(phpversion(), '5.5.0', '<')) {
+            $encoded = json_encode($value, $options);
+        } else {
+            $encoded = json_encode($value, $options, $depth);
+        }
         if (json_last_error() !== JSON_ERROR_NONE) {
             throw new JsonEncodeException('%s: %s', static::lastErrorMsg(), var_export($value, true));
         }
