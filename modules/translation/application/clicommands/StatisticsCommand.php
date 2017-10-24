3e771e0691d0e182d064f9f4f16504809ffd52ee
Jennifer Mourek <jennifer.mourek@icinga.com>
2017-10-24 13:52:17 +0200

    StatisticsCommand: Make command more intuitive and easier to use

--- modules/translation/application/clicommands/StatisticsCommand.php
+++ modules/translation/application/clicommands/StatisticsCommand.php
@@ -30,8 +30,10 @@ class StatisticsCommand extends TranslationStatisticsCommand
             $this->fail($this->translate('Options --module and --language cannot be used at the same time.'));
         } elseif ($language) {
             $this->extendShowActionLanguages($language);
-        } else {
+        } elseif ($module) {
             $this->extendShowActionModules($module);
+        } else {
+            $this->extendShowActionModules();
         }
     }
 }
