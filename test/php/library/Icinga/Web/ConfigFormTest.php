f0ad3d5188cf06ecb811f9a9df02107716414aaf
Eric Lippmann <eric.lippmann@icinga.com>
2017-11-09 09:10:57 +0100

    Add more test cases to the transformEmptyValuesToNull() test
    refs #2751

--- test/php/library/Icinga/Web/ConfigFormTest.php
+++ test/php/library/Icinga/Web/ConfigFormTest.php
@@ -11,42 +11,81 @@ class ConfigFormTest extends BaseTestCase
     public function testWhetherTransformEmptyValuesToNullHandlesValuesCorrectly()
     {
         $values = array(
-            'value1' => '',
-            'value2' => 'this is a test',
-            'value3' => array(),
-            'value4' => array('Test1', 'Test2'),
-            'value5' => 0,
-            'value6' => 1
-            );
+            'empty_string'      => '',
+            'example_string'    => 'this is a test',
+            'empty_array'       => array(),
+            'example_array'     => array('test1', 'test2'),
+            'zero_as_int'       => 0,
+            'one_as_int'        => 1,
+            'zero_as_string'    => '0',
+            'one_as_string'     => '1',
+            'bool_true'         => true,
+            'bool_false'        => false,
+            'null'              => null
+        );
+
         $values = ConfigForm::transformEmptyValuesToNull($values);
 
         $this->assertNull(
-            $values['value1'],
-            'ConfigForm::transformEmptyValuesToNull does not handle empty strings correctly'
+            $values['empty_string'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle empty strings correctly'
         );
-        $this->assertEquals(
+
+        $this->assertSame(
             'this is a test',
-            $values['value2'],
-            'ConfigForm::transformEmptyValuesToNull does not handle strings correctly'
+            $values['example_string'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle strings correctly'
         );
+
         $this->assertNull(
-            $values['value3'],
-            'ConfigForm::transformEmptyValuesToNull does not handle empty arrays correctly'
+            $values['empty_array'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle empty arrays correctly'
         );
-        $this->assertEquals(
-            'Test1',
-            $values['value4'][0],
-            'ConfigForm::transformEmptyValuesToNull does not handle arrays correctly'
+
+        $this->assertSame(
+            'test1',
+            $values['example_array'][0],
+            'ConfigForm::transformEmptyValuesToNull() does not handle arrays correctly'
         );
-        $this->assertEquals(
+
+        $this->assertSame(
             0,
-            $values['value5'],
-            'ConfigForm::transformEmptyValuesToNull does not handle zeros correctly'
+            $values['zero_as_int'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle zeros correctly'
         );
-        $this->assertEquals(
+
+        $this->assertSame(
             1,
-            $values['value6'],
-            'ConfigForm::transformEmptyValuesToNull does not handle numbers correctly'
+            $values['one_as_int'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle numbers correctly'
+        );
+
+        $this->assertSame(
+            '0',
+            $values['zero_as_string'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle zeros correctly'
+        );
+
+        $this->assertSame(
+            '1',
+            $values['one_as_string'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle numbers correctly'
+        );
+
+        $this->assertSame(
+            true,
+            $values['bool_true'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle bool true correctly'
+        );
+
+        $this->assertNull(
+            $values['bool_false'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle bool false correctly'
+        );
+
+        $this->assertNull(
+            $values['null'],
+            'ConfigForm::transformEmptyValuesToNull() does not handle null correctly'
         );
     }
 }
