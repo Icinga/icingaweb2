590297ff993e013d2f5639828704199d25e3011e
Alexander A. Klimov <alexander.klimov@icinga.com>
2017-11-17 18:02:03 +0100

    Add CRUD for root CA certificates
    refs #3016

--- application/controllers/TlsrootcacollectionController.php
+++ application/controllers/TlsrootcacollectionController.php
@@ -6,8 +6,10 @@ namespace Icinga\Controllers;
 use Exception;
 use Icinga\Application\Icinga;
 use Icinga\File\Storage\LocalFileStorage;
+use Icinga\Forms\Config\Tls\RootCaCollection\AddCaForm;
 use Icinga\Forms\Config\Tls\RootCaCollection\CreateForm;
 use Icinga\Forms\Config\Tls\RootCaCollection\EditForm;
+use Icinga\Forms\Config\Tls\RootCaCollection\RemoveCaForm;
 use Icinga\Forms\ConfirmRemovalForm;
 use Icinga\Web\Controller;
 use Icinga\Web\Notification;
@@ -40,18 +42,58 @@ class TlsrootcacollectionController extends Controller
 
     public function editAction()
     {
-        $this->view->form = $form = new EditForm();
+        $this->view->editForm = $editForm = new EditForm();
         $name = $this->params->getRequired('name');
-        $form->setOldName($name)
+        $editForm->setOldName($name)
             ->setRedirectUrl('tlsrootcacollection/edit')
             ->handleRequest();
+
+        $this->view->addCaForm = $addCaForm = new AddCaForm();
+        $addCaForm->setCollectionName($name)
+            ->setRedirectUrl('tlsrootcacollection/edit')
+            ->handleRequest();
+
+        preg_match_all(
+            '/-+BEGIN CERTIFICATE-+.+?-+END CERTIFICATE-+/s',
+            LocalFileStorage::common('tls/rootcacollections')->read(bin2hex($name) . '.pem'),
+            $certs
+        );
+
+        $certs = $certs[0];
+
+        foreach ($certs as & $cert) {
+            $sha256 = openssl_x509_fingerprint($cert, 'sha256');
+            $cert = array(
+                'info'          => openssl_x509_parse($cert),
+                'sha1'          => openssl_x509_fingerprint($cert, 'sha1'),
+                'sha256'        => $sha256,
+                'removalForm'   => new RemoveCaForm(array(
+                    'collectionName'    => $name,
+                    'certBySha256'      => $sha256,
+                    'redirectUrl'       => 'tlsrootcacollection/edit'
+                ))
+            );
+
+            $cert['removalForm']->handleRequest();
+        }
+
+        usort($certs, function($a, $b) {
+            $a = $a['info']['name'];
+            $b = $b['info']['name'];
+
+            if ($a < $b) {
+                return -1;
+            }
+
+            return $a > $b ? 1 : 0;
+        });
+
+        $this->view->certs = $certs;
 
         $this->addTitleTab(
             $this->translate('Edit Certificate Collection'),
             sprintf($this->translate('Edit TLS Root CA Certificate Collection "%s"'), $name)
         );
-
-        $this->render('form');
     }
 
     public function removeAction()
