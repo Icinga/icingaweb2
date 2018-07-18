7d47875241ff86ea6795634a058c20394672c2a9
Eric Lippmann <eric.lippmann@icinga.com>
2018-07-18 14:16:04 +0200

    Introduce PdfexportHook

---
+++ library/Icinga/Application/Hook/PdfexportHook.php
@@ -0,0 +1,25 @@
+<?php
+/* Icinga Web 2 | (c) 2018 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Application\Hook;
+
+/**
+ * Base class for the PDF Export Hook
+ */
+abstract class PdfexportHook
+{
+    /**
+     * Get whether PDF export is supported
+     *
+     * @return  bool
+     */
+    abstract public function isSupported();
+
+    /**
+     * Render the specified HTML to PDF and stream it to the client
+     *
+     * @param   string  $html       The HTML to render to PDF
+     * @param   string  $filename   The filename for the generated PDF
+     */
+    abstract public function streamPdfFromHtml($html, $filename);
+}
