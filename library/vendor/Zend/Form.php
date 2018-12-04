113a10481ba5c60c16a3f6740c4ba925920a4947
Markus Frosch <markus.frosch@icinga.com>
2018-12-04 16:43:30 +0100

    vendor: Update zf1 for PHP 7.3

--- library/vendor/Zend/Form.php
+++ library/vendor/Zend/Form.php
@@ -1159,7 +1159,7 @@ class Zend_Form implements Iterator, Countable, Zend_Validate_Interface
                 } else {
                     switch ($argc) {
                         case 0:
-                            continue;
+                            continue 2;
                         case (1 <= $argc):
                             $type = array_shift($spec);
                         case (2 <= $argc):
@@ -1664,7 +1664,7 @@ class Zend_Form implements Iterator, Countable, Zend_Validate_Interface
      */
     public function addSubForms(array $subForms)
     {
-        foreach ($subForms as $key => $spec) {          
+        foreach ($subForms as $key => $spec) {
             $name = (string) $key;
             if ($spec instanceof Zend_Form) {
                 $this->addSubForm($spec, $name);
@@ -1676,7 +1676,7 @@ class Zend_Form implements Iterator, Countable, Zend_Validate_Interface
                 $order = null;
                 switch ($argc) {
                     case 0:
-                        continue;
+                        continue 2;
                     case (1 <= $argc):
                         $subForm = array_shift($spec);
 
