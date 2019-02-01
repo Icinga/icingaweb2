ed7ecb32a54f0a2e9e3292db9253063653fcb319
Johannes Meyer <johannes.meyer@icinga.com>
2019-02-01 10:42:58 +0100

    IniParser: Properly unescape special characters in section names and option values
    refs #3648
    
    Signed-off-by: Eric Lippmann <eric.lippmann@icinga.com>

--- test/php/library/Icinga/File/Ini/IniParserTest.php
+++ test/php/library/Icinga/File/Ini/IniParserTest.php
@@ -3,7 +3,6 @@
 
 namespace Tests\Icinga\Config;
 
-use Icinga\File\Ini\Dom\Document;
 use Icinga\File\Ini\IniWriter;
 use Icinga\Test\BaseTestCase;
 use Icinga\Application\Config;
@@ -28,22 +27,45 @@ class IniParserTest extends BaseTestCase
     public function testSectionNameEscaping()
     {
         $config = <<<'EOD'
-[title with \]bracket]
-key1 = "1"
-key2 = "2"
-
 [title with \"quote]
-key1 = "1"
-key2 = "2"
+
+[title with \;semicolon]
+
+[title with \\backslash]
 EOD;
+
         $doc = IniParser::parseIni($config);
         $this->assertTrue(
-            $doc->hasSection('title with ]bracket'),
-            'IniParser does not recognize escaped bracket in section'
+            $doc->hasSection('title with "quote'),
+            'IniParser::parseIni does not recognize escaped quotes in section names'
         );
         $this->assertTrue(
-            $doc->hasSection('title with "quote'),
-            'IniParser does not recognize escaped quote in section'
+            $doc->hasSection('title with ;semicolon'),
+            'IniParser::parseIni does not recognize escaped semicolons in section names'
+        );
+        $this->assertTrue(
+            $doc->hasSection('title with \\backslash'),
+            'IniParser::parseIni does not recognize escaped backslashes in section names'
+        );
+
+        (new IniWriter(Config::fromArray([
+            'title with "quote'         => [],
+            'title with ;semicolon'     => [],
+            'title with \\backslash'    => []
+        ]), $this->tempFile))->write();
+
+        $configObject = IniParser::parseIniFile($this->tempFile);
+        $this->assertTrue(
+            $configObject->hasSection('title with "quote'),
+            'IniParser::parseIniFile does not recognize escaped quotes in section names'
+        );
+        $this->assertTrue(
+            $configObject->hasSection('title with ;semicolon'),
+            'IniParser::parseIniFile does not recognize escaped semicolons in section names'
+        );
+        $this->assertTrue(
+            $configObject->hasSection('title with \\backslash'),
+            'IniParser::parseIniFile does not recognize escaped backslashes in section names'
         );
     }
 
@@ -52,12 +74,50 @@ EOD;
         $config = <<<'EOD'
 [section]
 key1 = "key with escaped \"quote"
+key2 = "key with escaped backslash \\"
+key3 = "key with escaped backslash followed by quote \\\""
 EOD;
+
         $doc = IniParser::parseIni($config);
         $this->assertEquals(
             'key with escaped "quote',
             $doc->getSection('section')->getDirective('key1')->getValue(),
-            'IniParser does not recognize escaped bracket in section'
+            'IniParser::parseIni does not recognize escaped quotes in values'
+        );
+        $this->assertEquals(
+            'key with escaped backslash \\',
+            $doc->getSection('section')->getDirective('key2')->getValue(),
+            'IniParser::parseIni does not recognize escaped backslashes in values'
+        );
+        $this->assertEquals(
+            'key with escaped backslash followed by quote \\"',
+            $doc->getSection('section')->getDirective('key3')->getValue(),
+            'IniParser::parseIni does not recognize escaped backslashes followed by quotes in values'
+        );
+
+        (new IniWriter(Config::fromArray([
+            'section'   => [
+                'key1'  => 'key with escaped "quote',
+                'key2'  => 'key with escaped backslash \\',
+                'key3'  => 'key with escaped backslash followed by quote \\"'
+            ]
+        ]), $this->tempFile))->write();
+
+        $configObject = IniParser::parseIniFile($this->tempFile);
+        $this->assertEquals(
+            'key with escaped "quote',
+            $configObject->getSection('section')->get('key1'),
+            'IniParser::parseIniFile does not recognize escaped quotes in values'
+        );
+        $this->assertEquals(
+            'key with escaped backslash \\',
+            $configObject->getSection('section')->get('key2'),
+            'IniParser::parseIniFile does not recognize escaped backslashes in values'
+        );
+        $this->assertEquals(
+            'key with escaped backslash followed by quote \\"',
+            $configObject->getSection('section')->get('key3'),
+            'IniParser::parseIniFile does not recognize escaped backslashes followed by quotes in values'
         );
     }
 
