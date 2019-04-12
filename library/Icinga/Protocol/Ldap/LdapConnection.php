12ee9580a9bf1698c3dc8a728afb49eb95d7fea8
ignasr <ignas.linux@gmail.com>
2019-04-12 08:57:52 +0200

    When using LDAPS, check if the Host field has hostnames with ports and use the value from Port field if not.

--- library/Icinga/Protocol/Ldap/LdapConnection.php
+++ library/Icinga/Protocol/Ldap/LdapConnection.php
@@ -1185,6 +1185,9 @@ class LdapConnection implements Selectable, Inspectable
             $ldapUrls = explode(' ', $hostname);
             if (count($ldapUrls) > 1) {
                 foreach ($ldapUrls as & $uri) {
+                    if (preg_match('/:\d+$/', $uri) === 0) {
+                        $uri = $uri . ':' . $this->port;
+                    }
                     if (strpos($uri, '://') === false) {
                         $uri = 'ldaps://' . $uri;
                     }
