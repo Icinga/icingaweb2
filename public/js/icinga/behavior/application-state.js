85a673a1f4cf05fa5258024fd6cbf7ef9ac0cd7d
Florian Strohmaier <florian.strohmaier@icinga.com>
2019-06-05 15:35:27 +0200

    JS: Avoid unused inline css attributes

--- public/js/icinga/behavior/application-state.js
+++ public/js/icinga/behavior/application-state.js
@@ -35,8 +35,8 @@
     ApplicationState.prototype.onSummaryRendered = function(e) {
         var height = $(this).height();
 
-        $('#sidebar').css('bottom', height);
-        $('#main').css('bottom', height);
+        /*$('#sidebar').css('bottom', height);
+        $('#main').css('bottom', height);*/
     };
 
     Icinga.Behaviors.ApplicationState = ApplicationState;
