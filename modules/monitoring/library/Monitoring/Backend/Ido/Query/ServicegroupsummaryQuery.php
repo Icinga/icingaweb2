0e729bb85be67879044cea0dd6f4d510b8e25045
Johannes Meyer <johannes.meyer@icinga.com>
2019-09-25 16:20:13 +0200

    Revert "ServicegroupQuery: Use left joins instead of inner joins"
    This reverts commit 23066d2cc4308bf66cb1600c68cee12479b1a0c7.

--- modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicegroupsummaryQuery.php
+++ modules/monitoring/library/Monitoring/Backend/Ido/Query/ServicegroupsummaryQuery.php
@@ -23,7 +23,7 @@ class ServicegroupsummaryQuery extends IdoQuery
             'services_ok'                                   => 'SUM(CASE WHEN service_state = 0 THEN 1 ELSE 0 END)',
             'services_pending'                              => 'SUM(CASE WHEN service_state = 99 THEN 1 ELSE 0 END)',
             'services_severity'                             => 'MAX(service_severity)',
-            'services_total'                                => 'SUM(CASE WHEN service_state IS NOT NULL THEN 1 ELSE 0 END)',
+            'services_total'                                => 'SUM(1)',
             'services_unknown'                              => 'SUM(CASE WHEN service_state = 3 THEN 1 ELSE 0 END)',
             'services_unknown_handled'                      => 'SUM(CASE WHEN service_state = 3 AND service_handled = 1 THEN 1 ELSE 0 END)',
             'services_unknown_unhandled'                    => 'SUM(CASE WHEN service_state = 3 AND service_handled = 0 THEN 1 ELSE 0 END)',
