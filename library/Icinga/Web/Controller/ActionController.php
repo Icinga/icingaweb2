9fe43dda5f0ea1ccca8665d0e5dbf582ab7b4b68
Johannes Meyer <johannes.meyer@icinga.com>
2020-01-14 09:46:50 +0100

    ActionController: Drop method `handlerBrowserWindows()`

--- library/Icinga/Web/Controller/ActionController.php
+++ library/Icinga/Web/Controller/ActionController.php
@@ -122,7 +122,6 @@ class ActionController extends Zend_Controller_Action
             ->_setInvokeArgs($invokeArgs);
         $this->_helper = new Zend_Controller_Action_HelperBroker($this);
 
-        $this->handlerBrowserWindows();
         $moduleName = $this->getModuleName();
         $this->view->defaultTitle = static::DEFAULT_TITLE;
         $this->view->translationDomain = $moduleName !== 'default' ? $moduleName : 'icinga';
@@ -233,18 +232,6 @@ class ActionController extends Zend_Controller_Action
         }
 
         return $this->window;
-    }
-
-    protected function handlerBrowserWindows()
-    {
-        if ($this->isXhr()) {
-            $id = $this->_request->getHeader('X-Icinga-WindowId', null);
-
-            if ($id === Window::UNDEFINED) {
-                $this->window = new Window($id);
-                $this->_response->setHeader('X-Icinga-WindowId', Window::generateId());
-            }
-        }
     }
 
     protected function reloadCss()
