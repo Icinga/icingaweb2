6c35c0c288b05da6c27db7cbf85dcc6f3f51dcdf
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2020-03-24 14:42:21 +0100

    Remove unnecessary code

--- library/Icinga/Web/RememberMeCookie.php
+++ library/Icinga/Web/RememberMeCookie.php
@@ -14,30 +14,14 @@ use Icinga\Util\Json;
 class RememberMeCookie extends Cookie
 {
 
-    public function __construct($value)
+    public function __construct()
     {
-        parent::__construct('remember-me',$value);
+        parent::__construct('remember-me');
 
         $this->setExpire(60);
         $this->setPath('/');
         $this->setDomain("");
-        $this->setHttpOnly(true);
 
-        // not in use yet
-        /*if (isset($_COOKIE['remember-me'])) {
-            try {
-                $cookie = Json::decode($_COOKIE['remember-me'], true);
-            } catch (JsonDecodeException $e) {
-                Logger::error(
-                    "Can't decode the remember me cookie of user '%s'. An error occurred: %s",
-                    Auth::getInstance()->getUser()->getUsername(),
-                    $e
-                );
-
-                return;
-            }
-
-        }*/
     }
 
 }
