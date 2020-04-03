d04023fb0bbe27331100f6d2fcf1d602ac6d3e36
Yonas Habteab <yonas.habteab@icinga.com>
2020-04-03 15:28:41 +0200

    remove some etmpty lines

--- modules/dashboards/application/controllers/DashletsController.php
+++ modules/dashboards/application/controllers/DashletsController.php
@@ -5,21 +5,22 @@ namespace Icinga\Module\Dashboards\Controllers;
 use GuzzleHttp\Psr7\ServerRequest;
 use Icinga\Module\Dashboards\Form\DashletForm;
 use Icinga\Module\Dashboards\Web\Controller;
+use Icinga\Web\Notification;
 
 class DashletsController extends Controller
 {
-
     public function newAction()
     {
         $this->setTitle('New Dashlet');
 
         $dashletForm = new DashletForm();
-        $this->addContent($dashletForm);
-
-        $dashletForm->handleRequest(ServerRequest::fromGlobals());
 
         $dashletForm->on($dashletForm::ON_SUCCESS, function () {
+            Notification::success('Dashlet created');
             $this->redirectNow('dashboards/dashboards');
         });
+
+        $dashletForm->handleRequest(ServerRequest::fromGlobals());
+        $this->addContent($dashletForm);
     }
 }
