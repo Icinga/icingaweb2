f6fcae07d34e5f98e35b23f451536ce5a3c7ebc0
Eric Lippmann <eric.lippmann@icinga.com>
2020-04-08 20:47:35 +0200

    Add PHPDoc and is_iterable check

--- modules/dashboards/library/Dashboards/Web/Widget/DashboardWidget.php
+++ modules/dashboards/library/Dashboards/Web/Widget/DashboardWidget.php
@@ -2,18 +2,37 @@
 
 namespace Icinga\Module\Dashboards\Web\Widget;
 
+use InvalidArgumentException;
 use ipl\Html\BaseHtmlElement;
+
+use function ipl\Stdlib\get_php_type;
 
 class DashboardWidget extends BaseHtmlElement
 {
+    /** @var iterable Dashlets of the dashboard */
     protected $dashlets;
 
     protected $defaultAttributes = ['class' => 'dashboard content'];
 
     protected $tag = 'div';
 
+    /**
+     * Create a new dashboard widget
+     *
+     * @param iterable $dashlets The dashlets of the dashboard
+     *
+     * @throws InvalidArgumentException If $dashlets is not iterable
+     */
     public function __construct($dashlets)
     {
+        if (! is_iterable($dashlets)) {
+            throw new InvalidArgumentException(sprintf(
+                '%s expects parameter 1 to be iterable, got %s instead',
+                __METHOD__,
+                get_php_type($dashlets)
+            ));
+        }
+
         $this->dashlets = $dashlets;
     }
 
