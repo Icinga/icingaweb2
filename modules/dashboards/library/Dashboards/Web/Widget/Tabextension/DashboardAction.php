b169befd7d1cd3453e8e6274b2f8a9d683a3fc96
Yonas Habteab <yonas.habteab@icinga.com>
2020-04-14 10:43:33 +0200

    introduce DashboardAction to add tabs as Dropdown menu

---
+++ modules/dashboards/library/Dashboards/Web/Widget/Tabextension/DashboardAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Icinga\Module\Dashboards\Web\Widget\Tabextension;
+
+use Icinga\Web\Url;
+use Icinga\Web\Widget\Tabextension\Tabextension;
+use Icinga\Web\Widget\Tabs;
+
+/**
+ * Tabextension that allows to add the current URL to a dashboard
+ *
+ * Displayed as a dropdown field in the tabs
+ */
+class DashboardAction implements Tabextension
+{
+    /**
+     * Applies the dashboard extension to the provided tabset
+     *
+     * @param Tabs $tabs The tabs object to extend with
+     * @throws \Icinga\Exception\ProgrammingError
+     */
+    public function apply(Tabs $tabs)
+    {
+        $tabs->addAsDropdown(
+            'dashboard_add',
+            array(
+                'icon' => 'dashboard',
+                'label' => t('Add Dashlet'),
+                'url' => Url::fromPath('dashboards/dashlets/new'),
+                'urlParams' => array(
+                    'url' => rawurlencode(Url::fromRequest()->getRelativeUrl())
+                )
+            )
+        );
+    }
+}
