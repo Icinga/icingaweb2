6ca7e4de3e43f5399c854868ff0b138fb86a34a9
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2020-04-15 14:56:58 +0200

    Add database trait for global config resource

---
+++ library/Icinga/Common/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Icinga\Common;
+
+use Icinga\Application\Config as IcingaConfig;
+use Icinga\Data\ResourceFactory;
+use ipl\Sql\Config as SqlConfig;
+use ipl\Sql\Connection;
+use PDO;
+
+trait Database
+{
+    protected function getDb()
+    {
+        $config = new SqlConfig(ResourceFactory::getResourceConfig(
+            IcingaConfig::app()->get('global', 'config_resource')
+        ));
+        $config->options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION SQL_MODE='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE"
+                . ",ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
+        ];
+
+        $conn = new Connection($config);
+
+        return $conn;
+    }
+}
