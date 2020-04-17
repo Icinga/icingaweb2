58129a781e78e9affbc819f464c07b2ae2da6fd6
Yonas Habteab <yonas.habteab@icinga.com>
2020-04-17 11:11:51 +0200

    Catch a NotFoundError if dashboard doesn't exist

--- modules/dashboards/application/controllers/IndexController.php
+++ modules/dashboards/application/controllers/IndexController.php
@@ -2,10 +2,12 @@
 
 namespace Icinga\Module\Dashboards\Controllers;
 
+use Icinga\Exception\NotFoundError;
 use Icinga\Module\Dashboards\Common\Database;
 use Icinga\Module\Dashboards\Web\Controller;
 use Icinga\Module\Dashboards\Web\Widget\Tabextension\DashboardAction;
 use Icinga\Module\Dashboards\Web\Widget\DashboardWidget;
+use Icinga\Web\Notification;
 use Icinga\Web\Url;
 use ipl\Sql\Select;
 
@@ -15,11 +17,19 @@ class IndexController extends Controller
 
     public function indexAction()
     {
+        try {
+            $this->createTabsAndAutoActivateDashboard();
+        } catch (\Exception $e) {
+            $this->tabs->extend(new DashboardAction())->disableLegacyExtensions();
+
+            Notification::error('No dashboard found to create a Tab');
+        }
+
         $select = (new Select())
             ->columns('dashlet.name, dashlet.dashboard_id, dashlet.url')
             ->from('dashlet')
             ->join('dashboard d', 'dashlet.dashboard_id = d.id')
-            ->where(['d.id = ?' => $this->createTabsAndAutoActivateDashboard()]);
+            ->where(['d.id = ?' => $this->tabs->getActiveName()]);
 
         $dashlets = $this->getDb()->select($select);
 
@@ -28,13 +38,17 @@ class IndexController extends Controller
 
     /**
      * create Tabs and
-     * activate the first dashboard from the database when the url doesn't have a parameter
+     * activate the first dashboard from the database when the url $params doesn't given
      *
      * @return int
+     *
+     * @throws \Icinga\Exception\NotFoundError      If the database doesn't have a dashboard
      */
     protected function createTabsAndAutoActivateDashboard()
     {
+        $ids = [];
         $tabs = $this->getTabs();
+
         $select = (new Select())
             ->columns('*')
             ->from('dashboard');
