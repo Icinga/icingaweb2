45ca4081693c404b1078af348860b664d332b6f6
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2020-04-17 17:38:03 +0200

    Remove ini file and none option from select list
    This change makes the database selected as default

--- application/forms/Config/General/ApplicationConfigForm.php
+++ application/forms/Config/General/ApplicationConfigForm.php
@@ -78,9 +78,7 @@ class ApplicationConfigForm extends Form
                 'autosubmit'    => true,
                 'label'         => $this->translate('User Preference Storage Type'),
                 'multiOptions'  => array(
-                    'ini'   => $this->translate('File System (INI Files)'),
-                    'db'    => $this->translate('Database'),
-                    'none'  => $this->translate('Don\'t Store Preferences')
+                    'db'    => $this->translate('Database')
                 )
             )
         );
