d29adbfc343bcb9fcae96541f5234cc667037ad8
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2020-04-17 09:43:19 +0200

    Add icingaweb prefix

--- etc/schema/mysql.schema.sql
+++ etc/schema/mysql.schema.sql
@@ -41,15 +41,14 @@ CREATE TABLE `icingaweb_user_preference`(
   PRIMARY KEY (`username`,`section`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE rememberme (
-    id int(10) unsigned NOT NULL AUTO_INCREMENT,
-    username varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-    public_key text NOT NULL,
-    private_key text NOT NULL,
-    expires_in timestamp NULL DEFAULT NULL,
-    ctime timestamp NULL DEFAULT NULL,
-    mtime timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    CONSTRAINT idx_rememberme_username UNIQUE KEY (username)
+CREATE TABLE `icingaweb_rememberme`(
+    `id`  int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `public_key`     text NOT NULL,
+    `private_key`     text NOT NULL,
+    `expires_in`    timestamp NULL DEFAULT NULL,
+    `ctime`    timestamp NULL DEFAULT NULL,
+    `mtime`    timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    CONSTRAINT idx_rememberme_username UNIQUE KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
