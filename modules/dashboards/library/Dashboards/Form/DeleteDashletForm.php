b1c2b1e1613445ef81c553cb9d59cfeb365c44ad
Yonas Habteab <yonas.habteab@icinga.com>
2020-04-22 13:51:49 +0200

    Change code style

--- modules/dashboards/library/Dashboards/Form/DeleteDashletForm.php
+++ modules/dashboards/library/Dashboards/Form/DeleteDashletForm.php
@@ -19,9 +19,8 @@ class DeleteDashletForm extends CompatForm
 
     protected function assemble()
     {
-        $this->add(Html::tag('h1', null,
-            Html::sprintf('Please confirm deletion of dashlet %s', $this->dashlet->name)
-        ));
+        $this->add(Html::tag('h1', null, Html::sprintf('Please confirm deletion of dashlet %s',
+            $this->dashlet->name)));
 
         $this->addElement('input', 'btn_submit', [
             'type' => 'submit',
