ca656a372262396d8f13a5dae7b1c5aa4cb05e7c
Yonas Habteab <yonas.habteab@icinga.com>
2020-04-22 11:27:09 +0200

    Introduce delete dashboards form

---
+++ modules/dashboards/library/Dashboards/Form/DeleteDashboardForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Icinga\Module\Dashboards\Form;
+
+use Icinga\Module\Dashboards\Common\Database;
+use ipl\Html\Html;
+use ipl\Web\Compat\CompatForm;
+
+class DeleteDashboardForm extends CompatForm
+{
+    use Database;
+
+    protected $dashboard;
+
+    public function __construct($dashboard)
+    {
+        $this->dashboard = $dashboard;
+    }
+
+    protected function assemble()
+    {
+        $this->add(Html::tag('h1', null,Html::sprintf('Please confirm deletion of dashboard %s',
+            $this->dashboard->name)));
+
+        $this->addElement('input', 'btn_submit', [
+            'type'  => 'submit',
+            'value' => 'Confirm Removal',
+            'class' => 'btn-primary autofocus'
+        ]);
+    }
+
+    protected function onSuccess()
+    {
+        $this->getDb()->delete('dashlet', ['dashboard_id = ?' => $this->dashboard->id]);
+        $this->getDb()->delete('dashboard', ['id = ?' => $this->dashboard->id]);
+    }
+}
