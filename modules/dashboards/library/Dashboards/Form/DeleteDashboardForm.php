40a22fe1760d927f76814c25808514d3f0b71818
Yonas Habteab <yonas.habteab@icinga.com>
2020-04-23 10:49:11 +0200

    Change code sytle

--- modules/dashboards/library/Dashboards/Form/DeleteDashboardForm.php
+++ modules/dashboards/library/Dashboards/Form/DeleteDashboardForm.php
@@ -19,8 +19,11 @@ class DeleteDashboardForm extends CompatForm
 
     protected function assemble()
     {
-        $this->add(Html::tag('h1', null, Html::sprintf('Please confirm deletion of dashboard %s',
-            $this->dashboard->name)));
+        $this->add(
+            Html::tag('h1',
+                null,
+                Html::sprintf('Please confirm deletion of dashboard %s',
+                    $this->dashboard->name)));
 
         $this->addElement('input', 'btn_submit', [
             'type' => 'submit',
