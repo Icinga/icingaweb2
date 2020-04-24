82c1a46cc0bb70edfbed3f6fe5938084019253f9
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2020-04-24 16:21:25 +0200

    Fix syntax

--- modules/setup/library/Setup/WebWizard.php
+++ modules/setup/library/Setup/WebWizard.php
@@ -244,8 +244,7 @@ class WebWizard extends Wizard implements SetupWizard
                     unset($pageData['setup_config_db_resource']);
                     unset($pageData['setup_config_db_creation']);
                 }
-            }
-            elseif ($authData == null || $authData['type'] == 'external') {
+            } elseif ($authData == null || $authData['type'] == 'external') {
                 unset($pageData['setup_config_db_resource']);
             }
         }
