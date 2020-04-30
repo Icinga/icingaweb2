edc55128fdb670120c98fe9424ac05b4ed815d0a
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2020-04-30 13:34:13 +0200

    Change expires_in to expires at in Schema

--- etc/schema/pgsql-upgrades/2.9.0.sql
+++ etc/schema/pgsql-upgrades/2.9.0.sql
@@ -3,7 +3,7 @@ CREATE TABLE "icingaweb_rememberme" (
   "username"    character varying(254) NOT NULL,
   "public_key"  text NOT NULL,
   "private_key" text NOT NULL,
-  "expires_in"  timestamp NULL DEFAULT NULL,
+  "expires_at"  timestamp NULL DEFAULT NULL,
   "ctime"       timestamp NULL DEFAULT NULL,
   "mtime"       timestamp NULL DEFAULT NULL
 );
