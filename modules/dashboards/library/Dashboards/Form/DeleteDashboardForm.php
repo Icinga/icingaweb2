7454a6c8a056e939f782f1a07d24a3de0b83f934
Yonas Habteab <yonas.habteab@icinga.com>
2020-04-30 16:14:52 +0200

    Add new permission for deleting dashboard

--- modules/dashboards/library/Dashboards/Form/DeleteDashboardForm.php
+++ modules/dashboards/library/Dashboards/Form/DeleteDashboardForm.php
@@ -4,7 +4,6 @@ namespace Icinga\Module\Dashboards\Form;
 
 use Icinga\Authentication\Auth;
 use Icinga\Module\Dashboards\Common\Database;
-use Icinga\Web\Notification;
 use ipl\Html\Html;
 use ipl\Sql\Select;
 use ipl\Web\Compat\CompatForm;
@@ -48,27 +47,32 @@ class DeleteDashboardForm extends CompatForm
 
     protected function onSuccess()
     {
+        if ($this->dashboard->type === 'public' && ! Auth::getInstance()->getUser()->isMemberOf('admin')) {
+            throw new \ErrorException(sprintf(
+                "You don't have a Permission to delete a public dashboard %s.",
+                $this->dashboard->name
+            ));
+        }
+
         $select = (new Select())
-            ->from('users')
-            ->join('user_dashboard d', 'd.user_name = users.name')
-            ->columns('name')
+            ->from('user_dashboard')
             ->where([
-                'd.dashboard_id = ?' => $this->dashboard->id,
-                'users.name = ?' => Auth::getInstance()->getUser()->getUsername()
-                ]);
+                'dashboard_id = ?' => $this->dashboard->id,
+                'user_name = ?' => Auth::getInstance()->getUser()->getUsername()
+            ]);
 
         $user = $this->getDb()->select($select)->fetch();
 
-        if ($user) {
+        if (Auth::getInstance()->getUser()->isMemberOf('admin')) {
             $this->getDb()->delete('dashlet', ['dashboard_id = ?' => $this->dashboard->id]);
             $this->getDb()->delete('user_dashlet', ['user_dashboard_id = ?' => $this->dashboard->id]);
             $this->getDb()->delete('user_dashboard', ['dashboard_id = ?' => $this->dashboard->id]);
             $this->getDb()->delete('dashboard', ['id = ?' => $this->dashboard->id]);
-        } elseif ($this->dashboard->type === 'public') {
+        } elseif ($this->dashboard->type === 'private' && $user) {
+            $this->getDb()->delete('user_dashlet', ['user_dashboard_id = ?' => $this->dashboard->id]);
             $this->getDb()->delete('dashlet', ['dashboard_id = ?' => $this->dashboard->id]);
+            $this->getDb()->delete('user_dashboard', ['dashboard_id = ?' => $this->dashboard->id]);
             $this->getDb()->delete('dashboard', ['id = ?' => $this->dashboard->id]);
-        } else {
-            Notification::error("You don't have a permission to delete this dashboard!");
         }
     }
 }
