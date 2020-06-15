a758642ae2a3c07ef98b94cb7b87a3c9307451f9
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2020-06-15 09:11:22 +0200

    Change expires_in to expires_at

--- etc/schema/rememberme.schema.sql
+++ etc/schema/rememberme.schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE rememberme (
     username varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
     public_key text NOT NULL,
     private_key text NOT NULL,
-    expires_in timestamp NULL DEFAULT NULL,
+    expires_at timestamp NULL DEFAULT NULL,
     ctime timestamp NULL DEFAULT NULL,
     mtime timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
