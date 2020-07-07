7f6438e03c1e33fa780d08ab2cb1716dae9c7250
Johannes Meyer <johannes.meyer@icinga.com>
2020-07-07 16:11:10 +0200

    FilterParseException: Store invalid char and its position

--- library/Icinga/Data/Filter/FilterParseException.php
+++ library/Icinga/Data/Filter/FilterParseException.php
@@ -7,4 +7,25 @@ use Icinga\Exception\IcingaException;
 
 class FilterParseException extends IcingaException
 {
+    protected $char;
+
+    protected $charPos;
+
+    public function __construct($message, $filter, $char, $charPos, ...$additional)
+    {
+        parent::__construct($message, $filter, $char, $charPos, ...$additional);
+
+        $this->char = $char;
+        $this->charPos = $charPos;
+    }
+
+    public function getChar()
+    {
+        return $this->char;
+    }
+
+    public function getCharPos()
+    {
+        return $this->charPos;
+    }
 }
