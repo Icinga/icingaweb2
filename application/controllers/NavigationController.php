2891f64686409dba72ea3c8ec9364be41708cf92
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2020-09-02 09:57:48 +0200

    Add My Devices tab

--- application/controllers/NavigationController.php
+++ application/controllers/NavigationController.php
@@ -145,6 +145,13 @@ class NavigationController extends Controller
                 'label'     => $this->translate('Navigation'),
                 'url'       => 'navigation'
             )
+        )
+        ->add('devices',
+            array(
+                'title' => $this->translate('List of devices you are logged in'),
+                'label' => $this->translate('My Devices'),
+                'url'   => 'mydevices'
+            )
         );
         $this->setupSortControl(
             array(
