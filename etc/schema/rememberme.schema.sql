9815eb248c4c70662c17ec3944467fcacb0efab3
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2020-09-02 10:31:12 +0200

    Change table name and add column http_user_agent

--- etc/schema/rememberme.schema.sql
+++ etc/schema/rememberme.schema.sql
@@ -1,8 +1,9 @@
-CREATE TABLE rememberme (
+CREATE TABLE icingaweb_rememberme (
     id int(10) unsigned NOT NULL AUTO_INCREMENT,
     username varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
     public_key text NOT NULL,
     private_key text NOT NULL,
+    http_user_agent text NOT NULL,
     expires_at timestamp NULL DEFAULT NULL,
     ctime timestamp NULL DEFAULT NULL,
     mtime timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
