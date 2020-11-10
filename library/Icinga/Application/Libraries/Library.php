84c23fe92b0c0d52e1e3dc9078009932a003545a
Johannes Meyer <johannes.meyer@icinga.com>
2020-11-10 15:11:46 +0100

    Library: Add accessors for meta data

--- library/Icinga/Application/Libraries/Library.php
+++ library/Icinga/Application/Libraries/Library.php
@@ -3,9 +3,19 @@
 
 namespace Icinga\Application\Libraries;
 
+use Icinga\Exception\ConfigurationError;
+use Icinga\Exception\Json\JsonDecodeException;
+use Icinga\Util\Json;
+
 class Library
 {
     protected $path;
+
+    /** @var string */
+    protected $version;
+
+    /** @var array */
+    protected $metaData;
 
     /**
      * Create a new Library
@@ -15,6 +25,39 @@ class Library
     public function __construct($path)
     {
         $this->path = $path;
+    }
+
+    /**
+     * Get this library's name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->metaData()['name'];
+    }
+
+    /**
+     * Get this library's version
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        if ($this->version === null) {
+            if (isset($this->metaData()['version'])) {
+                $this->version = trim(ltrim($this->metaData()['version'], 'v'));
+            } else {
+                $versionFile = $this->path . DIRECTORY_SEPARATOR . 'VERSION';
+                if (file_exists($versionFile)) {
+                    $this->version = trim(ltrim(file_get_contents($versionFile), 'v'));
+                } else {
+                    $this->version = '';
+                }
+            }
+        }
+
+        return $this->version;
     }
 
     /**
@@ -28,5 +71,27 @@ class Library
         if (file_exists($autoloaderPath)) {
             require_once $autoloaderPath;
         }
+    }
+
+    /**
+     * Parse and return this library's metadata
+     *
+     * @return array
+     *
+     * @throws ConfigurationError
+     * @throws JsonDecodeException
+     */
+    protected function metaData()
+    {
+        if ($this->metaData === null) {
+            $metaData = file_get_contents($this->path . DIRECTORY_SEPARATOR . 'composer.json');
+            if ($metaData === false) {
+                throw new ConfigurationError('Library at "%s" is not a composerized project', $this->path);
+            }
+
+            $this->metaData = Json::decode($metaData, true);
+        }
+
+        return $this->metaData;
     }
 }
