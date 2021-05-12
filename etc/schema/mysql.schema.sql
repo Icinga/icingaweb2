a5470efab3685b8a350638af26089f44d9a62200
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2021-05-12 14:45:28 +0200

    Update SQL schema for rememberme table

--- etc/schema/mysql.schema.sql
+++ etc/schema/mysql.schema.sql
@@ -42,13 +42,13 @@ CREATE TABLE `icingaweb_user_preference`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `icingaweb_rememberme`(
-  id          int(10) unsigned NOT NULL AUTO_INCREMENT,
-  username    varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  public_key  text NOT NULL,
-  private_key text NOT NULL,
-  expires_at  timestamp NULL DEFAULT NULL,
-  ctime       timestamp NULL DEFAULT NULL,
-  mtime       timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  CONSTRAINT idx_rememberme_username UNIQUE KEY (username)
+  id                int(10) unsigned NOT NULL AUTO_INCREMENT,
+  username          varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+  passphrase        text NOT NULL,
+  random_iv         text NOT NULL,
+  http_user_agent   text NOT NULL,
+  expires_at        timestamp NULL DEFAULT NULL,
+  ctime             timestamp NULL DEFAULT NULL,
+  mtime             timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
