871a6ca275fc9b89b047881a4a8a5bda263af50d
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2021-05-19 12:48:06 +0200

    Change db char length and type

--- etc/schema/mysql.schema.sql
+++ etc/schema/mysql.schema.sql
@@ -44,9 +44,9 @@ CREATE TABLE `icingaweb_user_preference`(
 CREATE TABLE `icingaweb_rememberme`(
   id                int(10) unsigned NOT NULL AUTO_INCREMENT,
   username          varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  passphrase        varchar(254) NOT NULL,
+  passphrase        varchar(256) NOT NULL,
   random_iv         varchar(24) NOT NULL,
-  http_user_agent   varchar(254) NOT NULL,
+  http_user_agent   text NOT NULL,
   expires_at        timestamp NULL DEFAULT NULL,
   ctime             timestamp NULL DEFAULT NULL,
   mtime             timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
--- etc/schema/pgsql.schema.sql
+++ etc/schema/pgsql.schema.sql
@@ -104,9 +104,9 @@ CREATE UNIQUE INDEX idx_icingaweb_user_preference
 CREATE TABLE "icingaweb_rememberme" (
   "id"                  serial,
   "username"            character varying(254) NOT NULL,
-  "passphrase"          character varying(254) NOT NULL,
+  "passphrase"          character varying(256) NOT NULL,
   "random_iv"           character varying(24) NOT NULL,
-  "http_user_agent"     character varying(254) NOT NULL,
+  "http_user_agent"     text NOT NULL,
   "expires_at"          timestamp NULL DEFAULT NULL,
   "ctime"               timestamp NULL DEFAULT NULL,
   "mtime"               timestamp NULL DEFAULT NULL
