cd7ce6ea42eb718b12f15a9cd87b74d56bf2abff
Johannes Meyer <johannes.meyer@icinga.com>
2021-07-12 09:48:23 +0200

    Merge pull request from GHSA-2xv9-886q-p7xx
    Fix that custom variable protection and blacklists can be circumvented
    
    (cherry picked from commit 7abb62976a90e5dcae065a40739ef5ec546094e7)

---
+++ modules/monitoring/library/Monitoring/Data/CustomvarProtectionIterator.php
@@ -0,0 +1,25 @@
+<?php
+/* Icinga Web 2 | (c) 2021 Icinga GmbH | GPLv2+ */
+
+namespace Icinga\Module\Monitoring\Data;
+
+use Icinga\Module\Monitoring\Object\MonitoredObject;
+use IteratorIterator;
+
+class CustomvarProtectionIterator extends IteratorIterator
+{
+    const IS_CV_RE = '~^_(host|service)_([a-zA-Z0-9_]+)$~';
+
+    public function current()
+    {
+        $row = parent::current();
+
+        foreach ($row as $col => $val) {
+            if (preg_match(self::IS_CV_RE, $col, $m)) {
+                $row->$col = MonitoredObject::protectCustomVars([$m[2] => $val])[$m[2]];
+            }
+        }
+
+        return $row;
+    }
+}
