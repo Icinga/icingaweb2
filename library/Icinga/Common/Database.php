aee82ad342b8fad9fb2040cef5457a7273313770
Johannes Meyer <johannes.meyer@icinga.com>
2021-07-26 16:37:11 +0200

    Database: Always use `utf8mb4` as connection charset

--- library/Icinga/Common/Database.php
+++ library/Icinga/Common/Database.php
@@ -31,6 +31,7 @@ trait Database
         $config = new SqlConfig(ResourceFactory::getResourceConfig(
             IcingaConfig::app()->get('global', 'config_resource')
         ));
+        $config->charset = 'utf8mb4';
 
         $config->options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ];
         if ($config->db === 'mysql') {
