fa9431180d0d757eace186a827a3358b00d9a71e
Yonas Habteab <yonas.habteab@icinga.com>
2022-03-11 16:16:51 +0100

    Add DB schema for the enhanced dashboards

--- schema/mysql-upgrades/2.12.0.sql
+++ schema/mysql-upgrades/2.12.0.sql
@@ -1,58 +1,3 @@
-# Icinga Web 2 | (c) 2014 Icinga GmbH | GPLv2+
-
-CREATE TABLE `icingaweb_group`(
-  `id`     int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name`   varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `parent` int(10) unsigned NULL DEFAULT NULL,
-  `ctime`  timestamp NULL DEFAULT NULL,
-  `mtime`  timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_name` (`name`),
-  CONSTRAINT `fk_icingaweb_group_parent_id` FOREIGN KEY (`parent`)
-    REFERENCES `icingaweb_group` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
-CREATE TABLE `icingaweb_group_membership`(
-  `group_id`   int(10) unsigned NOT NULL,
-  `username`   varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ctime`      timestamp NULL DEFAULT NULL,
-  `mtime`      timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`group_id`,`username`),
-  CONSTRAINT `fk_icingaweb_group_membership_icingaweb_group` FOREIGN KEY (`group_id`)
-    REFERENCES `icingaweb_group` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
-CREATE TABLE `icingaweb_user`(
-  `name`          varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `active`        tinyint(1) NOT NULL,
-  `password_hash` varbinary(255) NOT NULL,
-  `ctime`         timestamp NULL DEFAULT NULL,
-  `mtime`         timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
-CREATE TABLE `icingaweb_user_preference`(
-  `username` varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `section`  varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name`     varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value`    varchar(255) NOT NULL,
-  `ctime`    timestamp NULL DEFAULT NULL,
-  `mtime`    timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`username`,`section`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
-CREATE TABLE `icingaweb_rememberme`(
-  id                int(10) unsigned NOT NULL AUTO_INCREMENT,
-  username          varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL,
-  passphrase        varchar(256) NOT NULL,
-  random_iv         varchar(32) NOT NULL,
-  http_user_agent   text NOT NULL,
-  expires_at        timestamp NULL DEFAULT NULL,
-  ctime             timestamp NULL DEFAULT NULL,
-  mtime             timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
-
 # Icinga Web 2 enhanced Dashboards
 
 CREATE TABLE icingaweb_dashboard_owner (
@@ -127,14 +72,3 @@ CREATE TABLE icingaweb_system_dashlet (
   CONSTRAINT fk_icingaweb_system_dashlet_icingaweb_module_dashlet FOREIGN KEY (module_dashlet_id)
     REFERENCES icingaweb_module_dashlet (id) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE icingaweb_schema (
-  id int unsigned NOT NULL AUTO_INCREMENT,
-  version smallint unsigned NOT NULL,
-  timestamp int unsigned NOT NULL,
-
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
-
-INSERT INTO icingaweb_schema (version, timestamp)
-  VALUES (6, UNIX_TIMESTAMP());
