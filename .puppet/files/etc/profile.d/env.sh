09e864475300305f747ae192cbb2745675e7c963
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2022-04-29 09:43:50 +0200

    Remove the Vagrant file and all its assets

--- .puppet/files/etc/profile.d/env.sh
+++
@@ -1,2 +0,0 @@
-export PATH="/opt/rh/rh-php73/root/bin:$PATH"
-export PATH="$PATH:/usr/local/bin"
--- .puppet/modules/monitoring_plugins/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class monitoring_plugins {
-  include epel
-
-  # nagios plugins from epel
-  package { 'nagios-plugins-all':
-    ensure  => latest,
-    require => Class['epel']
-  }
-}
