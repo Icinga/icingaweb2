2adad04bf5f71fac52dc5ed5f2645c3a49a60f3f
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2022-10-20 11:41:30 +0200

    Implement new search field to add user as group member
    * Make AddMemberForm an Ipl form extending `SimpleSearchField`

--- application/forms/Config/UserGroup/AddMemberForm.php
+++ application/forms/Config/UserGroup/AddMemberForm.php
@@ -1,28 +1,18 @@
 <?php
-/* Icinga Web 2 | (c) 2015 Icinga Development Team | GPLv2+ */
+/* Icinga Web 2 | (c) 2015 Icinga GmbH | GPLv2+ */
 
 namespace Icinga\Forms\Config\UserGroup;
 
 use Exception;
 use Icinga\Data\Extensible;
-use Icinga\Data\Filter\Filter;
-use Icinga\Data\Selectable;
-use Icinga\Exception\NotFoundError;
-use Icinga\Web\Form;
 use Icinga\Web\Notification;
+use ipl\Web\Control\SimpleSearchField;
 
 /**
  * Form for adding one or more group members
  */
-class AddMemberForm extends Form
+class AddMemberForm extends SimpleSearchField
 {
-    /**
-     * The data source to fetch users from
-     *
-     * @var Selectable
-     */
-    protected $ds;
-
     /**
      * The user group backend to use
      *
@@ -36,19 +26,6 @@ class AddMemberForm extends Form
      * @var string
      */
     protected $groupName;
-
-    /**
-     * Set the data source to fetch users from
-     *
-     * @param   Selectable  $ds
-     *
-     * @return  $this
-     */
-    public function setDataSource(Selectable $ds)
-    {
-        $this->ds = $ds;
-        return $this;
-    }
 
     /**
      * Set the user group backend to use
@@ -76,74 +53,18 @@ class AddMemberForm extends Form
         return $this;
     }
 
-    /**
-     * Create and add elements to this form
-     *
-     * @param   array   $formData   The data sent by the user
-     */
-    public function createElements(array $formData)
-    {
-        // TODO(jom): Fetching already existing members to prevent the user from mistakenly creating duplicate
-        // memberships (no matter whether the data source permits it or not, a member does never need to be
-        // added more than once) should be kept at backend level (GroupController::fetchUsers) but this does
-        // not work currently as our ldap protocol stuff is unable to handle our filter implementation..
-        $members = $this->backend
-            ->select()
-            ->from('group_membership', array('user_name'))
-            ->where('group_name', $this->groupName)
-            ->fetchColumn();
-        $filter = empty($members) ? Filter::matchAll() : Filter::not(Filter::where('user_name', $members));
-
-        $users = $this->ds->select()->from('user', array('user_name'))->applyFilter($filter)->fetchColumn();
-        if (! empty($users)) {
-            $this->addElement(
-                'multiselect',
-                'user_name',
-                array(
-                    'multiOptions'  => array_combine($users, $users),
-                    'label'         => $this->translate('Backend Users'),
-                    'description'   => $this->translate(
-                        'Select one or more users (fetched from your user backends) to add as group member'
-                    ),
-                    'class'         => 'grant-permissions'
-                )
-            );
-        }
-
-        $this->addElement(
-            'textarea',
-            'users',
-            array(
-                'required'      => empty($users),
-                'label'         => $this->translate('Users'),
-                'description'   => $this->translate(
-                    'Provide one or more usernames separated by comma to add as group member'
-                )
-            )
-        );
-
-        $this->setTitle(sprintf($this->translate('Add members for group %s'), $this->groupName));
-        $this->setSubmitLabel($this->translate('Add'));
-    }
-
-    /**
-     * Insert the members for the group
-     *
-     * @return  bool
-     */
     public function onSuccess()
     {
-        $userNames = $this->getValue('user_name') ?: array();
-        if (($users = $this->getValue('users'))) {
-            $userNames = array_merge($userNames, array_map('trim', explode(',', $users)));
-        }
+        $q = $this->getValue($this->getSearchParameter(), '');
+
+        $userNames = array_unique(array_filter(
+            array_map('trim', explode(self::TERM_SEPARATOR, rawurldecode($q)))
+        ));
 
         if (empty($userNames)) {
-            $this->info($this->translate(
-                'Please provide at least one username, either by choosing one '
-                . 'in the list or by manually typing one in the text box below'
-            ));
-            return false;
+            Notification::info(t('Please provide at least one username'));
+
+            return;
         }
 
         $single = null;
@@ -151,32 +72,28 @@ class AddMemberForm extends Form
             try {
                 $this->backend->insert(
                     'group_membership',
-                    array(
+                    [
                         'group_name'    => $this->groupName,
                         'user_name'     => $userName
-                    )
+                    ]
                 );
-            } catch (NotFoundError $e) {
-                throw $e; // Trigger 404, the group name is initially accessed as GET parameter
             } catch (Exception $e) {
                 Notification::error(sprintf(
-                    $this->translate('Failed to add "%s" as group member for "%s"'),
+                    t('Failed to add "%s" as group member for "%s"'),
                     $userName,
                     $this->groupName
                 ));
-                $this->error($e->getMessage());
-                return false;
+
+                return;
             }
 
             $single = $single === null;
         }
 
         if ($single) {
-            Notification::success(sprintf($this->translate('Group member "%s" added successfully'), $userName));
+            Notification::success(sprintf(t('Group member "%s" added successfully'), $userName));
         } else {
-            Notification::success($this->translate('Group members added successfully'));
+            Notification::success(t('Group members added successfully'));
         }
-
-        return true;
     }
 }
