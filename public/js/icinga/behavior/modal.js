437f1b0f0e3b3bc88b62326c649e934dfab5fa14
Johannes Meyer <johannes.meyer@icinga.com>
2023-01-12 09:53:45 +0100

    Merge pull request #4976 from Icinga/do-not-disable-all-inputs-on-form-submit
    js: Only disable submit buttons on form submit
    (cherry picked from commit 96179182e413b5285ad8be00e7124fd278e300c3)

--- public/js/icinga/behavior/modal.js
+++ public/js/icinga/behavior/modal.js
@@ -114,8 +114,10 @@
             }
         });
 
-        // otherwise the form is submitted several times by clicking the "Submit" button several times
-        $form.find('input:not(:disabled)').prop('disabled', true);
+        if (typeof $autoSubmittedBy === 'undefined') {
+            // otherwise the form is submitted several times by clicking the "Submit" button several times
+            $form.find('input[type=submit],button[type=submit],button:not([type])').prop('disabled', true);
+        }
 
         event.stopPropagation();
         event.preventDefault();
