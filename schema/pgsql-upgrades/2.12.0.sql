21bde13274bf49a96b58b1c780b6dc94ca8baa60
Yonas Habteab <yonas.habteab@icinga.com>
2023-06-21 17:24:32 +0200

    Introduce database models required by migration hooks

---
+++ schema/pgsql-upgrades/2.12.0.sql
@@ -0,0 +1,13 @@
+CREATE TYPE boolenum AS ENUM ('n', 'y');
+
+ALTER TABLE icingaweb_schema
+  ALTER COLUMN timestamp TYPE bigint,
+  ALTER COLUMN version TYPE varchar(64),
+  ADD COLUMN success boolenum DEFAULT NULL,
+  ADD COLUMN reason text DEFAULT NULL,
+  DROP CONSTRAINT IF EXISTS idx_icingaweb_schema_version,
+  ADD CONSTRAINT idx_icingaweb_schema_version UNIQUE (version);
+
+INSERT INTO icingaweb_schema (version, timestamp, success, reason)
+  VALUES('2.12.0', EXTRACT(EPOCH FROM now()) * 1000, 'y', NULL)
+    ON CONFLICT ON CONSTRAINT idx_icingaweb_schema_version DO UPDATE SET timestamp = EXCLUDED.timestamp, success = EXCLUDED.success, reason = EXCLUDED.reason;
