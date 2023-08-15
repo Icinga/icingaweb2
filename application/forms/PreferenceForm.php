eff262cafd7bbe38e0bce3be3b4199a009c06591
raviks789 <33730024+raviks789@users.noreply.github.com>
2023-08-15 10:39:11 +0200

    `PreferenceForm`: Use correct number of arguments for method `Preferences::get()`

--- application/forms/PreferenceForm.php
+++ application/forms/PreferenceForm.php
@@ -100,7 +100,7 @@ class PreferenceForm extends Form
         $defaultTheme = Config::app()->get('themes', 'default', StyleSheet::DEFAULT_THEME);
 
         $this->preferences = new Preferences($this->store ? $this->store->load() : array());
-        $webPreferences = $this->preferences->get('icingaweb', array());
+        $webPreferences = $this->preferences->get('icingaweb');
         foreach ($this->getValues() as $key => $value) {
             if ($value === ''
                 || $value === null
