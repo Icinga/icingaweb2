8f1eaf0c7bb37d5e530baad41431b8aa89094dc8
Johannes Meyer <johannes.meyer@icinga.com>
2024-11-04 16:19:44 +0100

    CustomVarRendererHook: Log an exception's message and location
    fixes #5258

--- modules/monitoring/library/Monitoring/Hook/CustomVarRendererHook.php
+++ modules/monitoring/library/Monitoring/Hook/CustomVarRendererHook.php
@@ -65,7 +65,8 @@ abstract class CustomVarRendererHook
                     $hooks[] = $hook;
                 }
             } catch (Exception $e) {
-                Logger::error('Failed to load hook %s:', get_class($hook), $e);
+                Logger::error('Failed to load hook %s: %s', get_class($hook), $e);
+                Logger::debug($e);
             }
         }
 
@@ -81,7 +82,8 @@ abstract class CustomVarRendererHook
                     $renderedValue = $hook->renderCustomVarValue($key, $value);
                     $group = $hook->identifyCustomVarGroup($key);
                 } catch (Exception $e) {
-                    Logger::error('Failed to use hook %s:', get_class($hook), $e);
+                    Logger::error('Failed to use hook %s: %s', get_class($hook), $e);
+                    Logger::debug($e);
                     continue;
                 }
 
