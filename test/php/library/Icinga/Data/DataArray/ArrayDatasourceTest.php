Icinga/icingaweb2

commit 384d9535a990c23fef3abc73657cb15931c2eba5

ArrayDatasource: Sort result first and then apply limit+offset

library/Icinga/Data/DataArray/ArrayDatasource.php
@@ -196,11 +196,16 @@ class ArrayDatasource implements Selectable
         $filter = $query->getFilter();
         $offset = $query->hasOffset() ? $query->getOffset() : 0;
         $limit = $query->hasLimit() ? $query->getLimit() : 0;
+        $data = $this->data;
+
+        if ($query->hasOrder()) {
+            uasort($data, [$query, 'compare']);
+        }
 
         $foundStringKey = false;
-        $result = array();
+        $result = [];
         $skipped = 0;
-        foreach ($this->data as $key => $row) {
+        foreach ($data as $key => $row) {
             if ($this->keyColumn !== null && !isset($row->{$this->keyColumn})) {
                 $row = clone $row; // Make sure that this won't affect the actual data
                 $row->{$this->keyColumn} = $key;
@@ -239,14 +244,7 @@ class ArrayDatasource implements Selectable
             }
         }
 
-        // Sort the result
-        if ($query->hasOrder()) {
-            if ($foundStringKey) {
-                uasort($result, array($query, 'compare'));
-            } else {
-                usort($result, array($query, 'compare'));
-            }
-        } elseif (! $foundStringKey) {
+        if (! $foundStringKey) {
             $result = array_values($result);
         }
 

test/php/library/Icinga/Data/DataArray/ArrayDatasourceTest.php
@@ -8,40 +8,128 @@ use Icinga\Data\DataArray\ArrayDatasource;
 
 class ArrayDatasourceTest extends BaseTestCase
 {
-    private $sampleData;
+    private $query;
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->sampleData = array(
-            (object) array(
-                'host'    => 'localhost',
+        $this->query = (new ArrayDatasource([
+            (object) [
+                'host'    => 'a host',
                 'problem' => '1',
-                'service' => 'ping',
                 'state'   => '2',
                 'handled' => '1'
-            ),
-            (object) array(
-                'host'    => 'localhost',
+            ],
+            (object) [
+                'host'    => 'b host',
                 'problem' => '1',
-                'service' => 'www.icinga.com',
                 'state'   => '0',
                 'handled' => '0'
-            ),
-            (object) array(
-                'host'    => 'localhost',
+            ],
+            (object) [
+                'host'    => 'c host',
                 'problem' => '1',
-                'service' => 'www.icinga.com',
                 'state'   => '1',
                 'handled' => '0'
-            )
-        );
+            ]
+        ]))->select();
     }
 
     public function testSelectFactory()
     {
-        $ds = new ArrayDatasource($this->sampleData);
-        $query = $ds->select();
-        $this->assertInstanceOf('Icinga\\Data\\SimpleQuery', $query);
+        $this->assertInstanceOf('Icinga\\Data\\SimpleQuery', $this->query);
+    }
+
+    public function testOrderWithOneRuleIsCorrect()
+    {
+        $result = $this->query
+            ->order('host', 'desc')
+            ->fetchAll();
+
+        $this->assertEquals(
+            [
+                (object) [
+                    'host'    => 'c host',
+                    'problem' => '1',
+                    'state'   => '1',
+                    'handled' => '0'
+                ],
+                (object) [
+                    'host'    => 'b host',
+                    'problem' => '1',
+                    'state'   => '0',
+                    'handled' => '0'
+                ],
+                (object) [
+                    'host'    => 'a host',
+                    'problem' => '1',
+                    'state'   => '2',
+                    'handled' => '1'
+                ]
+            ],
+            $result,
+            'ArrayDatasource does not sort queries correctly'
+        );
+    }
+
+    public function testOrderWithTwoRulesIsCorrect()
+    {
+        $result = $this->query
+            ->order('handled', 'asc')
+            ->order('host', 'asc')
+            ->fetchAll();
+
+        $this->assertEquals(
+            [
+                (object) [
+                    'host'    => 'b host',
+                    'problem' => '1',
+                    'state'   => '0',
+                    'handled' => '0'
+                ],
+                (object) [
+                    'host'    => 'c host',
+                    'problem' => '1',
+                    'state'   => '1',
+                    'handled' => '0'
+                ],
+                (object) [
+                    'host'    => 'a host',
+                    'problem' => '1',
+                    'state'   => '2',
+                    'handled' => '1'
+                ]
+            ],
+            $result,
+            'ArrayDatasource does not sort queries correctly'
+        );
+    }
+
+    public function testOrderIsCorrectWithLimitAndOffset()
+    {
+        $result = $this->query
+            ->order('handled', 'asc')
+            ->order('host', 'asc')
+            ->limit(2)
+            ->fetchAll();
+
+        $this->assertEquals(
+            [
+                (object) [
+                    'host'    => 'b host',
+                    'problem' => '1',
+                    'state'   => '0',
+                    'handled' => '0'
+                ],
+                (object) [
+                    'host'    => 'c host',
+                    'problem' => '1',
+                    'state'   => '1',
+                    'handled' => '0'
+                ]
+            ],
+            $result,
+            'ArrayDatasource does not sort limited queries correctly'
+        );
     }
 }

commit 01bcf71e376c305a0845c9d181eaa8f80215b2af

ArrayDatasource: Restore order by key column
Broke with 384d9535a990c23fef3abc73657cb15931c2eba5

library/Icinga/Data/DataArray/ArrayDatasource.php
@@ -196,7 +196,16 @@ class ArrayDatasource implements Selectable
         $filter = $query->getFilter();
         $offset = $query->hasOffset() ? $query->getOffset() : 0;
         $limit = $query->hasLimit() ? $query->getLimit() : 0;
-        $data = $this->data;
+
+        $data = [];
+        foreach ($this->data as $key => $row) {
+            if ($this->keyColumn !== null && ! isset($row->{$this->keyColumn})) {
+                $row = clone $row; // Make sure that this won't affect the actual data
+                $row->{$this->keyColumn} = $key;
+            }
+
+            $data[$key] = $row;
+        }
 
         if ($query->hasOrder()) {
             uasort($data, [$query, 'compare']);
@@ -206,11 +215,6 @@ class ArrayDatasource implements Selectable
         $result = [];
         $skipped = 0;
         foreach ($data as $key => $row) {
-            if ($this->keyColumn !== null && !isset($row->{$this->keyColumn})) {
-                $row = clone $row; // Make sure that this won't affect the actual data
-                $row->{$this->keyColumn} = $key;
-            }
-
             if (! $filter->matches($row)) {
                 continue;
             } elseif ($skipped < $offset) {

test/php/library/Icinga/Data/DataArray/ArrayDatasourceTest.php
@@ -132,4 +132,31 @@ class ArrayDatasourceTest extends BaseTestCase
             'ArrayDatasource does not sort limited queries correctly'
         );
     }
+
+    public function testOrderByKeyColumnIsCorrect()
+    {
+        $result = (new ArrayDatasource([
+            'a' => (object) [
+                'foo' => 'bar',
+                'baz' => 'qux'
+            ],
+            'b' => (object) [
+                'foo' => 'bar',
+                'baz' => 'qux'
+            ],
+            'c' => (object) [
+                'foo' => 'bar',
+                'baz' => 'qux'
+            ]
+        ]))->setKeyColumn('name')
+            ->select()
+            ->order('name', 'desc')
+            ->fetchAll();
+
+        $this->assertSame(
+            ['c', 'b', 'a'],
+            array_keys($result),
+            'ArrayDatasource does not sort queries correctly by key column'
+        );
+    }
 }