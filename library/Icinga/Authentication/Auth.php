3d0f6de0fba1b903becc9249f9505a32e8d04ff0
Johannes Meyer <johannes.meyer@icinga.com>
2025-07-08 14:22:56 +0200

    Auth: Perform authentication only once and not lazily
    Since authentication is now performed even for static
    resources, there's no reason anymore to support implicit
    authentication. This also limits authentication attempts
    to a single one, previously failed attempts were repeated.
    
    Requiring authentication during bootstrapping, i.e. before
    authentication has been performed, will now trigger a
    deprecation notice.
    
    refs #5265

--- library/Icinga/Authentication/Auth.php
+++ library/Icinga/Authentication/Auth.php
@@ -19,6 +19,7 @@ use Icinga\User\Preferences;
 use Icinga\User\Preferences\PreferencesStore;
 use Icinga\Web\Session;
 use Icinga\Web\StyleSheet;
+use LogicException;
 
 class Auth
 {
@@ -50,6 +51,14 @@ class Auth
      */
     private $user;
 
+    /**
+     * Whether the user has been authenticated
+     *
+     * If true, this does NOT mean authentication was successful.
+     *
+     * @var bool
+     */
+    private bool $authenticated = false;
 
     /**
      * @see getInstance()
@@ -88,14 +97,11 @@ class Auth
      */
     public function isAuthenticated()
     {
-        if ($this->user !== null) {
-            return true;
+        if (! $this->authenticated) {
+            trigger_error('Authentication can no longer be triggered implicitly', E_USER_DEPRECATED);
         }
-        $this->authenticateFromSession();
-        if ($this->user === null && ! $this->authExternal()) {
-            return false;
-        }
-        return true;
+
+        return $this->user !== null;
     }
 
     public function setAuthenticated(User $user, $persist = true)
@@ -210,6 +216,39 @@ class Auth
     public function setUser(User $user)
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Authenticate the user
+     *
+     * This method will try to authenticate the user using the session first, then external backends and finally HTTP
+     * authentication. If authentication has already been performed, an exception will be thrown.
+     *
+     * @throws LogicException If authentication has already been performed
+     *
+     * @return $this
+     */
+    public function authenticate(): static
+    {
+        if ($this->authenticated) {
+            throw new LogicException('Cannot perform authentication more than once');
+        }
+
+        $this->authenticateFromSession();
+        if ($this->user === null) {
+            $this->authExternal();
+
+            if ($this->user === null
+                && $this->getRequest()->isApiRequest()
+                && ! $this->getRequest()->isXmlHttpRequest()
+            ) {
+                $this->authHttp();
+            }
+        }
+
+        $this->authenticated = true;
 
         return $this;
     }
