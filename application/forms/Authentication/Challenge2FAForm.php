f854a41a3ccade526024175fb6b5037b5126720c
Jan Schuppik <jan.schuppik@icinga.com>
2025-07-24 21:09:08 +0200

    Add: 2FA login challenge without proper validation

---
+++ application/forms/Authentication/Challenge2FAForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Icinga\Forms\Authentication;
+
+use Icinga\Application\Hook\AuthenticationHook;
+use Icinga\Authentication\Auth;
+use Icinga\Web\Session;
+use Icinga\Web\Url;
+
+class Challenge2FAForm extends LoginForm
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        $this->setRequiredCue(null);
+        $this->setName('form_challenge_2fa');
+        $this->setSubmitLabel($this->translate('Verify'));
+        $this->setProgressLabel($this->translate('Verifying'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createElements(array $formData)
+    {
+        $this->addElement(
+            'text',
+            'code',
+            [
+                'autocapitalize' => 'off',
+                'class' => 'autofocus content-centered',
+                'placeholder' => $this->translate('Please enter your 2FA code'),
+                'required' => true,
+                'autocomplete'  => 'off',
+
+            ]
+        );
+
+        $this->addElement(
+            'hidden',
+            'redirect',
+            [
+                'value' => Url::fromRequest()->getParam('redirect')
+            ]
+        );
+    }
+
+    public function onSuccess()
+    {
+        // TODO: Implement proper 2FA code validation
+        if ($_POST['code'] == 666) {
+
+            $auth = Auth::getInstance();
+            $user = $auth->getUser();
+            Session::getSession()->set('challenged_successful_2fa_token', true);
+            Session::getSession()->delete('must_challenge_2fa_token');
+
+            $auth->setAuthenticated($user);
+
+            AuthenticationHook::triggerLogin($user);
+            $this->getResponse()->setRerenderLayout(true);
+            return true;
+        }
+
+        $this->getElement('code')->addError($this->translate('Code is invalid!'));
+
+        return false;
+    }
+}
