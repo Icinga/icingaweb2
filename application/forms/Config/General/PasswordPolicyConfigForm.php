ed7836581cb678f332980fcd5a7e323ffc334af0
Jolien Trog <jolien.trog@icinga.com>
2025-08-26 14:24:50 +0200

    fix code sniffer violations

--- application/forms/Config/General/PasswordPolicyConfigForm.php
+++ application/forms/Config/General/PasswordPolicyConfigForm.php
@@ -51,11 +51,13 @@ class PasswordPolicyConfigForm extends Form
             'global_password_policy',
             array(
                 'description' => $this->translate(
-                    'Enforce strong password requirements for new passwords'),
+                'Enforce strong password requirements for new passwords'
+                ),
                 'label'       => $this->translate('Password Policy'),
                 'multiOptions' => array_merge(
-                    ['' => sprintf(' - %s - ',
-                    $this->translate('No Password Policy')
+                    ['' => sprintf(
+                        ' - %s - ',
+                        $this->translate('No Password Policy')
                     )],
                     $passwordPolicies
                 ),
