3b481e4cbeff8ba2409bd59e5c2f04b4b2b58604
Jolien Trog <jolien.trog@icinga.com>
2025-08-27 09:48:05 +0200

    add constructor to PasswordValidator and codereview changes

--- application/forms/Config/General/PasswordPolicyConfigForm.php
+++ application/forms/Config/General/PasswordPolicyConfigForm.php
@@ -1,4 +1,5 @@
 <?php
+/* Icinga Web 2 | (c) 2025 Icinga GmbH | GPLv2+ */
 
 namespace Icinga\Forms\Config\General;
 
@@ -12,34 +13,14 @@ use Icinga\Web\Form;
  */
 class PasswordPolicyConfigForm extends Form
 {
-    /**
-     * {@inheritdoc}
-     */
     public function init(): void
     {
         $this->setName('form_config_general_password_policy');
     }
 
-    /**
-     * {@inheritdoc}
-     *
-     * @return  $this
-     */
-    public function createElements(array $formData)
+    public function createElements(array $formData): self
     {
-        $this->addElement(
-            'checkbox',
-            'global_password_policy',
-            array(
-                'label'       => $this->translate('Password Policy'),
-                'value'       => true,
-                'description' => $this->translate(
-                    'Enforce strong password requirements for new passwords'
-                ),
-            )
-        );
-
-        $passwordPolicies = [];
+      $passwordPolicies = [];
 
         foreach (Hook::all('passwordpolicy') as $class => $policy) {
             $passwordPolicies[$class] = $policy->getName();
@@ -49,19 +30,16 @@ class PasswordPolicyConfigForm extends Form
         $this->addElement(
             'select',
             'global_password_policy',
-            array(
+            [
                 'description' => $this->translate(
                     'Enforce strong password requirements for new passwords'
                 ),
                 'label'       => $this->translate('Password Policy'),
                 'multiOptions' => array_merge(
-                    ['' => sprintf(
-                        ' - %s - ',
-                        $this->translate('No Password Policy')
-                    )],
+                    ['' => $this->translate('No Password Policy')],
                     $passwordPolicies
                 ),
-            )
+            ]
         );
 
         return $this;
