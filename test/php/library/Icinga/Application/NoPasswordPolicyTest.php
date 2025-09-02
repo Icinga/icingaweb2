0c1d3bae7f868f59a27495fb2bc308fedf5c489e
Jolien Trog <jolien.trog@icinga.com>
2025-09-02 15:59:18 +0200

    codereview: Improve type hints and return types

--- test/php/library/Icinga/Application/NoPasswordPolicyTest.php
+++ test/php/library/Icinga/Application/NoPasswordPolicyTest.php
@@ -8,26 +8,21 @@ use Icinga\Application\ProvidedHook\NoPasswordPolicy;
 
 class NoPasswordPolicyTest extends BaseTestCase
 {
-    private object $object;
+    private PasswordPolicyHook $instance;
 
     public function setUp(): void
     {
-        $this->object = new NoPasswordPolicy();
-    }
-
-    public function testClassIsInstanzOf()
-    {
-        $this->assertInstanceOf(PasswordPolicyHook::class, $this->object);
+        $this->instance = new NoPasswordPolicy();
     }
 
     public function testMethodGetName(): void
     {
-        $this->assertSame('None', $this->object->getName());
+        $this->assertSame('None', $this->instance->getName());
     }
 
-    public function testValidatePasswordValid()
+    public function testValidatePasswordValid(): void
     {
-        $res = $this->object->validatePassword('icingaadmin');
+        $res = $this->instance->validatePassword('icingaadmin');
         $this->assertEmpty($res);
     }
 }
