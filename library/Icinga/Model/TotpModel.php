f93928b9a323dd84c8d81145bbb11be4705835cc
Johannes Rauh <johannes.rauh@icinga.com>
2025-11-19 12:02:27 +0100

    Add property docs for model

--- library/Icinga/Model/TotpModel.php
+++ library/Icinga/Model/TotpModel.php
@@ -4,10 +4,18 @@
 
 namespace Icinga\Model;
 
+use DateTime;
 use ipl\Orm\Behavior\MillisecondTimestamp;
 use ipl\Orm\Behaviors;
 use ipl\Orm\Model;
 
+/**
+ * Database model for the table that stores the secrets for 2FA
+ *
+ * @property string   $username The user who owns the secret
+ * @property string   $secret   The secret from which the tokens are generated
+ * @property DateTime $ctime    The creation time of the secret
+ */
 class TotpModel extends Model
 {
     public function getTableName(): string
