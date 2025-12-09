7ad2f7eb9c65b743980e879736d794b5fddc0623
Johannes Rauh <johannes.rauh@icinga.com>
2025-12-09 12:04:16 +0100

    Make `ctime` column not nullable

--- schema/pgsql.schema.sql
+++ schema/pgsql.schema.sql
@@ -134,7 +134,7 @@ CREATE TABLE "icingaweb_schema" (
 CREATE TABLE "icingaweb_2fa" (
   "username" varchar(254) NOT NULL,
   "secret"   varchar(255) NOT NULL,
-  "ctime"    bigint,
+  "ctime"    bigint NOT NULL,
   CONSTRAINT pk_icingaweb_2fa PRIMARY KEY ("username")
 );
 
