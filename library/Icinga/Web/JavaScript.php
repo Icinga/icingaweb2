1df38fd3e304c5cf04115019b8b35960d247b347
Jan Schuppik <jan.schuppik@icinga.com>
2026-01-21 12:18:10 +0100

    Revert: creation of the relative-time-behavior
    Revert "Fix: add stop method"
    
    This reverts commit 7605c655fd6f8f3271e3a0bfad952c27a2074556.
    
    Revert "Adjust onRendered method"
    
    This reverts commit ab868e15f83abc98b27ca65839723b359ebc6d7f.
    
    Revert "Adjust: html-attribute 'datetime' as update source"
    
    This reverts commit 8bf1b1746d4a68a8318c548f836671bb75d04042.
    
    Revert "Init: js relative-time behavior"
    
    This reverts commit f17bf24def4060f82b1d1a7afd69bf9ffddcdf48.

--- library/Icinga/Web/JavaScript.php
+++ library/Icinga/Web/JavaScript.php
@@ -45,8 +45,7 @@ class JavaScript
         'js/icinga/behavior/modal.js',
         'js/icinga/behavior/input-enrichment.js',
         'js/icinga/behavior/datetime-picker.js',
-        'js/icinga/behavior/copy-to-clipboard.js',
-        'js/icinga/behavior/relative-time.js'
+        'js/icinga/behavior/copy-to-clipboard.js'
     ];
 
     protected static $vendorFiles = [];
