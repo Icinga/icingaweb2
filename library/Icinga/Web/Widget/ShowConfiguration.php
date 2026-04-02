583529129aa285fde20f8bafd71bdb13501cc4f3
Alexander Rieß <alexander.riess@icinga.com>
2026-04-02 07:25:46 +0200

    Change license and add SPDX-header

--- library/Icinga/Web/Widget/ShowConfiguration.php
+++ library/Icinga/Web/Widget/ShowConfiguration.php
@@ -1,5 +1,7 @@
 <?php
-/* Icinga Web 2 | (c) 2026 Icinga GmbH | GPLv2+ */
+
+// SPDX-FileCopyrightText: 2026 Icinga GmbH <https://icinga.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 namespace Icinga\Web\Widget;
 
